7ae33d2ae8c997ee768c2b8f180a7136d3edd592
SoniaGrh <sonia.garrouch@hotmail.fr>
2023-02-16 15:51:07 +0000

    improvements on logs + train test split

--- captured-training-yolov5-detection/picsellia/docker_run_training_yolov5_detection.py
+++ captured-training-yolov5-detection/picsellia/docker_run_training_yolov5_detection.py
@@ -1,11 +1,10 @@
-from picsellia_yolov5.yolov5.train import train
-from picsellia_yolov5.yolov5.utils.callbacks import Callbacks
-from picsellia_yolov5.yolov5.utils.torch_utils import select_device
-from picsellia_yolov5.yolov5.utils.general import check_file, check_yaml, increment_path
-from picsellia_yolov5 import picsellia_utils
+from picsellia_yolov5.picsellia_yolov5.yolov5.train import train
+from picsellia_yolov5.picsellia_yolov5.yolov5.utils.callbacks import Callbacks
+from picsellia_yolov5.picsellia_yolov5.yolov5.utils.torch_utils import select_device
+from picsellia_yolov5.picsellia_yolov5 import picsellia_utils
 
 from picsellia.types.enums import AnnotationFileType
-from picsellia.sdk.asset import MultiAsset
+
 import os
 import random
 import json 
@@ -26,47 +25,50 @@ RANK = int(os.getenv('RANK', -1))
 experiment = picsellia_utils.get_experiment()
 
 experiment.download_artifacts(with_tree=True)
-
-dataset = experiment.list_attached_dataset_versions()[0]
 current_dir = os.path.join(os.getcwd(), experiment.base_dir)
-annotation_path = dataset.export_annotation_file(AnnotationFileType.COCO, current_dir)
-f = open(annotation_path)
-annotations_dict = json.load(f)
-annotations_coco=COCO(annotation_path)
+base_imgdir = experiment.png_dir
 
-labelmap = {}
-for x in annotations_dict['categories']:
-    labelmap[str(x['id'])] = x['name']
+parameters = experiment.get_log(name='parameters').data
+
+if len(experiment.list_attached_dataset_versions())==3:
+    train_ds, val_ds, test_ds = experiment.get_dataset(name='train'), experiment.get_dataset(name='val'), experiment.get_dataset(name='test')
+    
+    for data_type, dataset in {'train' : train_ds, 'val' : val_ds, 'test' : test_ds}.items():
+        annotation_path = dataset.export_annotation_file(AnnotationFileType.COCO, current_dir)
+        f = open(annotation_path)
+        annotations_dict = json.load(f)
+        annotations_coco=COCO(annotation_path)
+        
+        if data_type=='train':
+            labelmap = {}
+            for x in annotations_dict['categories']:
+                labelmap[str(x['id'])] = x['name']
+        
+        dataset.list_assets().download(target_path=os.path.join(base_imgdir, data_type, 'images'), max_workers=8)
+        picsellia_utils.create_yolo_detection_label(experiment, data_type, annotations_dict, annotations_coco)
+    
+else: 
+    dataset = experiment.list_attached_dataset_versions()[0]
+    
+    annotation_path = dataset.export_annotation_file(AnnotationFileType.COCO, current_dir)
+    f = open(annotation_path)
+    annotations_dict = json.load(f)
+    annotations_coco=COCO(annotation_path)
+    labelmap = {}
+    for x in annotations_dict['categories']:
+        labelmap[str(x['id'])] = x['name']
+    
+    prop = 0.7 if not 'prop_train_split' in parameters.keys() else parameters["prop_train_split"]
+
+    train_assets, test_assets, val_assets = picsellia_utils.train_test_val_split(experiment, dataset, prop)
+    
+    for data_type, assets in {'train' : train_assets, 'val' : val_assets, 'test' : test_assets}.items():
+        assets.download(target_path=os.path.join(base_imgdir, data_type, 'images'), max_workers=8)
+        picsellia_utils.create_yolo_detection_label(experiment, data_type, annotations_dict, annotations_coco)
+    
 experiment.log('labelmap', labelmap, 'labelmap', replace=True)
 
-targetdir = experiment.base_dir
-
-base_imgdir = experiment.png_dir
-prop = 0.7
-
-train_assets, test_assets, train_split, test_split, labels = dataset.train_test_split(prop=prop, random_seed=42)
-
-test_list = test_assets.items.copy()
-random.seed(42)
-random.shuffle(test_list)
-
-nb_asset = len(test_list)//2
-val_data = test_list[nb_asset:]
-test_data = test_list[:nb_asset]
-val_assets = MultiAsset(dataset.connexion, dataset.id, val_data)
-test_assets = MultiAsset(dataset.connexion, dataset.id, test_data)
-
-train_assets.download(target_path=os.path.join(experiment.png_dir, 'train', 'images'), max_workers=8)
-val_assets.download(target_path=os.path.join(experiment.png_dir, 'val', 'images'), max_workers=8)
-test_assets.download(target_path=os.path.join(experiment.png_dir, 'test', 'images'), max_workers=8)
-
-picsellia_utils.create_yolo_detection_label(experiment, annotations_dict, annotations_coco)
 data_yaml_path = picsellia_utils.generate_data_yaml(experiment, labelmap, current_dir)
-
-experiment.log('train-split', train_split, 'bar', replace=True)
-experiment.log('test-split', test_split, 'bar', replace=True)
-parameters = experiment.get_log(name='parameters').data
-# config_dir = os.path.join(os.getcwd(), experiment.config_dir)[1:]
 cfg = picsellia_utils.edit_model_yaml(label_map=labelmap, experiment_name=experiment.name, config_path=experiment.config_dir)
 opt = picsellia_utils.setup_hyp(
     experiment = experiment,
@@ -75,20 +77,12 @@ opt = picsellia_utils.setup_hyp(
     params=parameters,
     label_map=labelmap
     )
-opt.data, opt.cfg, opt.hyp, opt.weights, opt.project = check_file(opt.data), check_yaml(opt.cfg), check_yaml(opt.hyp), str(opt.weights), str(opt.project)  # checks
-assert len(opt.cfg) or len(opt.weights), 'either --cfg or --weights must be specified'
-if opt.evolve:
-    cwd = os.getcwd()
-    if opt.project == os.path.join(cwd, 'runs/train'):  # if default project name, rename to runs/evolve
-        opt.project = os.path.join(cwd, 'runs/evolve')
-    opt.exist_ok, opt.resume = opt.resume, False  # pass resume to exist_ok and disable resume
-if opt.name == 'cfg':
-    opt.name = Path(opt.cfg).stem  # use model.yaml as name
-opt.save_dir = str(increment_path(Path(opt.project) / opt.name, exist_ok=opt.exist_ok))
+
+picsellia_utils.check_files(opt)
 
 callbacks=Callbacks()
 device = select_device(opt.device, batch_size=opt.batch_size)
 
 train(opt.hyp, opt, device, callbacks, pxl=experiment)
 
-picsellia_utils.send_run_to_picsellia(experiment, targetdir)
+picsellia_utils.send_run_to_picsellia(experiment)
