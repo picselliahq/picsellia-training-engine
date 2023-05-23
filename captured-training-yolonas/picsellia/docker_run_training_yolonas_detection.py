b02ea5b2a8c9479ff85f89c4ca020df52b77c110
hajeressef <hajer.essefi@picsellia.com>
2023-05-23 16:04:03 +0000

    Refactor code

--- captured-training-yolonas/picsellia/docker_run_training_yolonas_detection.py
+++ captured-training-yolonas/picsellia/docker_run_training_yolonas_detection.py
@@ -8,11 +8,14 @@ from picsellia.types.enums import LogType
 
 from super_gradients.training import Trainer, dataloaders, models
 from super_gradients.training.dataloaders.dataloaders import (
-    coco_detection_yolo_format_train, coco_detection_yolo_format_val
+    coco_detection_yolo_format_train,
+    coco_detection_yolo_format_val,
 )
 from super_gradients.training.losses import PPYoloELoss
 from super_gradients.training.metrics import DetectionMetrics_050
-from super_gradients.training.models.detection_models.pp_yolo_e import PPYoloEPostPredictionCallback
+from super_gradients.training.models.detection_models.pp_yolo_e import (
+    PPYoloEPostPredictionCallback,
+)
 
 from super_gradients.training.utils.callbacks import Callback, PhaseContext
 from super_gradients.common.environment.ddp_utils import multi_process_safe
@@ -21,117 +24,149 @@ import yolonas_utils
 
 os.environ["PYTHONUNBUFFERED"] = "1"
 os.environ["PICSELLIA_SDK_DOWNLOAD_BAR_MODE"] = "2"
-os.environ['PICSELLIA_SDK_CUSTOM_LOGGING'] = "True"
+os.environ["PICSELLIA_SDK_CUSTOM_LOGGING"] = "True"
 
-logging.getLogger('picsellia').setLevel(logging.INFO)
+logging.getLogger("picsellia").setLevel(logging.INFO)
 
 
 # classback class to log training metrics to picsellia
 class SaveTrainingMetrics(Callback):
-
     @multi_process_safe
     def on_train_loader_end(self, context: PhaseContext) -> None:
-        experiment.log(name='training_loss_cls', type=LogType.LINE,
-                       data=float(context.metrics_dict['PPYoloELoss/loss_cls']))
-        experiment.log(name='training_loss_iou', type=LogType.LINE,
-                       data=float(context.metrics_dict['PPYoloELoss/loss_iou']))
-        experiment.log(name='training_loss_dfl', type=LogType.LINE,
-                       data=float(context.metrics_dict['PPYoloELoss/loss_dfl']))
-        experiment.log(name='training_loss', type=LogType.LINE, data=float(context.metrics_dict['PPYoloELoss/loss']))
+        experiment.log(
+            name="training_loss_cls",
+            type=LogType.LINE,
+            data=float(context.metrics_dict["PPYoloELoss/loss_cls"]),
+        )
+        experiment.log(
+            name="training_loss_iou",
+            type=LogType.LINE,
+            data=float(context.metrics_dict["PPYoloELoss/loss_iou"]),
+        )
+        experiment.log(
+            name="training_loss_dfl",
+            type=LogType.LINE,
+            data=float(context.metrics_dict["PPYoloELoss/loss_dfl"]),
+        )
+        experiment.log(
+            name="training_loss",
+            type=LogType.LINE,
+            data=float(context.metrics_dict["PPYoloELoss/loss"]),
+        )
 
     @multi_process_safe
     def on_validation_loader_end(self, context: PhaseContext) -> None:
-        experiment.log(name='validation_loss_cls', type=LogType.LINE,
-                       data=float(context.metrics_dict['PPYoloELoss/loss_cls']))
-        experiment.log(name='validation_loss_iou', type=LogType.LINE,
-                       data=float(context.metrics_dict['PPYoloELoss/loss_iou']))
-        experiment.log(name='validation_loss_dfl', type=LogType.LINE,
-                       data=float(context.metrics_dict['PPYoloELoss/loss_dfl']))
-        experiment.log(name='validation_loss', type=LogType.LINE, data=float(context.metrics_dict['PPYoloELoss/loss']))
+        experiment.log(
+            name="validation_loss_cls",
+            type=LogType.LINE,
+            data=float(context.metrics_dict["PPYoloELoss/loss_cls"]),
+        )
+        experiment.log(
+            name="validation_loss_iou",
+            type=LogType.LINE,
+            data=float(context.metrics_dict["PPYoloELoss/loss_iou"]),
+        )
+        experiment.log(
+            name="validation_loss_dfl",
+            type=LogType.LINE,
+            data=float(context.metrics_dict["PPYoloELoss/loss_dfl"]),
+        )
+        experiment.log(
+            name="validation_loss",
+            type=LogType.LINE,
+            data=float(context.metrics_dict["PPYoloELoss/loss"]),
+        )
 
 
 experiment = yolonas_utils.get_experiment()
 cwd = os.getcwd()
-path_dict = {} 
-path_dict, labelmap, label_names = yolonas_utils.get_yolo_dataset(experiment,cwd) # get the data 
-
+path_dict = {}
+path_dict = yolonas_utils.create_yolo_dataset(experiment, cwd)  # get the data
+labelmap, label_names = yolonas_utils.get_labelmap(experiment=experiment)
 # trainer params
 EXPERIMENT_NAME = experiment.name
-CHECKPOINT_DIR = os.path.join(cwd, experiment.name, 'checkpoints')  # path to save the checkpoints to
+# path to save the checkpoints to
+CHECKPOINT_DIR = os.path.join(cwd, experiment.name, "checkpoints")
 
 # dataset params
 DATA_DIR = os.path.join(cwd, experiment.base_dir)
-TRAIN_IMAGES_DIR = path_dict['train']['dataset_dir']  # child dir of DATA_DIR where train images are
-VAL_IMAGES_DIR = path_dict['val']['dataset_dir']  # child dir of DATA_DIR where validation images are
-TEST_IMAGES_DIR = path_dict['test']['dataset_dir']  # child dir of DATA_DIR where test images are
+# child dir of DATA_DIR where train images are
+TRAIN_IMAGES_DIR = path_dict["train"]["dataset_dir"]
+# child dir of DATA_DIR where validation images are
+VAL_IMAGES_DIR = path_dict["val"]["dataset_dir"]
+# child dir of DATA_DIR where test images are
+TEST_IMAGES_DIR = path_dict["test"]["dataset_dir"]
 
-TRAIN_LABELS_DIR = os.path.join(cwd, path_dict['train'][
-    'yolo_annotation_path'])  # child dir of DATA_DIR where train labels are
-VAL_LABELS_DIR = os.path.join(cwd, path_dict['val'][
-    'yolo_annotation_path'])  # child dir of DATA_DIR where validation labels are
-TEST_LABELS_DIR = os.path.join(cwd,
-                               path_dict['test']['yolo_annotation_path'])  # child dir of DATA_DIR where test labels are
+TRAIN_LABELS_DIR = os.path.join(
+    cwd, path_dict["train"]["yolo_annotation_path"]
+)  # child dir of DATA_DIR where train labels are
+VAL_LABELS_DIR = os.path.join(
+    cwd, path_dict["val"]["yolo_annotation_path"]
+)  # child dir of DATA_DIR where validation labels are
+TEST_LABELS_DIR = os.path.join(
+    cwd, path_dict["test"]["yolo_annotation_path"]
+)  # child dir of DATA_DIR where test labels are
 
 CLASSES = label_names
 NUM_CLASSES = len(CLASSES)
-parameters = experiment.get_log(name='parameters').data
+parameters = experiment.get_log(name="parameters").data
 
 batch_size = parameters.get("batch_size", 4)
 initial_lr = parameters.get("initial_lr", 0.0001)
 max_epochs = parameters.get("max_epochs", 5)
 
-DATALOADER_PARAMS = {
-    'batch_size': parameters['batch_size'],
-    'num_workers': 2
-}
+DATALOADER_PARAMS = {"batch_size": parameters["batch_size"], "num_workers": 2}
 # model params
-MODEL_NAME = 'yolo_nas_s'  # choose from yolo_nas_s, yolo_nas_m, yolo_nas_l
-PRETRAINED_WEIGHTS = 'coco'  # only one option here: coco
+MODEL_NAME = "yolo_nas_s"  # choose from yolo_nas_s, yolo_nas_m, yolo_nas_l
+PRETRAINED_WEIGHTS = "coco"  # only one option here: coco
 
 # get the checkpoints from experiment to experiment.base_dir
 experiment.download_artifacts(with_tree=False)
 
 train_data = coco_detection_yolo_format_train(
     dataset_params={
-        'data_dir': DATA_DIR,
-        'images_dir': TRAIN_IMAGES_DIR,
-        'labels_dir': TRAIN_LABELS_DIR,
-        'classes': CLASSES
+        "data_dir": DATA_DIR,
+        "images_dir": TRAIN_IMAGES_DIR,
+        "labels_dir": TRAIN_LABELS_DIR,
+        "classes": CLASSES,
     },
-    dataloader_params=DATALOADER_PARAMS
+    dataloader_params=DATALOADER_PARAMS,
 )
 
 val_data = coco_detection_yolo_format_val(
     dataset_params={
-        'data_dir': DATA_DIR,
-        'images_dir': VAL_IMAGES_DIR,
-        'labels_dir': VAL_LABELS_DIR,
-        'classes': CLASSES
+        "data_dir": DATA_DIR,
+        "images_dir": VAL_IMAGES_DIR,
+        "labels_dir": VAL_LABELS_DIR,
+        "classes": CLASSES,
     },
-    dataloader_params=DATALOADER_PARAMS
+    dataloader_params=DATALOADER_PARAMS,
 )
 
 test_data = coco_detection_yolo_format_val(
     dataset_params={
-        'data_dir': DATA_DIR,
-        'images_dir': TEST_IMAGES_DIR,
-        'labels_dir': TEST_LABELS_DIR,
-        'classes': CLASSES
+        "data_dir": DATA_DIR,
+        "images_dir": TEST_IMAGES_DIR,
+        "labels_dir": TEST_LABELS_DIR,
+        "classes": CLASSES,
     },
-    dataloader_params=DATALOADER_PARAMS
+    dataloader_params=DATALOADER_PARAMS,
 )
 
 trainer = Trainer(experiment_name=EXPERIMENT_NAME, ckpt_root_dir=CHECKPOINT_DIR)
 
-artifact_namepath = experiment.get_artifact(name='checkpoints_best').filename  # path to the pretrained weights
+artifact_namepath = experiment.get_artifact(
+    name="checkpoints_best"
+).filename  # path to the pretrained weights
 
 # get the weights locally and feed it to function
-model = models.get('yolo_nas_s', 
-                   checkpoint_path=os.path.join(cwd,experiment.base_dir, artifact_namepath),
-                   num_classes=NUM_CLASSES,
-                   download_required_code=False,
-                   checkpoint_num_classes=len(experiment.get_log('labelmap').data)   
-                   )   
+model = models.get(
+    "yolo_nas_s",
+    checkpoint_path=os.path.join(cwd, experiment.base_dir, artifact_namepath),
+    num_classes=NUM_CLASSES,
+    download_required_code=False,
+    checkpoint_num_classes=len(experiment.get_log("labelmap").data),
+)
 
 # experiment parameters
 train_params = {
@@ -148,16 +183,14 @@ train_params = {
     "zero_weight_decay_on_bias_and_bn": True,
     "ema": True,
     "ema_params": {"decay": 0.9, "decay_type": "threshold"},
-
     "phase_callbacks": [SaveTrainingMetrics()],
-
     "max_epochs": max_epochs,
     "mixed_precision": True,
     "loss": PPYoloELoss(
         use_static_assigner=False,
         # NOTE: num_classes needs to be defined here
         num_classes=NUM_CLASSES,
-        reg_max=16
+        reg_max=16,
     ),
     "valid_metrics_list": [
         DetectionMetrics_050(
@@ -170,54 +203,64 @@ train_params = {
                 score_threshold=0.01,
                 nms_top_k=1000,
                 max_predictions=300,
-                nms_threshold=0.7
-            )
+                nms_threshold=0.7,
+            ),
         )
     ],
-    "metric_to_watch": 'mAP@0.50'
+    "metric_to_watch": "mAP@0.50",
 }
 
-# log labelmap 
+# log labelmap
 labels = experiment.get_dataset("test").list_labels()
 experiment.log("labelmap", labelmap, "labelmap", replace=True)
 
 # launch the training
-trainer.train(model=model,
-              training_params=train_params,
-              train_loader=train_data,
-              valid_loader=val_data)
+trainer.train(
+    model=model,
+    training_params=train_params,
+    train_loader=train_data,
+    valid_loader=val_data,
+)
 
 # get the best trained model
-checkpoint_path=os.path.join(CHECKPOINT_DIR, EXPERIMENT_NAME, 'ckpt_best.pth')
-best_model = models.get(MODEL_NAME,
-                        num_classes=NUM_CLASSES,
-                        checkpoint_path=os.path.join(checkpoint_path))
+checkpoint_path = os.path.join(CHECKPOINT_DIR, EXPERIMENT_NAME, "ckpt_best.pth")
+best_model = models.get(
+    MODEL_NAME, num_classes=NUM_CLASSES, checkpoint_path=os.path.join(checkpoint_path)
+)
 
-# save model to onnx format 
-onnx_model_path = 'yolo_nas_s_checkpoints.onnx'
-models.convert_to_onnx(model=best_model, input_shape=(3,640,640), out_path=onnx_model_path)
+# save model to onnx format
+onnx_model_path = "yolo_nas_s_checkpoints.onnx"
+models.convert_to_onnx(
+    model=best_model, input_shape=(3, 640, 640), out_path=onnx_model_path
+)
 
 # store both best model in .pth and onnx format to picsellia
-experiment.store('model-latest',os.path.join(cwd,onnx_model_path))
-experiment.store('checkpoints_best', checkpoint_path)
+experiment.store("model-latest", os.path.join(cwd, onnx_model_path))
+experiment.store("checkpoints_best", checkpoint_path)
 
-test_results = trainer.test(model=best_model,
-            test_loader=test_data,
-            test_metrics_list=DetectionMetrics_050(score_thres=0.1,
-                                                   top_k_predictions=300,
-                                                   num_cls=NUM_CLASSES,
-                                                   normalize_targets=True,
-                                                   post_prediction_callback=PPYoloEPostPredictionCallback(score_threshold=0.01,
-                                                                                                          nms_top_k=1000,                                                               max_predictions=300,                                                                              nms_threshold=0.7)
-                                                  ))
+test_results = trainer.test(
+    model=best_model,
+    test_loader=test_data,
+    test_metrics_list=DetectionMetrics_050(
+        score_thres=0.1,
+        top_k_predictions=300,
+        num_cls=NUM_CLASSES,
+        normalize_targets=True,
+        post_prediction_callback=PPYoloEPostPredictionCallback(
+            score_threshold=0.01, nms_top_k=1000, max_predictions=300, nms_threshold=0.7
+        ),
+    ),
+)
 # convert test metrics to types we can log to experiment
-test_results = yolonas_utils.convert_test_results(test_results)
-experiment.log(name='test metrics', type=LogType.TABLE, data=test_results)
+test_results = yolonas_utils.format_test_results(test_results)
+experiment.log(name="test metrics", type=LogType.TABLE, data=test_results)
 
 # log evaluations
 test_image_list = experiment.get_dataset("test").list_assets()
 for asset in test_image_list:
-    bbox_list = yolonas_utils.get_asset_predictions(experiment, best_model, asset, conf=0.1)
+    bbox_list = yolonas_utils.get_asset_predictions(
+        experiment, best_model, asset, conf=0.1, dataset_type="test"
+    )
 
-    if len(bbox_list)>0:
+    if len(bbox_list) > 0:
         experiment.add_evaluation(asset, "replace", rectangles=bbox_list)
--- captured-training-yolonas/picsellia/yolonas_utils.py
+++ captured-training-yolonas/picsellia/yolonas_utils.py
@@ -4,99 +4,12 @@ import shutil
 import json
 from picsellia import Client
 from picsellia.types.enums import AnnotationFileType
-
+from picsellia.sdk.experiment import Experiment, Asset
 import torch
 
 
-def convert_bbox_coco2yolo(img_width, img_height, bbox):
-    """
-    Convert bounding box from COCO  format to YOLO format
-
-    Parameters
-    ----------
-    img_width : int
-        width of image
-    img_height : int
-        height of image
-    bbox : list[int]
-        bounding box annotation in COCO format:
-        [top left x position, top left y position, width, height]
-
-    Returns
-    -------
-    list[float]
-        bounding box annotation in YOLO format:
-        [x_center_rel, y_center_rel, width_rel, height_rel]
-    """
-
-    # YOLO bounding box format: [x_center, y_center, width, height]
-    # (float values relative to width and height of image)
-    x_tl, y_tl, w, h = bbox
-
-    dw = 1.0 / img_width
-    dh = 1.0 / img_height
-
-    x_center = x_tl + w / 2.0
-    y_center = y_tl + h / 2.0
-
-    x = x_center * dw
-    y = y_center * dh
-    w = w * dw
-    h = h * dh
-
-    return [x, y, w, h]
-
-
-def make_folders(path="output"):
-    if os.path.exists(path):
-        shutil.rmtree(path)
-    os.makedirs(path)
-    return path
-
-
-def convert_coco_json_to_yolo_txt(output_path, json_file):
-    """
-    Create folders containing dataset in yolo format 
-
-    Args:
-        output_path (str): 
-        json_file (str): json file containing the coco annotations 
-
-    Returns:
-        output_path (str): dataset directory path
-    """
-    path = make_folders(output_path)
-
-    with open(json_file) as f:
-        json_data = json.load(f)
-
-    # write _darknet.labels, which holds names of all classes (one class per line)
-    label_file = os.path.join(output_path, "_darknet.labels")
-    with open(label_file, "w") as f:
-        for category in tqdm(json_data["categories"], desc="Categories"):
-            category_name = category["name"]
-            f.write(f"{category_name}\n")
-
-    for image in tqdm(json_data["images"], desc="Annotation txt for each image"):
-        img_id = image["id"]
-        img_name = image["file_name"]
-        img_width = image["width"]
-        img_height = image["height"]
-
-        anno_in_image = [anno for anno in json_data["annotations"] if anno["image_id"] == img_id]
-        anno_txt = os.path.join(output_path, img_name.split(".")[0] + ".txt")
-        with open(anno_txt, "w") as f:
-            for anno in anno_in_image:
-                category = anno["category_id"]
-                bbox_COCO = anno["bbox"]
-                x, y, w, h = convert_bbox_coco2yolo(img_width, img_height, bbox_COCO)
-                f.write(f"{category} {x:.6f} {y:.6f} {w:.6f} {h:.6f}\n")
-
-    return output_path
-
-
 def get_experiment():
-    if 'api_token' not in os.environ:
+    if "api_token" not in os.environ:
         raise Exception("You must set an api_token to run this image")
     api_token = os.environ["api_token"]
 
@@ -110,11 +23,7 @@ def get_experiment():
     else:
         organization_id = os.environ["organization_id"]
 
-    client = Client(
-        api_token=api_token,
-        host=host,
-        organization_id=organization_id
-    )
+    client = Client(api_token=api_token, host=host, organization_id=organization_id)
 
     if "experiment_name" in os.environ:
         experiment_name = os.environ["experiment_name"]
@@ -126,103 +35,137 @@ def get_experiment():
             project = client.get_project(project_name)
         experiment = project.get_experiment(experiment_name)
     else:
-        raise Exception("You must set the project_token or project_name and experiment_name")
+        raise Exception(
+            "You must set the project_token or project_name and experiment_name"
+        )
     return experiment
 
 
-def convert_test_results(test_results):
-    """convert test results to be logged
+def get_labelmap(experiment: Experiment):
+    labels = experiment.get_dataset("train").list_labels()
+    label_names = [label.name for label in labels]
+    labelmap = {str(i): label.name for i, label in enumerate(labels)}
+    return label_names, labelmap
 
-    Args:
-        test_results (dict): test results 
 
-    Returns:
-        test_results: converted test results 
-    """
-    test_results['PPYoloELoss/loss_cls'] = float(test_results['PPYoloELoss/loss_cls'])
-    test_results['PPYoloELoss/loss_iou'] = float(test_results['PPYoloELoss/loss_iou'])
-    test_results['PPYoloELoss/loss_dfl'] = float(test_results['PPYoloELoss/loss_dfl'])
-    test_results['PPYoloELoss/loss'] = float(test_results['PPYoloELoss/loss'])
-    test_results['Precision@0.50'] = torch.Tensor.item(test_results['Precision@0.50'])
-    test_results['Recall@0.50'] = torch.Tensor.item(test_results['Recall@0.50'])
-    test_results['mAP@0.50'] = torch.Tensor.item(test_results['mAP@0.50'])
-    test_results['F1@0.50'] = torch.Tensor.item(test_results['F1@0.50'])
+def format_test_results(test_results):
+    test_results["PPYoloELoss/loss_cls"] = float(test_results["PPYoloELoss/loss_cls"])
+    test_results["PPYoloELoss/loss_iou"] = float(test_results["PPYoloELoss/loss_iou"])
+    test_results["PPYoloELoss/loss_dfl"] = float(test_results["PPYoloELoss/loss_dfl"])
+    test_results["PPYoloELoss/loss"] = float(test_results["PPYoloELoss/loss"])
+    test_results["Precision@0.50"] = torch.Tensor.item(test_results["Precision@0.50"])
+    test_results["Recall@0.50"] = torch.Tensor.item(test_results["Recall@0.50"])
+    test_results["mAP@0.50"] = torch.Tensor.item(test_results["mAP@0.50"])
+    test_results["F1@0.50"] = torch.Tensor.item(test_results["F1@0.50"])
 
     return test_results
 
 
-def get_yolo_dataset(experiment, cwd):
-    """download the dataset in coco format and convert it to yolo 
-        save new data under cwd in new directory with the experiment's name 
-
-    Args:
-        experiment: current experiment
-        cwd: current working directory
-
-    Returns:
-        path_dict (dict): dictionary containing relevant paths for the dataset 
-        labelmap (list): current labelmap 
-        label_names (dict): list of labels 
-    """
-
+def create_yolo_dataset(experiment: Experiment, cwd):
     path_dict = {}
-    for data_type in ['train', 'test', 'val']:
+    for data_type in ["train", "test", "val"]:
         path_dict[data_type] = {}
-        path_dict[data_type]['dataset_dir'] = os.path.join(cwd, experiment.base_dir, data_type, 'images')
+        path_dict[data_type]["dataset_dir"] = os.path.join(
+            cwd, experiment.base_dir, data_type, "images"
+        )
 
         assets = experiment.get_dataset(data_type)
-        assets.download(path_dict[data_type]['dataset_dir'])
+        assets.download(path_dict[data_type]["dataset_dir"])
 
         # get the coco annotation from picsellia
-        path_dict[data_type]['coco_annotation_path'] = assets.export_annotation_file(AnnotationFileType.COCO,
-                                                                                    target_path=path_dict[data_type][
-                                                                                        'dataset_dir'])
+        path_dict[data_type]["coco_annotation_path"] = assets.export_annotation_file(
+            AnnotationFileType.COCO, target_path=path_dict[data_type]["dataset_dir"]
+        )
 
         # convert coco to yolo
-        path_dict[data_type]['yolo_annotation_path'] = convert_coco_json_to_yolo_txt(
-            output_path=os.path.join(experiment.base_dir, data_type, 'labels'),
-            json_file=path_dict[data_type]['coco_annotation_path'])
+        path_dict[data_type]["yolo_annotation_path"] = convert_coco_json_to_yolo_txt(
+            output_path=os.path.join(experiment.base_dir, data_type, "labels"),
+            json_file=path_dict[data_type]["coco_annotation_path"],
+        )
 
-        if data_type == 'train':
-            labels = assets.list_labels()
-            label_names = [label.name for label in labels]
-            labelmap = {str(i): label.name for i, label in enumerate(labels)}
-
-    print("Converting COCO to YOLO finished!")
-    return path_dict, labelmap, label_names
+    return path_dict
 
 
-def get_asset_predictions(experiment, model, asset, conf):
-    """
-        Get prediction's bouding boxes of one asset
+def convert_coco_json_to_yolo_txt(output_path: str, json_file: str):
+    path = make_folders(output_path)
 
-    Args:
-        experiment (_type_): 
-        model: model to make inference with 
-        asset: asset to make predictions on 
-        conf (float): confidence score threshold below which bbox predictions are ignored 
+    with open(json_file) as f:
+        json_data = json.load(f)
 
-    Returns:
-        bbox_list (list): list containing the bounding boxes' info
-        [List[Tuple[List[List[int]], Label, float]]]
-    """
+    for image in tqdm(json_data["images"], desc="Annotation txt for each image"):
+        annotations = [
+            annotation
+            for annotation in json_data["annotations"]
+            if annotation["image_id"] == image["id"]
+        ]
+        annotation_txt = os.path.join(
+            output_path, image["file_name"].split(".")[0] + ".txt"
+        )
+        with open(annotation_txt, "w") as f:
+            for annotation in annotations:
+                category = annotation["category_id"]
+                x, y, w, h = convert_bbox_coco2yolo(
+                    image["width"], image["height"], annotation["bbox"]
+                )
+                f.write(f"{category} {x:.6f} {y:.6f} {w:.6f} {h:.6f}\n")
 
-    labels = experiment.get_dataset("test").list_labels()
-    image_path = os.path.join(experiment.base_dir, "test", "images", asset.filename)
-    predictions = model.predict(image_path,  conf=0.1)
+    return output_path
+
+
+def make_folders(path="output"):
+    if os.path.exists(path):
+        shutil.rmtree(path)
+    os.makedirs(path)
+    return path
+
+
+def convert_bbox_coco2yolo(img_width, img_height, bbox):
+    # YOLO bounding box format: [x_center, y_center, width, height]
+    # (float values relative to width and height of image)
+    x_tl, y_tl, w, h = bbox
+
+    x_center = x_tl + w / 2.0
+    y_center = y_tl + h / 2.0
+
+    x = x_center / img_width
+    y = y_center / img_height
+    w = w / img_width
+    h = h / img_height
+
+    return [x, y, w, h]
+
+
+def get_asset_predictions(
+    experiment: Experiment, model, asset, conf_threshold, dataset_type: str
+):
+    experiment_labels = experiment.get_dataset(dataset_type).list_labels()
+    image_path = os.path.join(
+        experiment.base_dir, dataset_type, "images", asset.filename
+    )
+    predictions = model.predict(image_path, conf=conf_threshold)
     bbox_list = []
     for image_prediction in predictions:
-        class_names = image_prediction.class_names
-        class_ids = image_prediction.prediction.labels
-        confidence = image_prediction.prediction.confidence
-        bboxes = image_prediction.prediction.bboxes_xyxy
-
-        for i, (label, conf, bbox) in enumerate(zip(class_ids, confidence, bboxes)):
+        for i, (label, conf, bbox) in enumerate(
+            zip(
+                image_prediction.prediction.labels,
+                image_prediction.prediction.confidence,
+                image_prediction.prediction.bboxes_xyxy,
+            )
+        ):
             for i, coord in enumerate(bbox):
                 if coord < 0:
                     bbox[i] = 0
             width = bbox[2] - bbox[0]
             height = bbox[3] - bbox[1]
-            bbox_list.append((int(bbox[0]), int(bbox[1]), int(width), int(height), labels[int(label)], float(conf)))
-    
-    return bbox_list 
+            bbox_list.append(
+                (
+                    int(bbox[0]),
+                    int(bbox[1]),
+                    int(width),
+                    int(height),
+                    experiment_labels[int(label)],
+                    float(conf),
+                )
+            )
+
+    return bbox_list
