d0b5bd31b30640e483eba1835cecbd42c8947e7c
SoniaGrh <sonia.garrouch@hotmail.fr>
2023-05-25 11:21:02 +0200

    define all methods as public + labelmap as param

--- evaluator/framework_formatter.py
+++ evaluator/framework_formatter.py
@@ -1,62 +1,53 @@
 from abc import ABC, abstractmethod
 from typing import List
 
-from evaluator.utils import (
-    cast_type_list_to_float,
-    cast_type_list_to_int,
-    convert_tensor_to_list,
-    rescale_normalized_box,
-)
+from evaluator.utils import (cast_type_list_to_float, cast_type_list_to_int,
+                             convert_tensor_to_list, rescale_normalized_box)
 from picsellia.sdk.asset import Asset
 
 
 class FrameworkFormatter(ABC):
-    @classmethod
+    def __init__(self, labelmap) -> None:
+        self._labelmap = labelmap
+
     @abstractmethod
-    def _format_classes(cls, prediction, labelmap) -> List[int]:
+    def format_classes(self, prediction) -> List[int]:
         pass
 
-    @classmethod
     @abstractmethod
-    def _format_confidences(cls, prediction) -> List[float]:
+    def format_confidences(self, prediction) -> List[float]:
         pass
 
-    @classmethod
     @abstractmethod
-    def _format_boxes(cls, asset: Asset, prediction) -> List[int]:
+    def format_boxes(self, asset: Asset, prediction) -> List[int]:
         pass
 
-    @classmethod
     @abstractmethod
-    def _format_polygons(cls, prediction):
+    def format_polygons(self, prediction):
         pass
 
 
 class YoloFormatter(FrameworkFormatter):
-    @classmethod
-    def _format_confidences(cls, prediction) -> List[float]:
+    def format_confidences(self, prediction) -> List[float]:
         if not prediction.boxes:
             return []
-            print("test classification task et si y a pas alors on renvoie []")
         confidences_list = convert_tensor_to_list(tensor=prediction.boxes.conf)
         casted_confidences = cast_type_list_to_float(_list=confidences_list)
         return casted_confidences
 
-    @classmethod
-    def _format_classes(cls, prediction, labelmap) -> List[int]:
+    def format_classes(self, prediction) -> List[int]:
         if not prediction.boxes:
             return []
-            print("test classification task et si y a pas alors on renvoie []")
         classes_list = convert_tensor_to_list(tensor=prediction.boxes.cls)
         casted_classes = cast_type_list_to_int(_list=classes_list)
-        picsellia_labels = list(map(lambda label: labelmap[label], casted_classes))
+        picsellia_labels = list(
+            map(lambda label: self._labelmap[label], casted_classes)
+        )
         return picsellia_labels
 
-    @classmethod
-    def _format_boxes(cls, asset: Asset, prediction) -> List[int]:
+    def format_boxes(self, asset: Asset, prediction) -> List[int]:
         if not prediction.boxes:
             return []
-
         normalized_boxes = prediction.boxes.xyxyn
         boxes_list = convert_tensor_to_list(tensor=normalized_boxes)
         rescaled_boxes = list(
@@ -70,8 +61,7 @@ class YoloFormatter(FrameworkFormatter):
         casted_boxes = list(map(cast_type_list_to_int, rescaled_boxes))
         return casted_boxes
 
-    @classmethod
-    def _format_polygons(cls, prediction):
+    def format_polygons(self, prediction):
         if prediction.masks is not None:
             polygons = prediction.masks.xy
             casted_polygons = list(map(lambda polygon: polygon.astype(int), polygons))
