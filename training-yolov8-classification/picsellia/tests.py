041805f639d3a4759c8b4ca0fdd753e5f205798d
hajeressef <hajer.essefi@picsellia.com>
2023-08-31 17:09:07 +0200

    add tests

--- training-yolov8-classification/picsellia/tests.py
+++ training-yolov8-classification/picsellia/tests.py
@@ -1,4 +1,6 @@
 import os
+import numpy as np
+import shutil
 import unittest
 from utils import (
     get_prop_parameter,
@@ -7,9 +9,13 @@ from utils import (
     _get_three_attached_datasets,
     _create_coco_objects,
     _create_class_directories,
-    _move_files_in_class_directories,
     get_image_annotation,
+    _transform_two_attached_datasets_to_three,
+    get_train_test_eval_datasets_from_experiment,
+    format_confusion_matrix,
+    order_repartition_according_labelmap,
 )
+from helpers import Yolov8ClassificationTrainer
 from picsellia import Client
 from datetime import date
 import time
@@ -18,7 +24,7 @@ TOKEN = os.environ["TEST_TOKEN"]
 ORGA_NAME = os.environ["TEST_ORGA"]
 
 
-class TestYoloClassification(unittest.TestCase):
+class TestYoloClassificationUtils(unittest.TestCase):
     eval_set = None
     test_set = None
     train_set = None
@@ -28,6 +34,7 @@ class TestYoloClassification(unittest.TestCase):
     client = None
     organization_name: str
     token: str
+    labelmap = {"0": "covid", "1": "normal", "2": "pneumonia"}
 
     @classmethod
     def setUpClass(cls) -> None:
@@ -37,7 +44,7 @@ class TestYoloClassification(unittest.TestCase):
             api_token=cls.token, organization_name=cls.organization_name
         )
         cls.project = cls.client.create_project(
-            name=f"test_yolo_classif{str(date.today())}-{str(time.time())}"
+            name=f"test_yolo_classif-utils{str(date.today())}-{str(time.time())}"
         )
         cls.model_version = cls.client.get_public_model(
             "yolov8-classification"
@@ -68,6 +75,9 @@ class TestYoloClassification(unittest.TestCase):
     @classmethod
     def tearDownClass(cls) -> None:
         cls.project.delete()
+        sample_file_path = "data/test/grape_image.jpeg"
+        if os.path.isfile(sample_file_path):
+            shutil.move(sample_file_path, "test_files")
 
     def test_get_prop_parameters(self):
         parameters = {"epochs": 100, "patience": 500, "image_size": 640}
@@ -90,7 +100,6 @@ class TestYoloClassification(unittest.TestCase):
             is_created = True
         self.assertTrue(is_created)
 
-    def test_move_image(self):
         move_image(
             filename="grape_image.jpeg",
             old_location_path="test_files",
@@ -128,6 +137,124 @@ class TestYoloClassification(unittest.TestCase):
         expected_cat = {"id": 0, "name": "covid", "supercategory": None}
 
         self.assertEqual(expected_cat, cat)
+
+    def test_get_three_attached_datasets(self):
+        train_set, test_set, eval_test = _get_three_attached_datasets(self.experiment)
+        self.assertEqual(
+            (self.train_set, self.test_set, self.eval_set),
+            (train_set, test_set, eval_test),
+        )
+
+    def test_transform_two_attached_datasets_to_three(self):
+        train_set, test_set, eval_set = _transform_two_attached_datasets_to_three(
+            experiment=self.experiment
+        )
+        self.assertEqual(
+            (self.train_set, self.test_set, self.test_set),
+            (train_set, test_set, eval_set),
+        )
+
+    def test_get_train_eval_datasets_from_experiment(self):
+        results = get_train_test_eval_datasets_from_experiment(self.experiment)
+        expected_results = (False, True, self.train_set, self.test_set, self.eval_set)
+        self.assertEqual(expected_results, results)
+
+    def test_format_confusion_matrix(self):
+        matrix = np.array([[60, 0, 0], [46, 0, 0], [39, 0, 0]])
+        formatted_matrix = format_confusion_matrix(
+            labelmap=self.labelmap, matrix=matrix
+        )
+        expected_matrix = {
+            "categories": ["covid", "normal", "pneumonia"],
+            "values": [[60, 0, 0], [46, 0, 0], [39, 0, 0]],
+        }
+        self.assertEqual(expected_matrix, formatted_matrix)
+
+    def test_order_repartition_according_labelmap(self):
+        repartition = {"x": ["covid", "pneumonia", "normal"], "y": [12, 11, 7]}
+        ordered_repartition = order_repartition_according_labelmap(
+            labelmap=self.labelmap,
+            repartition=repartition,
+        )
+        expected_results = {"x": ["covid", "normal", "pneumonia"], "y": [12, 7, 11]}
+
+        self.assertEqual(expected_results, ordered_repartition)
+
+
+class TestYoloClassificationHelpers(unittest.TestCase):
+    model_version = None
+    dataset = None
+    organization_name: str
+    token: str
+    experiment = None
+    client = None
+    project = None
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.token = TOKEN
+        cls.organization_name = ORGA_NAME
+        cls.client = Client(
+            api_token=cls.token, organization_name=cls.organization_name
+        )
+        cls.project = cls.client.create_project(
+            name=f"test_yolo_classif-helpers{str(date.today())}-{str(time.time())}"
+        )
+        cls.model_version = cls.client.get_public_model(
+            "yolov8-classification"
+        ).get_version(0)
+        cls.experiment = cls.project.create_experiment(name="yolo-triple-dataset")
+        cls.dataset = cls.client.get_dataset_by_id(
+            "01888c1b-cfb6-768b-83dd-2e1c460e79cf"
+        )
+        cls.experiment.attach_dataset(
+            name="train", dataset_version=cls.dataset.get_version("train")
+        )
+        cls.experiment.log_parameters({"epochs": 1, "patience": 500, "image_size": 640})
+        cls.experiment.attach_model_version(
+            model_version=cls.model_version, do_attach_base_parameters=False
+        )
+
+        os.environ["experiment_id"] = str(cls.experiment.id)
+
+        cls.training_pipeline = Yolov8ClassificationTrainer()
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        pass
+        cls.project.delete()
+        shutil.rmtree("data")
+        shutil.rmtree("images")
+        shutil.rmtree("train")
+        os.remove("data.zip")
+        os.remove("last.pt")
+        os.remove("yolov8x-cls.pt")
+
+    def test_preprocess_train_test(self):
+        self.training_pipeline.prepare_data_for_training()
+        self.assertTrue(
+            len(os.listdir(self.training_pipeline.val_folder_path)),
+            len(self.training_pipeline.labelmap),
+        )
+
+        self.training_pipeline.train()
+        self.assertTrue(os.path.isfile(self.training_pipeline.weights_path))
+        self.assertTrue(
+            os.path.isfile(
+                os.path.join(self.training_pipeline.weights_dir_path, "last.onnx")
+            )
+        )
+
+        self.training_pipeline.test()
+        self.assertTrue(
+            type(self.training_pipeline.experiment.get_log("confusion").data) == dict
+        )
+
+        self.training_pipeline.eval()
+        evaluation_results = self.client.connexion.get(
+            f"/sdk/experiment/{str(self.experiment.id)}/evaluations"
+        ).json()
+        self.assertGreater(evaluation_results["count"], 0)
 
 
 if __name__ == "__main__":
