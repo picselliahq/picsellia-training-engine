3d3a1543771c016a1b13e4e90f6db25cfecf0e61
hajeressef <hajer.essefi@picsellia.com>
2023-09-04 11:29:21 +0200

    change logged loss name to test

--- training-yolov8-classification/picsellia/main.py
+++ training-yolov8-classification/picsellia/main.py
@@ -15,7 +15,7 @@ experiment = training_pipeline.experiment
 def on_train_epoch_end(trainer):
     metrics = trainer.metrics
     experiment.log("accuracy", float(metrics["metrics/accuracy_top1"]), "line")
-    experiment.log("val/loss", float(metrics["val/loss"]), "line")
+    experiment.log("test/loss", float(metrics["test/loss"]), "line")
 
 
 training_pipeline.model.add_callback("on_train_epoch_end", on_train_epoch_end)
