948cf1607bedb5b87629bd1dfbbb2df05cb8d74c
hajeressef <hajer.essefi@picsellia.com>
2023-09-08 18:34:59 +0200

    first draft of unet training script

---
+++ training-unet-instance-segmentation/picsellia/main.py
@@ -0,0 +1,28 @@
+import os
+import logging
+import keras
+from trainer import UnetSegmentationTrainer
+from picsellia.types.enums import LogType
+
+
+os.environ["PICSELLIA_SDK_CUSTOM_LOGGING"] = "True"
+os.environ["PICSELLIA_SDK_DOWNLOAD_BAR_MODE"] = "2"
+logging.getLogger("picsellia").setLevel(logging.INFO)
+
+training_pipeline = UnetSegmentationTrainer()
+training_pipeline.prepare_data_for_training()
+experiment = training_pipeline.experiment
+
+
+class LogTrainingMetrics(keras.callbacks.Callback):
+    def on_epoch_end(self, epoch, logs=None):
+        print("logs on_epoch_end: ", logs)
+        for metric_name in logs.keys():
+            experiment.log(
+                name=metric_name, type=LogType.LINE, data=float(logs[metric_name])
+            )
+
+
+training_pipeline.callbacks.append(LogTrainingMetrics())
+training_pipeline.train()
+training_pipeline.eval()
