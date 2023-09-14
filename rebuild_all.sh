694bd9b25370f666e9ee73197e100b5676de650a
hajeressef <hajer.essefi@picsellia.com>
2023-09-14 11:41:32 +0200

    add build and push commands for unet

--- rebuild_all.sh
+++ rebuild_all.sh
@@ -24,3 +24,6 @@ docker push picsellia/training-yolov8-detection:latest
 
 docker build . -f yolov8-segmentation/Dockerfile -t picsellia/training-yolov8-segmentation:latest
 docker push picsellia/training-yolov8-segmentation:latest
+
+docker build . -f unet-instance-segmentation/Dockerfile -t picsellia/training-unet-segmentation:latest
+docker push picsellia/training-unet-segmentation:latest
