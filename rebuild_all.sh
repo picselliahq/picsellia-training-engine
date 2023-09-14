611e48242897f75dd15af4073d3f39821a6395a5
hajeressef <131779194+hajeressef@users.noreply.github.com>
2023-09-14 16:04:18 +0200

    Feat/refactor new yolov8 classif (#22)
    * add test file
    
    * rename yolov8 classif folders
    
    * change workdir
    
    * update rebuild_all shell script
    
    * add build and push commands for unet
    
    * update docker images
    
    * fix/yolov8 detection package name
    
    * add unet notebook
    
    * copy abstract_trainer in dockerfile
    
    * remove pip install picsellia fro mbase image
    
    ---------
    
    Co-authored-by: PN-picsell <pierre-nicolas@picsellia.com>

--- rebuild_all.sh
+++ rebuild_all.sh
@@ -4,23 +4,26 @@ docker push picsellia/cuda:11.4.3-cudnn8-ubuntu20.04
 docker build . -f base/11.7.1-cuda-cudnn8/Dockerfile -t picsellia/cuda:11.7.1-cudnn8-ubuntu20.04
 docker push picsellia/cuda:11.7.1-cudnn8-ubuntu20.04
 
-docker build . -f captured-training-tf2/Dockerfile -t picsellia/training-tf2:latest
+docker build . -f tf2/Dockerfile -t picsellia/training-tf2:latest
 docker push picsellia/training-tf2:latest
 
-docker build . -f captured-classification-keras/Dockerfile -t picsellia/training-keras-classification:latest
+docker build . -f keras-classification/Dockerfile -t picsellia/training-keras-classification:latest
 docker push picsellia/training-keras-classification:latest
 
-docker build . -f captured-training-yolov5-detection/Dockerfile -t picsellia/training-yolov5-detection:latest
+docker build . -f yolov5-detection/Dockerfile -t picsellia/training-yolov5-detection:latest
 docker push picsellia/training-yolov5-detection:latest
 
-docker build . -f captured-training-yolov5-segmentation/Dockerfile -t picsellia/training-yolov5-segmentation:latest
+docker build . -f yolov5-segmentation/Dockerfile -t picsellia/training-yolov5-segmentation:latest
 docker push picsellia/training-yolov5-segmentation:latest
 
-docker build . -f captured-training-yolov8-classification/Dockerfile -t picsellia/training-yolov8-classification:latest
+docker build . -f yolov8-classification/Dockerfile -t picsellia/training-yolov8-classification:latest
 docker push picsellia/training-yolov8-classification:latest
 
-docker build . -f captured-training-yolov8-detection/Dockerfile -t picsellia/training-yolov8-detection:latest
+docker build . -f yolov8-detection/Dockerfile -t picsellia/training-yolov8-detection:latest
 docker push picsellia/training-yolov8-detection:latest
 
-docker build . -f captured-training-yolov8-segmentation/Dockerfile -t picsellia/training-yolov8-segmentation:latest
+docker build . -f yolov8-segmentation/Dockerfile -t picsellia/training-yolov8-segmentation:latest
 docker push picsellia/training-yolov8-segmentation:latest
+
+docker build . -f unet-instance-segmentation/Dockerfile -t picsellia/training-unet-segmentation:latest
+docker push picsellia/training-unet-segmentation:latest
