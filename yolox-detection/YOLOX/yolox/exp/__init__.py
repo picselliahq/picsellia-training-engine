2a5e83e4d860727943f832cec37d57991d7e1193
picsalex <132259399+picsalex@users.noreply.github.com>
2023-12-29 15:19:43 +0100

    feat: added yolox

---
+++ yolox-detection/YOLOX/yolox/exp/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+# Copyright (c) Megvii Inc. All rights reserved.
+
+from .base_exp import BaseExp  # noqa
+from .build import get_exp, get_exp_by_name  # noqa
+from .yolox_base import Exp, check_exp_value  # noqa
