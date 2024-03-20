ea7ba4f8c0c412f1c8d76dda57b760802ba3a5ed
picsalex <132259399+picsalex@users.noreply.github.com>
2024-03-20 11:20:41 +0100

    fix: PR comments

--- poc/models/contexts/picsellia_context.py
+++ poc/models/contexts/picsellia_context.py
@@ -1,7 +1,7 @@
 import os
 from abc import ABC, abstractmethod
 from collections import defaultdict
-from typing import Type
+from typing import Type, Optional, Any
 
 import picsellia
 from picsellia import Experiment
@@ -12,7 +12,11 @@ from poc.models.parameters.hyper_parameters import HyperParameters
 
 class PicselliaContext(ABC):
     def __init__(
-        self, api_token=None, host=None, organization_id=None, organization_name=None
+        self,
+        api_token: Optional[str] = None,
+        host: Optional[str] = None,
+        organization_id: Optional[str] = None,
+        organization_name: Optional[str] = None,
     ):
         self.api_token = api_token or os.getenv("api_token")
 
@@ -35,6 +39,42 @@ class PicselliaContext(ABC):
             organization_name=self.organization_name,
         )
 
+    def _format_parameter_with_color_and_suffix(
+        self, value: Any, key: str, defaulted_keys: set
+    ) -> str:
+        """
+        Formats a given value with ANSI color codes and a suffix indicating whether it is a default value.
+
+        Args:
+            value: The value to be formatted. This could be of any type but is converted to a string.
+            key: The key associated with the value. Used to determine if the value is defaulted.
+            defaulted_keys: A set of keys that are considered to have default values.
+
+        Returns:
+            The value formatted as a string with appropriate color coding and suffix.
+        """
+        suffix = " (default)" if key in defaulted_keys else ""
+        color_code = "\033[33m" if suffix else "\033[34m"
+        return f"{color_code}{value}\033[0m{suffix}"
+
+    def _process_parameters(self, parameters_dict: dict, defaulted_keys: set) -> dict:
+        """
+        Processes parameters by applying color coding and suffixes to their values based on whether they are default.
+
+        Args:
+            parameters_dict: The dictionary of parameters to process.
+            defaulted_keys: A set of parameter keys that are considered to have default values.
+
+        Returns:
+            A dictionary of processed parameters with color coding and suffixes applied.
+        """
+        processed_params = {}
+        for key, value in parameters_dict.items():
+            processed_params[key] = self._format_parameter_with_color_and_suffix(
+                value, key, defaulted_keys
+            )
+        return processed_params
+
     @abstractmethod
     def to_dict(self):
         pass
@@ -45,10 +85,10 @@ class PicselliaTrainingContext(PicselliaContext):
         self,
         hyperparameters_cls: Type[HyperParameters],
         augmentation_parameters_cls: Type[AugmentationParameters],
-        api_token=None,
-        host=None,
-        organization_id=None,
-        experiment_id=None,
+        api_token: Optional[str] = None,
+        host: Optional[str] = None,
+        organization_id: Optional[str] = None,
+        experiment_id: Optional[str] = None,
     ):
         super().__init__(api_token, host, organization_id)
         self.experiment_id = experiment_id or os.getenv("experiment_id")
@@ -67,11 +107,10 @@ class PicselliaTrainingContext(PicselliaContext):
             log_data=parameters_log_data
         )
 
-    def _initialize_experiment(self) -> Experiment:
-        return self.client.get_experiment_by_id(self.experiment_id)
-
-    def to_dict(self):
+    def to_dict(self) -> dict:
         result = defaultdict(dict)
+
+        # Context Parameters
         result["context_parameters"] = {
             "host": self.host,
             "organization_id": self.organization_id,
@@ -79,28 +118,29 @@ class PicselliaTrainingContext(PicselliaContext):
             "experiment_id": self.experiment_id,
         }
 
-        for key, value in self.hyperparameters.to_dict().items():
-            suffix = " (default)" if key in self.hyperparameters.defaulted_keys else ""
-            color_code = "\033[33m" if suffix else "\033[34m"
-            result["hyperparameters"][key] = f"{color_code}{value}\033[0m{suffix}"
-
-            # Augmentation Parameters
-        for key, value in self.augmentation_parameters.to_dict().items():
-            suffix = (
-                " (default)"
-                if key in self.augmentation_parameters.defaulted_keys
-                else ""
-            )
-            color_code = "\033[33m" if suffix else "\033[34m"
-            result["augmentation_parameters"][
-                key
-            ] = f"{color_code}{value}\033[0m{suffix}"
+        # Process Hyperparameters and Augmentation Parameters
+        result["hyperparameters"] = self._process_parameters(
+            self.hyperparameters.to_dict(), self.hyperparameters.defaulted_keys
+        )
+        result["augmentation_parameters"] = self._process_parameters(
+            self.augmentation_parameters.to_dict(),
+            self.augmentation_parameters.defaulted_keys,
+        )
 
         return result
 
+    def _initialize_experiment(self) -> Experiment:
+        return self.client.get_experiment_by_id(self.experiment_id)
+
 
 class PicselliaProcessingContext(PicselliaContext):
-    def __init__(self, api_token=None, host=None, organization_id=None, job_id=None):
+    def __init__(
+        self,
+        api_token: Optional[str] = None,
+        host: Optional[str] = None,
+        organization_id: Optional[str] = None,
+        job_id: Optional[str] = None,
+    ):
         # Initialize the Picsellia client from the base class
         super().__init__(api_token, host, organization_id)
 
@@ -124,17 +164,20 @@ class PicselliaProcessingContext(PicselliaContext):
         context = self.job.sync()["dataset_version_processing_job"]
 
         self.model_version_id = context.get("model_version_id")
-        self.dataset_version_id = context.get("input_dataset_version_id")
+        self.input_dataset_version_id = context.get("input_dataset_version_id")
+        self.output_dataset_version_id = context.get("output_dataset_version_id")
 
-        if not self.model_version_id or not self.dataset_version_id:
+        if not self.model_version_id or not self.input_dataset_version_id:
             raise ValueError(
                 "Failed to retrieve necessary context from the job. Please ensure the job is correctly configured."
             )
         return context
 
     def to_dict(self):
-        return {
+        result = defaultdict(dict)
+        result["context_parameters"] = {
             "host": self.host,
             "organization_id": self.organization_id,
             "job_id": self.job_id,
         }
+        return result
