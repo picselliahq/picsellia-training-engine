cb67cf93969054041d3fcc3d326098d7bc209aab
SoniaGrh <sonia.garrouch@hotmail.fr>
2024-03-22 17:01:01 +0100

    feat: create general data extractor

---
+++ src/steps/data_extraction/data_extractors.py
@@ -0,0 +1,16 @@
+from src import Pipeline
+from src import step
+from src.models.contexts.picsellia_context import PicselliaTrainingContext
+from src.steps.data_extraction.utils.dataset_handler import DatasetHandler
+
+
+@step
+def data_extractor():
+    context: PicselliaTrainingContext = Pipeline.get_active_context()
+    dataset_handler = DatasetHandler(
+        experiment=context.experiment,
+        prop_train_split=context.hyperparameters.prop_train_split,
+    )
+    dataset_collection = dataset_handler.get_dataset_collection()
+    dataset_collection.download()
+    return dataset_collection
