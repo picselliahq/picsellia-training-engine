c14ffed4c69c1b91ce521f0db1ae0be6476013c9
SoniaGrh <sonia.garrouch@hotmail.fr>
2024-03-22 16:58:18 +0100

    feat: implement classif dataset organizer

--- src/models/dataset/dataset_organizer.py
+++ src/models/dataset/dataset_organizer.py
@@ -1,33 +1,42 @@
 import os
 import shutil
 
+from src.steps.data_extraction.utils.dataset_context import DatasetContext
 
-class DatasetOrganizer:
-    def __init__(self, coco_file, images_dir, dest_dir):
-        self.coco_file = coco_file
-        self.images_dir = images_dir
-        self.dest_dir = dest_dir
+
+class ClassificationDatasetOrganizer:
+    def __init__(self, dataset_context: DatasetContext):
+        self.dataset_context = dataset_context
+        self.destination_dir = dataset_context.dataset_extraction_path
 
     def organize(self):
         categories = {
-            category.id: category.name for category in self.coco_file.categories
+            category.id: category.name
+            for category in self.dataset_context.coco_file.categories
         }
         image_categories = {}
-        for annotation in self.coco_file.annotations:
+        for annotation in self.dataset_context.coco_file.annotations:
             image_id = annotation.image_id
             category_id = annotation.category_id
             image_categories[image_id] = category_id
 
-        for image in self.coco_file.images:
+        for image in self.dataset_context.coco_file.images:
             image_id = image.id
             if image_id in image_categories:
                 category_id = image_categories[image_id]
                 category_name = categories[category_id]
 
-                category_dir = os.path.join(self.dest_dir, category_name)
+                category_dir = os.path.join(self.destination_dir, category_name)
                 if not os.path.exists(category_dir):
                     os.makedirs(category_dir)
 
-                src_image_path = os.path.join(self.images_dir, image.file_name)
+                src_image_path = os.path.join(
+                    self.dataset_context.image_dir, image.file_name
+                )
                 dest_image_path = os.path.join(category_dir, image.file_name)
                 shutil.copy(src_image_path, dest_image_path)
+
+        shutil.rmtree(self.dataset_context.image_dir)
+
+        self.dataset_context.image_dir = self.dataset_context.dataset_extraction_path
+        return self.dataset_context
