598fc9c6919a1a379e39a9aebb3f70c5cee06a61
SoniaGrh <sonia.garrouch@hotmail.fr>
2024-03-26 18:44:43 +0100

    refactor: type parameters

--- tests/steps/fixtures/initialize_integration_tests_fixtures.py
+++ tests/steps/fixtures/initialize_integration_tests_fixtures.py
@@ -2,63 +2,44 @@ import os
 import sys
 
 import pytest
-from picsellia import Client
+from picsellia import Client, Dataset, DatasetVersion, Data
+from picsellia.types.enums import InferenceType
+from picsellia.sdk.asset import MultiAsset
 
 
-def get_images_path(dataset_name: str):
-    return os.path.join(
-        os.getcwd(),
-        "tests",
-        "data",
-        "classification",
-        "Tyre_Quality_Classification",
-        f"pytest-{dataset_name}",
-        "images",
-    )
-
-
-def get_annotations_path(dataset_name: str):
-    return os.path.join(
-        os.getcwd(),
-        "tests",
-        "data",
-        "classification",
-        "Tyre_Quality_Classification",
-        f"pytest-{dataset_name}",
-        "annotations",
-        "coco.json",
-    )
-
-
-def get_multi_asset(dataset_version):
+def get_multi_asset(dataset_version: DatasetVersion) -> MultiAsset:
     return dataset_version.list_assets()
 
 
-def get_labelmap(dataset_version):
+def get_labelmap(dataset_version: DatasetVersion) -> dict:
     return {label.name: label for label in dataset_version.list_labels()}
 
 
 def create_dataset_version(
-    dataset, version_name, dataset_type, uploaded_data, annotations_path
-):
-    dataset_version = dataset.create_version(version_name, type=dataset_type)
-    dataset_version.add_data(uploaded_data).wait_for_done()
+    dataset: Dataset,
+    version_name: str,
+    dataset_type: InferenceType,
+    uploaded_data: Data,
+    annotations_path: str,
+) -> DatasetVersion:
+    dataset_version = dataset.create_version(version=version_name, type=dataset_type)
+    dataset_version.add_data(data=uploaded_data).wait_for_done()
     dataset_version.import_annotations_coco_file(file_path=annotations_path)
 
     return dataset_version
 
 
-def upload_data(picsellia_client, images_path):
+def upload_data(picsellia_client: Client, images_path: str) -> Data:
     datalake = picsellia_client.get_datalake()
 
     files = [os.path.join(images_path, file) for file in os.listdir(images_path)]
-    data = datalake.upload_data(files)
+    data = datalake.upload_data(filepaths=files)
 
     return data
 
 
 @pytest.fixture
-def picsellia_client():
+def picsellia_client() -> Client:
     try:
         TOKEN = os.environ["PICSELLIA_TEST_TOKEN"]
         HOST = os.environ["PICSELLIA_TEST_HOST"]
@@ -77,10 +58,10 @@ def picsellia_client():
 
 
 @pytest.fixture
-def destination_path():
+def destination_path() -> str:
     return os.path.join(os.getcwd(), "tests", "destination_path")
 
 
 @pytest.fixture
-def dataset(picsellia_client):
+def dataset(picsellia_client: Client) -> Dataset:
     return picsellia_client.create_dataset("test-picsellia-training-engine")
