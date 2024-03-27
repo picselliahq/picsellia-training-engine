3b9e79dc270af01b1889def22f8f7b572556013a
SoniaGrh <sonia.garrouch@hotmail.fr>
2024-03-27 09:52:36 +0100

    refacto: create smaller functions

--- src/models/dataset/dataset_organizer.py
+++ src/models/dataset/dataset_organizer.py
@@ -1,42 +1,66 @@
 import os
 import shutil
-
+from typing import Dict
 from src.steps.data_extraction.utils.dataset_context import DatasetContext
+
+
+class FileSystemOperations:
+    @staticmethod
+    def create_directory(path: str):
+        if not os.path.exists(path):
+            os.makedirs(path)
+
+    @staticmethod
+    def copy_file(source_path: str, destination_path: str):
+        shutil.copy(source_path, destination_path)
+
+    @staticmethod
+    def remove_directory(path: str):
+        shutil.rmtree(path)
 
 
 class ClassificationDatasetOrganizer:
     def __init__(self, dataset_context: DatasetContext):
         self.dataset_context = dataset_context
+        self.fs_operations = FileSystemOperations()
         self.destination_dir = dataset_context.dataset_extraction_path
 
     def organize(self):
-        categories = {
+        categories = self._extract_categories()
+        image_categories = self._map_image_to_category()
+        self._organize_images(categories, image_categories)
+        self._cleanup()
+        return self.dataset_context
+
+    def _extract_categories(self) -> Dict[int, str]:
+        return {
             category.id: category.name
             for category in self.dataset_context.coco_file.categories
         }
-        image_categories = {}
-        for annotation in self.dataset_context.coco_file.annotations:
-            image_id = annotation.image_id
-            category_id = annotation.category_id
-            image_categories[image_id] = category_id
 
+    def _map_image_to_category(self) -> Dict[str, int]:
+        return {
+            annotation.image_id: annotation.category_id
+            for annotation in self.dataset_context.coco_file.annotations
+        }
+
+    def _organize_images(
+        self, categories: Dict[int, str], image_categories: Dict[str, int]
+    ):
         for image in self.dataset_context.coco_file.images:
             image_id = image.id
             if image_id in image_categories:
                 category_id = image_categories[image_id]
                 category_name = categories[category_id]
+                self._create_category_dir_and_copy_image(category_name, image)
 
-                category_dir = os.path.join(self.destination_dir, category_name)
-                if not os.path.exists(category_dir):
-                    os.makedirs(category_dir)
+    def _create_category_dir_and_copy_image(self, category_name: str, image):
+        category_dir = os.path.join(self.destination_dir, category_name)
+        self.fs_operations.create_directory(category_dir)
+        src_image_path = os.path.join(self.dataset_context.image_dir, image.file_name)
+        dest_image_path = os.path.join(category_dir, image.file_name)
+        self.fs_operations.copy_file(src_image_path, dest_image_path)
 
-                src_image_path = os.path.join(
-                    self.dataset_context.image_dir, image.file_name
-                )
-                dest_image_path = os.path.join(category_dir, image.file_name)
-                shutil.copy(src_image_path, dest_image_path)
-
-        shutil.rmtree(self.dataset_context.image_dir)
-
-        self.dataset_context.image_dir = self.dataset_context.dataset_extraction_path
-        return self.dataset_context
+    def _cleanup(self):
+        self.fs_operations.remove_directory(self.dataset_context.image_dir)
+        self.dataset_context.image_dir = self.destination_dir
