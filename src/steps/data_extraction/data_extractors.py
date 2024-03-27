ce663b6fdacbfe74527f2f7bb637a2a936a799f7
SoniaGrh <sonia.garrouch@hotmail.fr>
2024-03-27 18:10:47 +0100

    refacto: typing missing

--- src/steps/data_extraction/data_extractors.py
+++ src/steps/data_extraction/data_extractors.py
@@ -1,11 +1,12 @@
 from src import Pipeline
 from src import step
 from src.models.contexts.picsellia_context import PicselliaTrainingContext
+from src.steps.data_extraction.utils.dataset_collection import DatasetCollection
 from src.steps.data_extraction.utils.dataset_handler import DatasetHandler
 
 
 @step
-def data_extractor():
+def data_extractor() -> DatasetCollection:
     context: PicselliaTrainingContext = Pipeline.get_active_context()
     dataset_handler = DatasetHandler(
         experiment=context.experiment,
