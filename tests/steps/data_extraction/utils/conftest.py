9c352f257430ee8bce9d5387bd0571b23ecd8fb4
SoniaGrh <sonia.garrouch@hotmail.fr>
2024-03-27 09:52:09 +0100

    feat: plug the pytest fixtures

---
+++ tests/steps/data_extraction/utils/conftest.py
@@ -0,0 +1,6 @@
+pytest_plugins = [
+    "tests.steps.fixtures.initialize_integration_tests_fixtures",
+    "tests.steps.data_extraction.fixtures.classification_dataset_fixtures",
+    "tests.steps.data_extraction.fixtures.dataset_collection_fixtures",
+    "tests.steps.data_extraction.fixtures.dataset_handler_fixtures",
+]
