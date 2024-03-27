bdbb007fe6d2b43edec8f1bab2c10649b02c3811
SoniaGrh <sonia.garrouch@hotmail.fr>
2024-03-27 18:10:02 +0100

    refacto: type and use DatasetTestMetadata

--- tests/steps/data_extraction/fixtures/classification_dataset_fixtures.py
+++ tests/steps/data_extraction/fixtures/classification_dataset_fixtures.py
@@ -1,13 +1,14 @@
 import logging
 import os
 import shutil
+from typing import Callable
 
 import pytest
 from picsellia import Client, Dataset, DatasetVersion, Data
 from picsellia.types.enums import InferenceType
 
-from src.models.dataset.dataset_split_name import DatasetSplitName
 from src.steps.data_extraction.utils.dataset_context import DatasetContext
+from tests.steps.data_extraction.utils.conftest import DatasetTestMetadata
 from tests.steps.fixtures.initialize_integration_tests_fixtures import (
     create_dataset_version,
     upload_data,
@@ -18,51 +19,46 @@ from tests.steps.fixtures.initialize_integration_tests_fixtures import (
 uploaded_data_registry = []
 
 
-def get_images_path(
-    dataset_split_name: DatasetSplitName, dataset_type: InferenceType
-) -> str:
+def get_dataset_name(dataset_type: InferenceType) -> str:
     if dataset_type == InferenceType.CLASSIFICATION:
-        return os.path.join(
-            os.getcwd(),
-            "tests",
-            "data",
-            "classification",
-            "Tyre_Quality_Classification",
-            f"pytest-{dataset_split_name.value}",
-            "images",
-        )
+        return "Tyre_Quality_Classification"
     else:
         raise ValueError(f"Dataset type {dataset_type} is not supported")
+
+
+def get_data_path(
+    dataset_metadata: DatasetTestMetadata,
+) -> str:
+    return os.path.join(
+        os.getcwd(),
+        "tests",
+        "data",
+        dataset_metadata.dataset_type.value.lower(),
+        get_dataset_name(dataset_type=dataset_metadata.dataset_type),
+        f"pytest-{dataset_metadata.dataset_split_name.value}",
+    )
+
+
+def get_images_path(
+    dataset_metadata: DatasetTestMetadata,
+) -> str:
+    return os.path.join(get_data_path(dataset_metadata=dataset_metadata), "images")
 
 
 def get_annotations_path(
-    dataset_split_name: DatasetSplitName, dataset_type: InferenceType
+    dataset_metadata: DatasetTestMetadata,
 ) -> str:
-    if dataset_type == InferenceType.CLASSIFICATION:
-        return os.path.join(
-            os.getcwd(),
-            "tests",
-            "data",
-            "classification",
-            "Tyre_Quality_Classification",
-            f"pytest-{dataset_split_name.value}",
-            "annotations",
-            "coco.json",
-        )
-    else:
-        raise ValueError(f"Dataset type {dataset_type} is not supported")
+    return os.path.join(
+        get_data_path(dataset_metadata=dataset_metadata), "annotations", "coco.json"
+    )
 
 
 @pytest.fixture
-def mock_uploaded_data(picsellia_client: Client):
-    def _mock_uploaded_data(
-        dataset_split_name: DatasetSplitName, dataset_type: InferenceType
-    ) -> Data:
+def mock_uploaded_data(picsellia_client: Client) -> Callable:
+    def _mock_uploaded_data(dataset_metadata: DatasetTestMetadata) -> Data:
         uploaded_data = upload_data(
             picsellia_client=picsellia_client,
-            images_path=get_images_path(
-                dataset_split_name=dataset_split_name, dataset_type=dataset_type
-            ),
+            images_path=get_images_path(dataset_metadata=dataset_metadata),
         )
         uploaded_data_registry.append(uploaded_data)
         return uploaded_data
@@ -71,23 +67,15 @@ def mock_uploaded_data(picsellia_client: Client):
 
 
 @pytest.fixture
-def mock_dataset_version(dataset: Dataset, mock_uploaded_data):
-    def _mock_dataset_version(
-        dataset_split_name: DatasetSplitName,
-        dataset_type: InferenceType,
-    ) -> DatasetVersion:
-        uploaded_data = mock_uploaded_data(
-            dataset_split_name=dataset_split_name,
-            dataset_type=dataset_type,
-        )
+def mock_dataset_version(dataset: Dataset, mock_uploaded_data: Callable) -> Callable:
+    def _mock_dataset_version(dataset_metadata: DatasetTestMetadata) -> DatasetVersion:
+        uploaded_data = mock_uploaded_data(dataset_metadata=dataset_metadata)
         dataset_version = create_dataset_version(
             dataset=dataset,
-            version_name=dataset_split_name.value,
-            dataset_type=dataset_type,
+            version_name=dataset_metadata.dataset_split_name.value,
+            dataset_type=dataset_metadata.dataset_type,
             uploaded_data=uploaded_data,
-            annotations_path=get_annotations_path(
-                dataset_split_name=dataset_split_name, dataset_type=dataset_type
-            ),
+            annotations_path=get_annotations_path(dataset_metadata=dataset_metadata),
         )
         return dataset_version
 
@@ -96,21 +84,15 @@ def mock_dataset_version(dataset: Dataset, mock_uploaded_data):
 
 @pytest.fixture
 def mock_dataset_context(
-    destination_path: str, mock_uploaded_data, mock_dataset_version
-):
-    def _mock_dataset_context(
-        dataset_split_name: DatasetSplitName,
-        dataset_type: InferenceType,
-    ) -> DatasetContext:
-        dataset_version = mock_dataset_version(
-            dataset_split_name=dataset_split_name,
-            dataset_type=dataset_type,
-        )
+    destination_path: str, mock_uploaded_data: Callable, mock_dataset_version: Callable
+) -> Callable:
+    def _mock_dataset_context(dataset_metadata: DatasetTestMetadata) -> DatasetContext:
+        dataset_version = mock_dataset_version(dataset_metadata=dataset_metadata)
         dataset_context = DatasetContext(
-            name=dataset_split_name.value,
+            dataset_name=dataset_metadata.dataset_split_name.value,
             dataset_version=dataset_version,
-            multi_asset=get_multi_asset(dataset_version),
-            labelmap=get_labelmap(dataset_version),
+            multi_asset=get_multi_asset(dataset_version=dataset_version),
+            labelmap=get_labelmap(dataset_version=dataset_version),
             destination_path=destination_path,
         )
         return dataset_context
