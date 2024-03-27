89579c772b8124926c09e1b3cf9e915f1c32bd29
SoniaGrh <sonia.garrouch@hotmail.fr>
2024-03-27 18:11:30 +0100

    refacto: add function to iterate, get and set item

--- src/steps/data_extraction/utils/dataset_collection.py
+++ src/steps/data_extraction/utils/dataset_collection.py
@@ -1,25 +1,27 @@
-from src.models.dataset.dataset_split_name import DatasetSplitName
 from src.steps.data_extraction.utils.dataset_context import DatasetContext
 
 
 class DatasetCollection:
     def __init__(
         self,
-        train_context: DatasetContext,
-        val_context: DatasetContext,
-        test_context: DatasetContext,
+        train_dataset_context: DatasetContext,
+        val_dataset_context: DatasetContext,
+        test_dataset_context: DatasetContext,
     ):
-        self.train = train_context
-        self.val = val_context
-        self.test = test_context
+        self.train = train_dataset_context
+        self.val = val_dataset_context
+        self.test = test_dataset_context
+
+    def __getitem__(self, key):
+        return getattr(self, key)
+
+    def __setitem__(self, key, value):
+        setattr(self, key, value)
+
+    def __iter__(self):
+        return iter([self.train, self.val, self.test])
 
     def download(self):
-        for context_name in [
-            DatasetSplitName.TRAIN.value,
-            DatasetSplitName.VAL.value,
-            DatasetSplitName.TEST.value,
-        ]:
-            context = getattr(self, context_name)
-            if context is not None:
-                context.download_assets()
-                context.download_coco_file()
+        for dataset_context in self:
+            dataset_context.download_assets()
+            dataset_context.download_coco_file()
