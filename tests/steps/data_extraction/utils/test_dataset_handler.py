ba48115ee264dd1fdf76a9bef6765c0e3c1fd1f5
SoniaGrh <sonia.garrouch@hotmail.fr>
2024-03-27 18:15:58 +0100

    refacto: make generation of params more clear

--- tests/steps/data_extraction/utils/test_dataset_handler.py
+++ tests/steps/data_extraction/utils/test_dataset_handler.py
@@ -1,65 +1,215 @@
-import os
-from typing import List
+from typing import List, Union, Callable
 
 import pytest
 from picsellia.exceptions import ResourceNotFoundError
 from picsellia.types.enums import InferenceType
 
 from src.models.dataset.dataset_split_name import DatasetSplitName
+from src.steps.data_extraction.utils.dataset_collection import DatasetCollection
+from tests.steps.data_extraction.utils.conftest import DatasetTestMetadata
 
 
-def generate_params(
-    num_datasets: List[int], with_ratios=False, with_num_datasets=False
-):
-    ratios_exceptions = {
-        1: ([0.8, 0.1, 0.1], None),
-        2: ([0.8, 0.2], None),
-        3: (None, RuntimeError),
-    }
+def get_experiment_name(nb_dataset: int) -> str:
+    return f"test-{nb_dataset}-dataset" + ("s" if nb_dataset > 1 else "")
 
-    dataset_combinations = [
-        (i, [DatasetSplitName.TRAIN, DatasetSplitName.TEST, DatasetSplitName.VAL][:i])
-        for i in range(1, 4)
-    ]
 
-    for i, dataset_splits in dataset_combinations:
-        if i in num_datasets:
-            datasets = [
-                (split, InferenceType.CLASSIFICATION) for split in dataset_splits
+def generate_general_params(
+    nb_datasets: List[int],
+) -> tuple[str, List[DatasetTestMetadata]]:
+    for nb_dataset in nb_datasets:
+        experiment_name = get_experiment_name(nb_dataset=nb_dataset)
+        if nb_dataset == 1:
+            dataset_metadatas = [
+                DatasetTestMetadata(
+                    dataset_split_name=DatasetSplitName.TRAIN,
+                    dataset_type=InferenceType.CLASSIFICATION,
+                )
             ]
-            name = f"test-{i}-dataset" + ("s" if i > 1 else "")
-            if with_ratios:
-                expected_ratios, expected_exception = ratios_exceptions[i]
-                yield name, datasets, expected_ratios, expected_exception
-            elif with_num_datasets:
-                yield name, datasets, i
-            else:
-                yield name, datasets
+        elif nb_dataset == 2:
+            dataset_metadatas = [
+                DatasetTestMetadata(
+                    dataset_split_name=DatasetSplitName.TRAIN,
+                    dataset_type=InferenceType.CLASSIFICATION,
+                ),
+                DatasetTestMetadata(
+                    dataset_split_name=DatasetSplitName.TEST,
+                    dataset_type=InferenceType.CLASSIFICATION,
+                ),
+            ]
+        elif nb_dataset == 3:
+            dataset_metadatas = [
+                DatasetTestMetadata(
+                    dataset_split_name=DatasetSplitName.TRAIN,
+                    dataset_type=InferenceType.CLASSIFICATION,
+                ),
+                DatasetTestMetadata(
+                    dataset_split_name=DatasetSplitName.TEST,
+                    dataset_type=InferenceType.CLASSIFICATION,
+                ),
+                DatasetTestMetadata(
+                    dataset_split_name=DatasetSplitName.VAL,
+                    dataset_type=InferenceType.CLASSIFICATION,
+                ),
+            ]
+        else:
+            raise ValueError(f"Unsupported number of datasets: {nb_dataset}")
+        yield experiment_name, dataset_metadatas
 
 
-def generate_random_dataset_name():
-    return "random_dataset_" + os.urandom(4).hex()
+def generate_params_with_split_ratios(
+    nb_datasets: List[int],
+) -> tuple[str, List[DatasetTestMetadata], Union[list, None], Union[Exception, None]]:
+    for nb_dataset in nb_datasets:
+        experiment_name = get_experiment_name(nb_dataset=nb_dataset)
+        if nb_dataset == 1:
+            dataset_metadatas = [
+                DatasetTestMetadata(
+                    dataset_split_name=DatasetSplitName.TRAIN,
+                    dataset_type=InferenceType.CLASSIFICATION,
+                )
+            ]
+            split_ratios = [0.8, 0.1, 0.1]
+            expected_exception = None
+        elif nb_dataset == 2:
+            dataset_metadatas = [
+                DatasetTestMetadata(
+                    dataset_split_name=DatasetSplitName.TRAIN,
+                    dataset_type=InferenceType.CLASSIFICATION,
+                ),
+                DatasetTestMetadata(
+                    dataset_split_name=DatasetSplitName.TEST,
+                    dataset_type=InferenceType.CLASSIFICATION,
+                ),
+            ]
+            split_ratios = [0.8, 0.2]
+            expected_exception = None
+        elif nb_dataset == 3:
+            dataset_metadatas = [
+                DatasetTestMetadata(
+                    dataset_split_name=DatasetSplitName.TRAIN,
+                    dataset_type=InferenceType.CLASSIFICATION,
+                ),
+                DatasetTestMetadata(
+                    dataset_split_name=DatasetSplitName.TEST,
+                    dataset_type=InferenceType.CLASSIFICATION,
+                ),
+                DatasetTestMetadata(
+                    dataset_split_name=DatasetSplitName.VAL,
+                    dataset_type=InferenceType.CLASSIFICATION,
+                ),
+            ]
+            split_ratios = None
+            expected_exception = RuntimeError
+        else:
+            raise ValueError(f"Unsupported number of datasets: {nb_dataset}")
+        yield experiment_name, dataset_metadatas, split_ratios, expected_exception
 
 
-def check_splited_assets(dataset_collection, num_datasets):
-    expected_counts = {
-        1: (8, 1, 1),
-        2: (8, 2, 10),
-        3: (10, 10, 10),
-    }
-    train, val, test = expected_counts[num_datasets]
+def generate_params_for_nested_datasets(
+    nb_datasets: List[int],
+) -> tuple[str, List[DatasetTestMetadata], Exception]:
+    for nb_dataset in nb_datasets:
+        experiment_name = get_experiment_name(nb_dataset=nb_dataset)
+        if nb_dataset == 1:
+            dataset_metadatas = [
+                DatasetTestMetadata(
+                    dataset_split_name=DatasetSplitName.TRAIN,
+                    dataset_type=InferenceType.CLASSIFICATION,
+                    attached_name="train_nested",
+                )
+            ]
+            expected_exception = ResourceNotFoundError
+            yield experiment_name, dataset_metadatas, expected_exception
+        elif nb_dataset == 2:
+            dataset_metadatas = [
+                DatasetTestMetadata(
+                    dataset_split_name=DatasetSplitName.TRAIN,
+                    dataset_type=InferenceType.CLASSIFICATION,
+                ),
+                DatasetTestMetadata(
+                    dataset_split_name=DatasetSplitName.TEST,
+                    dataset_type=InferenceType.CLASSIFICATION,
+                    attached_name="test_nested",
+                ),
+            ]
+            expected_exception = ResourceNotFoundError
+            yield experiment_name, dataset_metadatas, expected_exception
+        elif nb_dataset == 3:
+            experiment_name_1 = experiment_name + "_1"
+            experiment_name_2 = experiment_name + "_2"
+            dataset_metadatas_1 = [
+                DatasetTestMetadata(
+                    dataset_split_name=DatasetSplitName.TRAIN,
+                    dataset_type=InferenceType.CLASSIFICATION,
+                ),
+                DatasetTestMetadata(
+                    dataset_split_name=DatasetSplitName.TEST,
+                    dataset_type=InferenceType.CLASSIFICATION,
+                    attached_name="test_nested",
+                ),
+                DatasetTestMetadata(
+                    dataset_split_name=DatasetSplitName.VAL,
+                    dataset_type=InferenceType.CLASSIFICATION,
+                ),
+            ]
+            dataset_metadatas_2 = [
+                DatasetTestMetadata(
+                    dataset_split_name=DatasetSplitName.TRAIN,
+                    dataset_type=InferenceType.CLASSIFICATION,
+                ),
+                DatasetTestMetadata(
+                    dataset_split_name=DatasetSplitName.TEST,
+                    dataset_type=InferenceType.CLASSIFICATION,
+                ),
+                DatasetTestMetadata(
+                    dataset_split_name=DatasetSplitName.VAL,
+                    dataset_type=InferenceType.CLASSIFICATION,
+                    attached_name="val_nested",
+                ),
+            ]
+            expected_exception = ResourceNotFoundError
+            yield experiment_name_1, dataset_metadatas_1, expected_exception
+            yield experiment_name_2, dataset_metadatas_2, expected_exception
+        else:
+            raise ValueError(f"Unsupported number of datasets: {nb_dataset}")
+
+
+def get_nb_assets_after_split(nb_dataset: int) -> tuple[int, int, int]:
+    if nb_dataset == 1:
+        nb_train_assets = 8
+        nb_val_assets = 1
+        nb_test_assets = 1
+    elif nb_dataset == 2:
+        nb_train_assets = 8
+        nb_val_assets = 2
+        nb_test_assets = 10
+    elif nb_dataset == 3:
+        nb_train_assets = 10
+        nb_val_assets = 10
+        nb_test_assets = 10
+    else:
+        raise ValueError(f"Unsupported number of datasets: {nb_dataset}")
+    return nb_train_assets, nb_val_assets, nb_test_assets
+
+
+def check_splited_assets(
+    dataset_collection: DatasetCollection, nb_dataset: int
+) -> None:
+    nb_train_assets, nb_val_assets, nb_test_assets = get_nb_assets_after_split(
+        nb_dataset=nb_dataset
+    )
     assert (
-        len(dataset_collection.train.multi_asset) == train
-    ), f"Incorrect number of train assets for {num_datasets} dataset(s)"
+        len(dataset_collection.train.multi_asset) == nb_train_assets
+    ), f"Incorrect number of train assets for {nb_dataset} dataset(s)"
     assert (
-        len(dataset_collection.val.multi_asset) == val
-    ), f"Incorrect number of val assets for {num_datasets} dataset(s)"
+        len(dataset_collection.val.multi_asset) == nb_val_assets
+    ), f"Incorrect number of val assets for {nb_dataset} dataset(s)"
     assert (
-        len(dataset_collection.test.multi_asset) == test
-    ), f"Incorrect number of test assets for {num_datasets} dataset(s)"
+        len(dataset_collection.test.multi_asset) == nb_test_assets
+    ), f"Incorrect number of test assets for {nb_dataset} dataset(s)"
 
 
-def check_multi_asset(dataset_collection):
+def check_multi_asset_in_dataset(dataset_collection):
     assert all(
         asset in dataset_collection.train.dataset_version.list_assets()
         for asset in dataset_collection.train.multi_asset
@@ -74,48 +224,55 @@ def check_multi_asset(dataset_collection):
     ), "Test assets mismatch"
 
 
-def check_labelmap(dataset_collection, num_datasets):
-    if num_datasets == 1:
+def check_labelmap(dataset_collection: DatasetCollection, nb_dataset: int) -> None:
+    if nb_dataset == 1:
         assert (
             dataset_collection.train.labelmap
             == dataset_collection.val.labelmap
             == dataset_collection.test.labelmap
         ), "Inconsistent labelmap across contexts"
-    elif num_datasets == 2:
+    elif nb_dataset == 2:
         assert (
             dataset_collection.train.labelmap == dataset_collection.val.labelmap
         ), "Inconsistent labelmap between train and val"
-    elif num_datasets == 3:
+    elif nb_dataset == 3:
         print("No labelmap check for 3 datasets")
 
 
 class TestDatasetHandler:
     @pytest.mark.parametrize(
         "experiment_name, datasets",
-        list(generate_params(num_datasets=[1, 2, 3])),
+        list(generate_general_params(nb_datasets=[1, 2, 3])),
     )
     def test_get_dataset_collection(
-        self, mock_dataset_handler, experiment_name, datasets
-    ):
-        dataset_handler = mock_dataset_handler(experiment_name, datasets)
+        self,
+        mock_dataset_handler: Callable,
+        experiment_name: str,
+        datasets: List[DatasetTestMetadata],
+    ) -> None:
+        dataset_handler = mock_dataset_handler(
+            experiment_name=experiment_name, datasets=datasets
+        )
         dataset_collection = dataset_handler.get_dataset_collection()
         assert dataset_collection.train.name == DatasetSplitName.TRAIN.value
         assert dataset_collection.val.name == DatasetSplitName.VAL.value
         assert dataset_collection.test.name == DatasetSplitName.TEST.value
 
     @pytest.mark.parametrize(
-        "experiment_name, datasets, expected_ratios, expected_exception",
-        list(generate_params(num_datasets=[1, 2, 3], with_ratios=True)),
+        "experiment_name, datasets, expected_split_ratios, expected_exception",
+        list(generate_params_with_split_ratios(nb_datasets=[1, 2, 3])),
     )
     def test_get_split_ratios(
         self,
-        mock_dataset_handler,
-        experiment_name,
-        datasets,
-        expected_ratios,
-        expected_exception,
-    ):
-        dataset_handler = mock_dataset_handler(experiment_name, datasets)
+        mock_dataset_handler: Callable,
+        experiment_name: str,
+        datasets: List[DatasetTestMetadata],
+        expected_split_ratios: Union[list, None],
+        expected_exception: Union[Exception, None],
+    ) -> None:
+        dataset_handler = mock_dataset_handler(
+            experiment_name=experiment_name, datasets=datasets
+        )
         if expected_exception:
             with pytest.raises(expected_exception):
                 dataset_handler._get_split_ratios(count_datasets=len(datasets))
@@ -123,101 +280,72 @@ class TestDatasetHandler:
             split_ratios = dataset_handler._get_split_ratios(
                 count_datasets=len(datasets)
             )
-            assert split_ratios == expected_ratios
+            assert split_ratios == expected_split_ratios
 
     @pytest.mark.parametrize(
-        "experiment_name, datasets, num_datasets",
-        list(generate_params(num_datasets=[1, 2, 3], with_num_datasets=True)),
+        "experiment_name, datasets",
+        list(generate_general_params(nb_datasets=[1, 2, 3])),
     )
     def test_handle_datasets(
-        self, mock_dataset_handler, experiment_name, datasets, num_datasets
+        self,
+        mock_dataset_handler: Callable,
+        experiment_name: str,
+        datasets: List[DatasetTestMetadata],
     ):
-        dataset_handler = mock_dataset_handler(experiment_name, datasets)
+        dataset_handler = mock_dataset_handler(
+            experiment_name=experiment_name, datasets=datasets
+        )
+        nb_dataset = len(datasets)
 
-        if num_datasets == 1:
+        if nb_dataset == 1:
             dataset_collection = dataset_handler._handle_one_dataset(
                 train_dataset_version=dataset_handler.experiment.get_dataset(
-                    DatasetSplitName.TRAIN.value
+                    name=DatasetSplitName.TRAIN.value
                 )
             )
-        elif num_datasets == 2:
+        elif nb_dataset == 2:
             dataset_collection = dataset_handler._handle_two_datasets(
                 train_dataset_version=dataset_handler.experiment.get_dataset(
-                    DatasetSplitName.TRAIN.value
+                    name=DatasetSplitName.TRAIN.value
                 ),
                 test_dataset_version=dataset_handler.experiment.get_dataset(
-                    DatasetSplitName.TEST.value
+                    name=DatasetSplitName.TEST.value
                 ),
             )
-        elif num_datasets == 3:
+        elif nb_dataset == 3:
             dataset_collection = dataset_handler._handle_three_datasets(
                 train_dataset_version=dataset_handler.experiment.get_dataset(
-                    DatasetSplitName.TRAIN.value
+                    name=DatasetSplitName.TRAIN.value
                 ),
                 val_dataset_version=dataset_handler.experiment.get_dataset(
-                    DatasetSplitName.VAL.value
+                    name=DatasetSplitName.VAL.value
                 ),
                 test_dataset_version=dataset_handler.experiment.get_dataset(
-                    DatasetSplitName.TEST.value
+                    name=DatasetSplitName.TEST.value
                 ),
             )
         else:
-            pytest.fail(f"Unsupported number of datasets: {num_datasets}")
+            pytest.fail(f"Unsupported number of datasets: {nb_dataset}")
 
-        check_splited_assets(dataset_collection, num_datasets)
-
-        check_labelmap(dataset_collection, num_datasets)
-
-        check_multi_asset(dataset_collection)
+        check_splited_assets(
+            dataset_collection=dataset_collection, nb_dataset=nb_dataset
+        )
+        check_labelmap(dataset_collection=dataset_collection, nb_dataset=nb_dataset)
+        check_multi_asset_in_dataset(dataset_collection=dataset_collection)
 
     @pytest.mark.parametrize(
         "experiment_name, datasets, expected_exception",
-        [
-            (
-                "test-one-random-dataset",
-                [
-                    (
-                        DatasetSplitName.TRAIN,
-                        InferenceType.CLASSIFICATION,
-                        generate_random_dataset_name(),
-                    )
-                ],
-                ResourceNotFoundError,
-            ),
-            (
-                "test-two-random-datasets",
-                [
-                    (DatasetSplitName.TRAIN, InferenceType.CLASSIFICATION),
-                    (
-                        DatasetSplitName.TEST,
-                        InferenceType.CLASSIFICATION,
-                        generate_random_dataset_name(),
-                    ),
-                ],
-                ResourceNotFoundError,
-            ),
-            (
-                "test-three-random-datasets",
-                [
-                    (DatasetSplitName.TRAIN, InferenceType.CLASSIFICATION),
-                    (
-                        DatasetSplitName.TEST,
-                        InferenceType.CLASSIFICATION,
-                        generate_random_dataset_name(),
-                    ),
-                    (
-                        DatasetSplitName.VAL,
-                        InferenceType.CLASSIFICATION,
-                        generate_random_dataset_name(),
-                    ),
-                ],
-                ResourceNotFoundError,
-            ),
-        ],
+        list(generate_params_for_nested_datasets(nb_datasets=[1, 2, 3])),
     )
     def test_handle_datasets_exceptions(
-        self, mock_dataset_handler, experiment_name, datasets, expected_exception
+        self,
+        mock_dataset_handler: Callable,
+        experiment_name: str,
+        datasets: List[DatasetTestMetadata],
+        expected_exception: Exception,
     ):
         with pytest.raises(expected_exception):
-            dataset_handler = mock_dataset_handler(experiment_name, datasets)
+            dataset_handler = mock_dataset_handler(
+                experiment_name=experiment_name, datasets=datasets
+            )
             dataset_handler.get_dataset_collection()
