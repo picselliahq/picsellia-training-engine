f5ecb2212f434d14fdcea763d93299010afd427e
SoniaGrh <sonia.garrouch@hotmail.fr>
2024-03-27 18:11:52 +0100

    refacto: use DatasetTestMetadata

--- tests/steps/data_extraction/fixtures/dataset_collection_fixtures.py
+++ tests/steps/data_extraction/fixtures/dataset_collection_fixtures.py
@@ -1,27 +1,35 @@
-import pytest
+from typing import Callable
 
+import pytest
 from picsellia.types.enums import InferenceType
 
 from src.models.dataset.dataset_split_name import DatasetSplitName
 from src.steps.data_extraction.utils.dataset_collection import DatasetCollection
+from tests.steps.data_extraction.utils.conftest import DatasetTestMetadata
 
 
 @pytest.fixture
-def mock_dataset_collection(mock_dataset_context):
+def mock_dataset_collection(mock_dataset_context: Callable) -> Callable:
     def _mock_dataset_collection(dataset_type: InferenceType):
         train_dataset_context = mock_dataset_context(
-            dataset_split_name=DatasetSplitName.TRAIN.value, dataset_type=dataset_type
+            dataset_metadata=DatasetTestMetadata(
+                dataset_split_name=DatasetSplitName.TRAIN, dataset_type=dataset_type
+            )
         )
         val_dataset_context = mock_dataset_context(
-            dataset_split_name=DatasetSplitName.VAL.value, dataset_type=dataset_type
+            dataset_metadata=DatasetTestMetadata(
+                dataset_split_name=DatasetSplitName.VAL, dataset_type=dataset_type
+            )
         )
         test_dataset_context = mock_dataset_context(
-            dataset_split_name=DatasetSplitName.TEST.value, dataset_type=dataset_type
+            dataset_metadata=DatasetTestMetadata(
+                dataset_split_name=DatasetSplitName.TEST, dataset_type=dataset_type
+            )
         )
         dataset_collection = DatasetCollection(
-            train_context=train_dataset_context,
-            val_context=val_dataset_context,
-            test_context=test_dataset_context,
+            train_dataset_context=train_dataset_context,
+            val_dataset_context=val_dataset_context,
+            test_dataset_context=test_dataset_context,
         )
         return dataset_collection
 
