d1c86807cf5f5defcdf78fcf1256e60634e9475a
SoniaGrh <sonia.garrouch@hotmail.fr>
2024-04-10 14:10:35 +0200

    fix: run validate_at_least_one_image_per_class for all sets

--- src/steps/data_validation/utils/classification_dataset_validator.py
+++ src/steps/data_validation/utils/classification_dataset_validator.py
@@ -35,18 +35,20 @@ class ClassificationDatasetValidator(DatasetValidator, ABC):
 
     def validate_at_least_one_image_per_class(self):
         """
-        Validate that each class in the train dataset has at least 1 image.
+        Validate that each class in the classification dataset has at least 1 image. If a class has no images,
+        the dataset is considered invalid.
 
         Raises:
-            ValueError: If a class in the train dataset has no images.
+            ValueError: If any class in any dataset in the collection has no images.
 
         """
-        class_names = self.dataset_collection.train.labelmap.keys()
-        for class_name in class_names:
-            folder_path = os.path.join(
-                self.dataset_collection.train.image_dir, str(class_name)
-            )
-            if not os.path.exists(folder_path) or len(os.listdir(folder_path)) == 0:
-                raise ValueError(
-                    f"Train dataset must have at least 1 image for class {class_name}"
+        for dataset_context in self.dataset_collection:
+            class_names = dataset_context.labelmap.keys()
+            for class_name in class_names:
+                folder_path = os.path.join(
+                    self.dataset_collection.train.image_dir, str(class_name)
                 )
+                if not os.path.exists(folder_path) or len(os.listdir(folder_path)) == 0:
+                    raise ValueError(
+                        f"{dataset_context.name} dataset must have at least 1 image for class {class_name}"
+                    )
