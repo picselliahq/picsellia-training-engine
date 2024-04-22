2bba7f4cfc4d757074d9da6eb765eb61122ec460
SoniaGrh <sonia.garrouch@hotmail.fr>
2024-04-22 17:45:17 +0200

    fix: add optional param + download

--- src/models/dataset/dataset_context.py
+++ src/models/dataset/dataset_context.py
@@ -1,7 +1,12 @@
 import os
+from typing import Optional
 
-from picsellia import DatasetVersion
+from picsellia import DatasetVersion, Label
 from picsellia.sdk.asset import MultiAsset
+
+from src.steps.data_extraction.utils.image_utils import get_labelmap
+
+from picsellia.exceptions import NoDataError
 
 
 class DatasetContext:
@@ -27,9 +32,9 @@ class DatasetContext:
         self,
         dataset_name: str,
         dataset_version: DatasetVersion,
-        multi_asset: MultiAsset,
-        labelmap: dict,
         destination_path: str,
+        multi_asset: Optional[MultiAsset] = None,
+        labelmap: Optional[dict[str, Label]] = None,
     ):
         """
         Initializes the DatasetContext with dataset metadata and configuration.
@@ -43,10 +48,20 @@ class DatasetContext:
         """
         self.dataset_name = dataset_name
         self.dataset_version = dataset_version
-        self.multi_asset = multi_asset
         self.dataset_extraction_path = os.path.join(destination_path, self.dataset_name)
-        self.labelmap = labelmap
-        self.image_dir = None
+        os.makedirs(self.dataset_extraction_path, exist_ok=True)
+        if not labelmap:
+            self.labelmap = get_labelmap(dataset_version=dataset_version)
+        else:
+            self.labelmap = labelmap or {}
+        if not multi_asset:
+            try:
+                self.multi_asset = dataset_version.list_assets()
+            except NoDataError:
+                self.multi_asset = None
+        else:
+            self.multi_asset = multi_asset
+        self.image_dir = os.path.join(self.dataset_extraction_path, "images")
         self.coco_file = None
 
     def download_assets(self) -> None:
@@ -54,14 +69,23 @@ class DatasetContext:
         Downloads the dataset assets to a local directory. Initializes `image_dir`
         to the path where images are stored.
         """
-        self.image_dir = os.path.join(self.dataset_extraction_path, "images")
-        self.multi_asset.download(target_path=self.image_dir, use_id=True)
+        if self.multi_asset:
+            self.multi_asset.download(target_path=self.image_dir, use_id=True)
+        else:
+            raise ValueError("No assets found in the dataset")
 
     def download_coco_file(self) -> None:
         """
         Builds the COCO file associated with the dataset. Initializes `coco_file` to the COCO file object.
         """
-        self.coco_file = self.dataset_version.build_coco_file_locally(
-            assets=self.multi_asset,
-            use_id=True,
-        )
+        if self.multi_asset:
+            self.coco_file = self.dataset_version.build_coco_file_locally(
+                assets=self.multi_asset,
+                use_id=True,
+            )
+        else:
+            raise ValueError("No assets found in the dataset")
+
+    def download(self) -> None:
+        self.download_assets()
+        self.download_coco_file()
