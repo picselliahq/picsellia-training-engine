f00f6ebce0f812b8b7b36e9dbcf032bb5f3b9f06
SoniaGrh <sonia.garrouch@hotmail.fr>
2024-04-24 17:27:16 +0200

    feat: add processing parameters

---
+++ src/pipelines/bounding_box_cropper/utils/bounding_box_cropper_parameters.py
@@ -0,0 +1,13 @@
+from src.models.parameters.parameters import Parameters
+
+
+class BoundingBoxCropperParameters(Parameters):
+    def __init__(self, log_data):
+        super().__init__(log_data)
+
+        self.datalake = self.extract_parameter(
+            keys=["datalake"], expected_type=str, default="default"
+        )
+        self.label_name_to_extract = self.extract_parameter(
+            keys=["label_name_to_extract"], expected_type=str
+        )
