5c094885af13ff5d09113f4aa5a2f309eacfd8d6
SoniaGrh <sonia.garrouch@hotmail.fr>
2024-04-26 09:57:52 +0200

    fix: add only data steps

--- src/pipelines/yolov8_classification/training_pipeline.py
+++ src/pipelines/yolov8_classification/training_pipeline.py
@@ -1,28 +1,27 @@
+# type: ignore
+
 from src import pipeline
 from src.models.contexts.picsellia_context import (
     PicselliaTrainingContext,
-    PicselliaContext,
 )
 from src.models.parameters.augmentation_parameters import (
     UltralyticsAugmentationParameters,
 )
 from src.models.parameters.hyper_parameters import UltralyticsHyperParameters
-from src.steps.callbacks_preparators import callback_preparator
-from src.steps.data_validators import data_validator
-from src.steps.weights_preparators import weights_preparator
-from src.steps.weights_extractors import weights_extractor
-from src.steps.weights_validators import weights_validator
-from src.steps.data_extractors import data_extractor
-from src.steps.data_preparators import data_preparator
-from src.steps.model_evaluators import model_evaluator
-from src.steps.model_exporters import model_exporter
-from src.steps.model_inferences import model_inference
-from src.steps.model_loaders import model_loader
-from src.steps.model_registers import model_register
-from src.steps.model_trainers import model_trainer
+from src.steps.data_extraction.data_extractor import training_data_extractor
+from src.steps.data_preparation.classification_data_preparator import (
+    classification_data_preparator,
+)
+from src.steps.data_validation.classification_data_validator import (
+    classification_data_validator,
+)
 
 
-def get_context() -> PicselliaContext:
+def get_context() -> (
+    PicselliaTrainingContext[
+        UltralyticsHyperParameters, UltralyticsAugmentationParameters
+    ]
+):
     return PicselliaTrainingContext(
         hyperparameters_cls=UltralyticsHyperParameters,
         augmentation_parameters_cls=UltralyticsAugmentationParameters,
@@ -36,36 +35,11 @@ def get_context() -> PicselliaContext:
 )
 def yolov8_classification_training_pipeline():
     # Data pipeline
-    dataset_context = data_extractor()
-    dataset_context = data_validator(dataset_context=dataset_context)
-    data_path = data_preparator(dataset_context=dataset_context)
-
-    # Model pipeline
-    weights_path = weights_extractor()
-    weights_path = weights_validator(weights_path=weights_path)
-    weights_path = weights_preparator(weights_path=weights_path)
-    model = model_loader(weights_path=weights_path)
-
-    # Training pipeline
-    callbacks = callback_preparator()
-    model = model_trainer(model=model, callbacks=callbacks, dataset_path=data_path)
-    exported_model_path = model_exporter(model=model)
-    _ = model_register(
-        weights_name="model-latest-onnx",
-        weights_path=str(exported_model_path),
+    dataset_collection = training_data_extractor()
+    dataset_collection = classification_data_preparator(
+        dataset_collection=dataset_collection
     )
-
-    # Evaluation pipeline
-    picsellia_predictions = model_inference(
-        model=model,
-        dataset_context=dataset_context,
-        attached_dataset_version="test",
-    )
-    _ = model_evaluator(
-        dataset_context=dataset_context,
-        attached_dataset_version="test",
-        picsellia_predictions=picsellia_predictions,
-    )
+    classification_data_validator(dataset_collection=dataset_collection)
 
 
 if __name__ == "__main__":
