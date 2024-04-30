b54a30a63e13054a88a4aeb8ce89f6567122fcb1
SoniaGrh <sonia.garrouch@hotmail.fr>
2024-04-30 12:23:00 +0200

    fix: naming

--- src/steps/data_validation/utils/image_utils.py
+++ src/steps/data_validation/utils/image_utils.py
@@ -3,7 +3,7 @@ import os
 from typing import List
 
 
-def get_image_path_list(image_dir: str) -> List[str]:
+def get_images_path_list(image_dir: str) -> List[str]:
     """
     Generates a list of all image file paths within a specified directory.
 
@@ -13,8 +13,8 @@ def get_image_path_list(image_dir: str) -> List[str]:
     Returns:
         List[str]: A list containing the paths to all images found within the directory and its subdirectories.
     """
-    image_path_list = []
+    images_path_list = []
     for root, dirs, files in os.walk(image_dir):
         for file in files:
-            image_path_list.append(os.path.join(root, file))
-    return image_path_list
+            images_path_list.append(os.path.join(root, file))
+    return images_path_list
