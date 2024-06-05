87e6a5f725484ef32a0111a52e54a0d0d3950ad2
SoniaGrh <sonia.garrouch@hotmail.fr>
2024-06-05 11:29:56 +0200

    fix: imports

--- tests/models/steps/data_validation/common/conftest.py
+++ tests/models/steps/data_validation/common/conftest.py
@@ -1,6 +1,6 @@
 pytest_plugins = [
-    "tests.steps.fixtures.dataset_version_fixtures",
-    "tests.steps.fixtures.integration_tests_fixtures",
+    "tests.fixtures.dataset_version_fixtures",
+    "tests.fixtures.initialize_integration_tests_fixtures",
     "tests.models.dataset.common.fixtures.dataset_context_fixtures",
     "tests.models.steps.data_validation.common.fixtures.dataset_context_validator_fixtures",
     "tests.models.steps.data_validation.common.fixtures.dataset_collection_validator_fixtures",
