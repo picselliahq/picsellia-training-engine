97a71fc2777c5364d09518fd8d755128dde64f65
SoniaGrh <sonia.garrouch@hotmail.fr>
2024-06-26 17:14:21 +0200

    feat: use dataset collection

--- src/models/steps/processing/dataset_version_creation/slicer_processing.py
+++ src/models/steps/processing/dataset_version_creation/slicer_processing.py
@@ -1,12 +1,12 @@
 import concurrent.futures
 import os
+import shutil
 from pathlib import Path
-from typing import List, Tuple, Optional, Union
+from typing import List, Optional, Union
 
 import numpy as np
 import sahi
 from PIL import Image
-from picsellia import DatasetVersion, Client
 from sahi.slicing import (
     slice_coco,
     SliceImageResult,
@@ -25,9 +25,8 @@ from sahi.utils.cv import (
 from shapely.geometry import box, MultiPolygon, GeometryCollection, Polygon
 from shapely.validation import make_valid
 
-from src.models.dataset.dataset_context import DatasetContext
-from src.models.steps.processing.dataset_version_creation.dataset_version_creation_processing import (
-    DatasetVersionCreationProcessing,
+from src.models.dataset.common.processing.processing_dataset_collection import (
+    ProcessingDatasetCollection,
 )
 
 
@@ -239,51 +238,28 @@ def custom_slice_image(
 sahi.slicing.slice_image = custom_slice_image
 
 
-class SlicerProcessing(DatasetVersionCreationProcessing):
+class SlicerProcessing:
     """
     This class is used to extract bounding boxes from images in a dataset version for a specific label.
     """
 
     def __init__(
         self,
-        client: Client,
-        input_dataset_context: DatasetContext,
+        dataset_collection: ProcessingDatasetCollection,
         slice_height: int,
         slice_width: int,
         overlap_height_ratio: float,
         overlap_width_ratio: float,
         min_area_ratio: float,
-        output_dataset_version: DatasetVersion,
-        datalake: str,
-        destination_path: str,
     ):
-        super().__init__(
-            client=client,
-            output_dataset_version=output_dataset_version,
-            dataset_type=input_dataset_context.dataset_version.type,
-            dataset_description=f"Dataset sliced from dataset version "
-            f"'{input_dataset_context.dataset_version.version}' "
-            f"(id: {input_dataset_context.dataset_version.id}) "
-            f"in dataset '{input_dataset_context.dataset_version.name}' "
-            f"with slice size {slice_height}x{slice_width}.",
-            datalake=datalake,
-        )
-        self.dataset_context = input_dataset_context
-        self.processed_dataset_context = DatasetContext(
-            dataset_name="processed_dataset",
-            dataset_version=self.output_dataset_version,
-            destination_path=destination_path,
-            multi_asset=None,
-            labelmap=None,
-            use_id=False,
-        )
+        self.dataset_collection = dataset_collection
         self.slice_height = slice_height
         self.slice_width = slice_width
         self.overlap_height_ratio = overlap_height_ratio
         self.overlap_width_ratio = overlap_width_ratio
         self.min_area_ratio = min_area_ratio
 
-    def _process_images(self) -> Tuple[List[str], str]:
+    def _process_dataset_collection(self):
         """
         Processes all images in the dataset to extract the bounding boxes for the specified label.
         Returns:
@@ -291,36 +267,45 @@ class SlicerProcessing(DatasetVersionCreationProcessing):
         """
         CocoAnnotation.get_sliced_coco_annotation = custom_get_sliced_coco_annotation
         slice_coco(
-            coco_annotation_file_path=self.dataset_context.coco_file_path,
-            image_dir=self.dataset_context.image_dir,
+            coco_annotation_file_path=self.dataset_collection.input.coco_file_path,
+            image_dir=self.dataset_collection.input.image_dir,
             slice_height=self.slice_height,
             slice_width=self.slice_width,
             overlap_height_ratio=self.overlap_height_ratio,
             overlap_width_ratio=self.overlap_width_ratio,
             min_area_ratio=self.min_area_ratio,
             output_coco_annotation_file_name="sliced_annotations",
-            output_dir=self.processed_dataset_context.destination_path,
+            output_dir=self.dataset_collection.output.image_dir,
         )
 
-        output_files = os.listdir(self.processed_dataset_context.destination_path)
-        sliced_images_list = [
-            os.path.join(self.processed_dataset_context.destination_path, file)
-            for file in output_files
-            if not file.endswith(".json")
-        ]
+        output_files = os.listdir(self.dataset_collection.output.image_dir)
         sliced_annotations_path = [
-            os.path.join(self.processed_dataset_context.destination_path, file)
+            os.path.join(self.dataset_collection.output.image_dir, file)
             for file in output_files
             if file.endswith(".json")
         ][0]
-        return sliced_images_list, sliced_annotations_path
+        os.remove(self.dataset_collection.output.coco_file_path)
+        shutil.move(
+            sliced_annotations_path, self.dataset_collection.output.coco_file_path
+        )
+        self.dataset_collection.output.build_coco_file(
+            coco_file_path=self.dataset_collection.output.coco_file_path
+        )
 
-    def process(self) -> None:
-        sliced_images_list, sliced_annotations_path = self._process_images()
-        self._add_images_to_dataset_version(
-            images_to_upload=sliced_images_list,
-            images_tags=["picsellia_sliced", self.output_dataset_version.version],
+    def _update_output_dataset_version(self):
+        output_dataset_type = self.dataset_collection.input.dataset_version.type
+        output_dataset_description = (
+            f"Dataset sliced from dataset version "
+            f"'{self.dataset_collection.input.dataset_version.version}' "
+            f"(id: {self.dataset_collection.input.dataset_version.id}) "
+            f"in dataset '{self.dataset_collection.input.dataset_version.name}' "
+            f"with slice size {self.slice_height}x{self.slice_width}."
         )
-        self._add_coco_annotations_to_dataset_version(
-            annotation_path=sliced_annotations_path
+        self.dataset_collection.output.dataset_version.update(
+            description=output_dataset_description, type=output_dataset_type
         )
+
+    def process(self) -> ProcessingDatasetCollection:
+        self._update_output_dataset_version()
+        self._process_dataset_collection()
+        return self.dataset_collection
