8ad68c23b18ebb898146c650593914457092d6d0
SoniaGrh <sonia.garrouch@hotmail.fr>
2024-06-26 17:13:50 +0200

    feat: add dataset collection for processing

---
+++ src/models/steps/data_extraction/processing/processing_dataset_collection_extractor.py
@@ -0,0 +1,49 @@
+import os
+from typing import Optional
+
+from picsellia import DatasetVersion
+
+from src.models.dataset.common.dataset_context import DatasetContext
+from src.models.dataset.common.processing.processing_dataset_collection import (
+    ProcessingDatasetCollection,
+)
+
+
+class ProcessingDatasetCollectionExtractor:
+    def __init__(
+        self,
+        input_dataset_version: DatasetVersion,
+        output_dataset_version: DatasetVersion,
+        job_id: Optional[str] = None,
+        use_id: Optional[bool] = True,
+    ):
+        self.input_dataset_version = input_dataset_version
+        self.output_dataset_version = output_dataset_version
+        if not job_id:
+            self.destination_path = os.path.join(os.getcwd(), "current_job")
+        else:
+            self.destination_path = os.path.join(os.getcwd(), str(job_id))
+        self.use_id = use_id
+
+    def get_dataset_collection(self) -> ProcessingDatasetCollection:
+        # get input and output dataset versions
+        input_dataset_context = DatasetContext(
+            dataset_name="input",
+            dataset_version=self.input_dataset_version,
+            destination_path=self.destination_path,
+            multi_asset=None,
+            labelmap=None,
+            use_id=self.use_id,
+        )
+        output_dataset_context = DatasetContext(
+            dataset_name="output",
+            dataset_version=self.output_dataset_version,
+            destination_path=self.destination_path,
+            multi_asset=None,
+            labelmap=None,
+            use_id=self.use_id,
+        )
+        return ProcessingDatasetCollection(
+            input_dataset_context=input_dataset_context,
+            output_dataset_context=output_dataset_context,
+        )
