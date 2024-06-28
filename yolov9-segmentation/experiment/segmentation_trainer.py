67e69266bd766eb5d83cda428ebbb021830cc7e8
picsalex <132259399+picsalex@users.noreply.github.com>
2024-06-28 15:52:12 +0200

    feat: yolov9 segmentation training

---
+++ yolov9-segmentation/experiment/segmentation_trainer.py
@@ -0,0 +1,83 @@
+import logging
+import math
+import os
+
+from picsellia import Experiment
+from picsellia.types.enums import LogType
+from ultralytics import YOLO
+from ultralytics.models.yolo.segment.train import SegmentationTrainer
+
+from core_utils.yolov8 import store_model_files
+
+
+class PicselliaSegmentationTrainer(SegmentationTrainer):
+    def __init__(self, experiment: Experiment, cfg=None, parameters: dict = None):
+        args = dict(
+            task=cfg.task,
+            model=cfg.model or "yolov8n-seg.pt",
+            data=cfg.data or "coco128-seg.yaml",
+            device=cfg.device if cfg.device is not None else "",
+            epochs=cfg.epochs,
+            batch=cfg.batch,
+            imgsz=cfg.imgsz,
+            save_period=cfg.save_period,
+            name=cfg.name,
+            project=cfg.project,
+            patience=cfg.patience,
+        )
+        cfg_dict = vars(cfg)
+        for parameter_key in parameters.keys():
+            if parameter_key in cfg_dict:
+                parameter_type = type(cfg_dict[parameter_key])
+                args[parameter_key] = parameter_type(parameters[parameter_key])
+
+        super().__init__(overrides=args)
+        self.experiment = experiment
+        # self.cwd = cfg.cwd
+
+    def save_metrics(self, metrics):
+        super().save_metrics(metrics)
+        model = None
+        for name, value in metrics.items():
+            if value is not None and not math.isnan(value):
+                log_name = str(name).replace("/", "_")
+                self._log_metric(log_name, float(value), retry=1)
+
+        if self.epoch == self.epochs - 1:
+            store_model_files(
+                experiment=self.experiment,
+                save_dir=self.save_dir,
+                task="segment",
+            )
+
+        if (
+            (self.epoch > 1)
+            and (self.save_period > 0)
+            and ((self.epoch - 1) % self.save_period == 0)
+        ):
+            try:
+                model = YOLO(os.path.join(self.save_dir, "weights", "best.pt"))
+            except FileNotFoundError:
+                try:
+                    model = YOLO(os.path.join(self.save_dir, "weights", "last.pt"))
+                except FileNotFoundError as e:
+                    logging.warning(
+                        "Can't find intermediary checkpoint at save period, they will be uploaded at the end"
+                    )
+                    logging.warning(e)
+            if model:
+                model.export(format="onnx", imgsz=self.args.imgsz, task="segment")
+                store_model_files(
+                    experiment=self.experiment,
+                    save_dir=self.save_dir,
+                    task="segment",
+                )
+
+    def _log_metric(self, name: str, value: float, retry: int):
+        try:
+            self.experiment.log(name=name, type=LogType.LINE, data=value)
+        except Exception:
+            logging.exception(f"couldn't log {name}")
+            if retry > 0:
+                logging.info(f"retrying log {name}")
+                self._log_metric(name, value, retry - 1)
---
+++ yolov9-segmentation/experiment/trainer.py
@@ -0,0 +1,63 @@
+import os
+import sys
+
+from picsellia.sdk.asset import MultiAsset
+
+from abstract_trainer.yolov8_trainer import Yolov8Trainer
+from core_utils.yolov8 import (
+    generate_data_yaml,
+    setup_hyp,
+)
+
+sys.path.append(os.path.join(os.getcwd(), "yolov8-segmentation", "experiment"))
+
+from evaluator.yolo_evaluator import SegmentationYOLOEvaluator
+from segmentation_trainer import PicselliaSegmentationTrainer
+from utils import create_yolo_segmentation_label
+
+
+class Yolov8SegmentationTrainer(Yolov8Trainer):
+    def __init__(self):
+        super().__init__()
+        self.final_run_path = None
+
+    def download_data_with_label(self, assets: MultiAsset, data_type: str):
+        assets.download(
+            target_path=os.path.join(self.experiment.png_dir, data_type, "images"),
+            max_workers=8,
+        )
+        create_yolo_segmentation_label(
+            self.experiment,
+            data_type,
+            self.annotations_dict,
+            self.annotations_coco,
+            self.label_names,
+        )
+
+    def setup_trainer(self):
+        data_yaml_path = generate_data_yaml(
+            experiment=self.experiment,
+            labelmap=self.labelmap,
+            config_path=self.current_dir,
+        )
+        config = setup_hyp(
+            experiment=self.experiment,
+            data_yaml_path=data_yaml_path,
+            params=self.parameters,
+            cwd=self.current_dir,
+            task="segment",
+        )
+
+        self.trainer = PicselliaSegmentationTrainer(
+            experiment=self.experiment, cfg=config, parameters=self.parameters
+        )
+
+    def eval(self):
+        segmentation_evaluator = SegmentationYOLOEvaluator(
+            experiment=self.experiment,
+            dataset=self.evaluation_ds,
+            asset_list=self.evaluation_assets,
+            confidence_threshold=self.parameters.get("confidence_threshold", 0.1),
+        )
+
+        segmentation_evaluator.evaluate()
