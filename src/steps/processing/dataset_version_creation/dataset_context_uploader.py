95ce822e841194644c77ccab506a4e7f771c7e29
SoniaGrh <sonia.garrouch@hotmail.fr>
2024-07-01 15:32:48 +0200

    fix: adapt images tags

--- src/steps/processing/dataset_version_creation/dataset_context_uploader.py
+++ src/steps/processing/dataset_version_creation/dataset_context_uploader.py
@@ -1,7 +1,6 @@
-from src.models.contexts.picsellia_context import PicselliaProcessingContext
-
 from src import Pipeline
 from src import step
+from src.models.contexts.picsellia_context import PicselliaProcessingContext
 from src.models.dataset.common.dataset_context import DatasetContext
 from src.models.steps.processing.dataset_version_creation.dataset_context_uploader import (
     DatasetContextUploader,
@@ -11,11 +10,14 @@ from src.models.steps.processing.dataset_version_creation.dataset_context_upload
 @step
 def dataset_context_uploader(dataset_context: DatasetContext):
     context: PicselliaProcessingContext = Pipeline.get_active_context()
-
     uploader = DatasetContextUploader(
         client=context.client,
         dataset_context=dataset_context,
         datalake=context.processing_parameters.datalake,
-        images_tags=context.processing_parameters.images_tags,
+        images_tags=[
+            context.processing_parameters.images_tag,
+            dataset_context.dataset_version.version,
+            "picsellia_sliced",
+        ],
     )
     uploader.upload_dataset_context()
