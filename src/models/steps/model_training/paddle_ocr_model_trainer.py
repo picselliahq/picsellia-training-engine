94ee1a25e5dac428d75c9f38cfdd46aebe71065d
SoniaGrh <sonia.garrouch@hotmail.fr>
2024-07-04 18:44:21 +0200

    fix: refacto trainer

--- src/models/steps/model_training/paddle_ocr_model_trainer.py
+++ src/models/steps/model_training/paddle_ocr_model_trainer.py
@@ -1,51 +1,12 @@
+import os
+
+from src.models.model.model_context import ModelContext
 from src.models.model.paddle_ocr_model_collection import PaddleOCRModelCollection
 
 import subprocess
 
-
-def train_model(config_path):
-    """
-    Trains a Paddle OCR model given a configuration path and captures the output metrics line by line.
-
-    Args:
-        config_path (str): The path to the model's YAML configuration file.
-    """
-    # Set up the environment variable for PYTHONPATH
-    import os
-
-    current_pythonpath = os.environ.get("PYTHONPATH", "")
-    os.environ["PYTHONPATH"] = f".:{current_pythonpath}"
-
-    command = [
-        "python3",
-        "src/pipelines/paddle_ocr/PaddleOCR/tools/train.py",
-        "-c",
-        config_path,
-    ]
-
-    # Execute the command and capture output in real-time
-    process = subprocess.Popen(
-        command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True
-    )
-
-    try:
-        if process.stdout:
-            for line in iter(process.stdout.readline, ""):
-                if "epoch:" in line:
-                    extract_and_log_metrics(line)
-    except Exception as e:
-        print("Error during model training:", e)
-
-    # Wait for the process to terminate and get the exit code
-    process.wait()
-    if process.returncode != 0:
-        print("Training failed with errors")
-        if process.stderr:
-            errors = process.stderr.read()
-            print(errors)
-
-    # Reset PYTHONPATH if necessary
-    os.environ["PYTHONPATH"] = current_pythonpath
+from picsellia import Experiment
+from picsellia.sdk.log import LogType
 
 
 def extract_and_log_metrics(log_line):
@@ -72,26 +33,80 @@ def extract_and_log_metrics(log_line):
                     metrics[key] = float(value)
                 else:
                     metrics[key] = int(value)
+                if key == "epoch":
+                    metrics[key] = int(value.replace("[", "").split("/")[0])
             except ValueError:
                 metrics[key] = value  # Keep as string if it can't be converted
 
-    print("Metrics Parsed:", metrics)
-    # Optionally, send these metrics to your monitoring or logging platform
-    # log_metrics_to_platform(metrics)
+    return metrics
 
 
 class PaddleOCRModelTrainer:
-    def __init__(self, model_collection: PaddleOCRModelCollection):
+    def __init__(
+        self, model_collection: PaddleOCRModelCollection, experiment: Experiment
+    ):
         self.model_collection = model_collection
+        self.experiment = experiment
 
     def train(self):
         """
         Trains both the bounding box detection and text recognition models in the model collection.
         """
         print("Starting training for bounding box model...")
-        train_model(self.model_collection.bbox_model.config_file_path)
+        self.train_model(model_context=self.model_collection.bbox_model)
 
         print("Starting training for text recognition model...")
-        train_model(self.model_collection.text_model.config_file_path)
+        self.train_model(model_context=self.model_collection.text_model)
 
         return self.model_collection
+
+    def train_model(self, model_context: ModelContext):
+        """
+        Trains a Paddle OCR model given a configuration path and captures the output metrics line by line.
+
+        Args:
+            prefix_model_name: The prefix of the model name to train (either "bbox" or "text").
+        """
+        current_pythonpath = os.environ.get("PYTHONPATH", "")
+        os.environ["PYTHONPATH"] = f".:{current_pythonpath}"
+
+        config_path = model_context.config_file_path
+        if not config_path:
+            raise ValueError("No configuration file path found in model context")
+
+        command = [
+            "python3",
+            "src/pipelines/paddle_ocr/PaddleOCR/tools/train.py",
+            "-c",
+            config_path,
+        ]
+
+        joined_command = " ".join(command)
+
+        process = subprocess.Popen(
+            joined_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True
+        )
+
+        try:
+            if process.stdout:
+                for line in iter(process.stdout.readline, ""):
+                    if "epoch:" in line:
+                        metrics = extract_and_log_metrics(line)
+                        for key, value in metrics.items():
+                            if isinstance(value, (int, float)):
+                                self.experiment.log(
+                                    name=f"{model_context.prefix_model_name}_{key}",
+                                    data=value,
+                                    type=LogType.LINE,
+                                )
+        except Exception as e:
+            print("Error during model training:", e)
+
+        process.wait()
+        if process.returncode != 0:
+            print("Training failed with errors")
+            if process.stderr:
+                errors = process.stderr.read()
+                print(errors)
+
+        os.environ["PYTHONPATH"] = current_pythonpath
