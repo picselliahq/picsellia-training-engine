528c1606806c215c1d0b65277c3db39e778ff53f
SoniaGrh <sonia.garrouch@hotmail.fr>
2024-07-04 18:35:18 +0200

    fix: adapt annotations for bbox and text

--- src/models/steps/data_preparation/training/paddle_ocr_dataset_context_preparator.py
+++ src/models/steps/data_preparation/training/paddle_ocr_dataset_context_preparator.py
@@ -2,19 +2,17 @@ import json
 import os
 from typing import Union, Dict, List
 
+import numpy as np
+
 from src.models.dataset.common.dataset_collection import TDatasetContext
 from src.models.dataset.common.paddle_ocr_dataset_context import PaddleOCRDatasetContext
 
-
-def write_class_file(coco_data: Dict, class_file_path: str):
-    categories = [category["name"] for category in coco_data["categories"]]
-    with open(class_file_path, "w", encoding="utf-8") as file:
-        for category in sorted(categories):
-            file.write(category.upper() + "\n")
+from imutils import perspective
+import cv2
 
 
 def write_annotations_file(data, output_path):
-    with open(output_path, "w") as file:
+    with open(output_path, "w", encoding="utf-8") as file:
         for line in data:
             file.write(line + "\n")
 
@@ -36,7 +34,6 @@ def find_category_name(categories: List[Dict], category_id: str) -> Union[str, N
 def find_image_id(images: List[Dict], image_filename: str) -> Union[str, None]:
     for image in images:
         if image["file_name"] == image_filename:
-            print(f'Found image id {image["id"]} for image {image_filename}')
             return image["id"]
     return None
 
@@ -51,34 +48,63 @@ def load_coco_text(file_path: str) -> Dict:
         return json.load(file)
 
 
-def process_annotations(coco: Dict, image_directory: str):
+def get_bbox_annotations(coco: Dict, image_directory: str):
     processed_data: List[str] = []
     paddle_ocr_annotations: List[Dict] = []
     group_image_id = None
-    for annotation in coco["annotations"]:
-        print(f'Processing annotation {annotation["id"]}')
-        current_image_id = annotation["image_id"]
-        print(f"Current image id: {current_image_id}")
-        if not group_image_id:
-            group_image_id = current_image_id
-        if group_image_id and current_image_id != group_image_id:
+
+    def append_annotations():
+        if group_image_id is not None:
             image_path = os.path.join(
                 image_directory, coco["images"][group_image_id]["file_name"]
             )
-            processed_data.append(f"{image_path}\t{json.dumps(paddle_ocr_annotations)}")
+            processed_data.append(
+                f"{image_path}\t{json.dumps(paddle_ocr_annotations, ensure_ascii=False)}"
+            )
+
+    for annotation in coco["annotations"]:
+        current_image_id = annotation["image_id"]
+        if group_image_id is None:
+            group_image_id = current_image_id
+
+        if current_image_id != group_image_id:
+            append_annotations()
             paddle_ocr_annotations = []
             group_image_id = current_image_id
-        else:
-            paddle_ocr_annotation = {
-                "transcription": annotation["utf8_string"],
-                "label": find_category_name(
-                    coco["categories"], annotation["category_id"]
-                ),
-                "points": get_points_from_bbox(annotation["bbox"]),
-                "id": annotation["id"],
-                "linking": annotation.get("linking", []),
-            }
-            paddle_ocr_annotations.append(paddle_ocr_annotation)
+
+        paddle_ocr_annotation = {
+            "transcription": annotation["utf8_string"],
+            "points": get_points_from_bbox(annotation["bbox"]),
+        }
+        paddle_ocr_annotations.append(paddle_ocr_annotation)
+
+    append_annotations()
+
+    return processed_data
+
+
+def get_text_annotations(coco: Dict, image_directory: str, new_image_directory: str):
+    processed_data: List[str] = []
+    img_counter = 0
+
+    for annotation in coco["annotations"]:
+        current_image_id = annotation["image_id"]
+        image_path = os.path.join(
+            image_directory, coco["images"][current_image_id]["file_name"]
+        )
+
+        points = get_points_from_bbox(annotation["bbox"])
+
+        formatted_points = np.asarray([(x, y) for x, y in points], dtype=np.float32)
+        image = cv2.imread(image_path)
+        warped = perspective.four_point_transform(image, formatted_points)
+        new_image_filename = "img_" + str(img_counter) + ".png"
+        new_image_path = os.path.join(new_image_directory, new_image_filename)
+        cv2.imwrite(new_image_path, warped)
+
+        processed_data.append(f"{new_image_path}\t{annotation['utf8_string']}")
+        img_counter = img_counter + 1
+
     return processed_data
 
 
@@ -91,15 +117,7 @@ class PaddleOCRDatasetContextPreparator:
             dataset_context (DatasetContext): The dataset context to organize.
         """
         self.dataset_context = dataset_context
-        self.destination_path = dataset_context.destination_path
         self.paddle_ocr_dataset_context = PaddleOCRDatasetContext(
-            paddle_ocr_annotations_path=os.path.join(
-                self.destination_path, f"{self.dataset_context.dataset_name}.txt"
-            ),
-            paddle_ocr_class_path=os.path.join(
-                self.destination_path,
-                f"{self.dataset_context.dataset_name}_class_list.txt",
-            ),
             dataset_name=self.dataset_context.dataset_name,
             dataset_version=self.dataset_context.dataset_version,
             destination_path=self.dataset_context.destination_path,
@@ -110,14 +128,21 @@ class PaddleOCRDatasetContextPreparator:
 
     def organize(self) -> PaddleOCRDatasetContext:
         coco_data = load_coco_text(self.dataset_context.coco_file_path)
-        paddleocr_annotations = process_annotations(
-            coco_data, self.dataset_context.image_dir
+        paddleocr_bbox_annotations = get_bbox_annotations(
+            coco=coco_data, image_directory=self.dataset_context.image_dir
+        )
+        paddleocr_text_annotations = get_text_annotations(
+            coco=coco_data,
+            image_directory=self.dataset_context.image_dir,
+            new_image_directory=self.paddle_ocr_dataset_context.text_image_dir,
+        )
+
+        write_annotations_file(
+            paddleocr_bbox_annotations,
+            self.paddle_ocr_dataset_context.paddle_ocr_bbox_annotations_path,
         )
         write_annotations_file(
-            paddleocr_annotations,
-            self.paddle_ocr_dataset_context.paddle_ocr_annotations_path,
-        )
-        write_class_file(
-            coco_data, self.paddle_ocr_dataset_context.paddle_ocr_class_path
+            paddleocr_text_annotations,
+            self.paddle_ocr_dataset_context.paddle_ocr_text_annotations_path,
         )
         return self.paddle_ocr_dataset_context
