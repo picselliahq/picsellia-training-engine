bb8db949226e5f4eb1c933b001eac1b94de0ea19
SoniaGrh <sonia.garrouch@hotmail.fr>
2024-07-04 18:44:56 +0200

    fix: use experiment

--- src/steps/model_training/paddle_ocr_trainer.py
+++ src/steps/model_training/paddle_ocr_trainer.py
@@ -1,11 +1,17 @@
+from src import step, Pipeline
+from src.models.contexts.training.picsellia_training_context import (
+    PicselliaTrainingContext,
+)
 from src.models.steps.model_training.paddle_ocr_model_trainer import (
     PaddleOCRModelTrainer,
 )
 
 
+@step
 def paddle_ocr_trainer(model_collection):
-    model_trainer = PaddleOCRModelTrainer(model_collection=model_collection)
-
+    context: PicselliaTrainingContext = Pipeline.get_active_context()
+    model_trainer = PaddleOCRModelTrainer(
+        model_collection=model_collection, experiment=context.experiment
+    )
     model_collection = model_trainer.train()
-
     return model_collection
