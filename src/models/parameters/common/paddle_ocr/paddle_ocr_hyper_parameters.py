4c3c1932b6de4c6d09e89289207662651e6c973c
SoniaGrh <sonia.garrouch@hotmail.fr>
2024-07-04 15:49:21 +0200

    feat: add paddle ocr hyperparameters

---
+++ src/models/parameters/common/paddle_ocr/paddle_ocr_hyper_parameters.py
@@ -0,0 +1,37 @@
+from picsellia.types.schemas import LogDataType
+
+from src.models.parameters.common.parameters import Parameters
+
+
+class PaddleOCRHyperParameters(Parameters):
+    def __init__(self, log_data: LogDataType):
+        super().__init__(log_data=log_data)
+        self.train_set_split_ratio = self.extract_parameter(
+            keys=["prop_train_split", "train_set_split_ratio"],
+            expected_type=float,
+            default=0.8,
+        )
+        self.bbox_epochs = self.extract_parameter(
+            keys=["bbox_epoch"], expected_type=int, default=100
+        )
+        self.text_epochs = self.extract_parameter(
+            keys=["text_epoch"], expected_type=int, default=100
+        )
+        self.bbox_batch_size = self.extract_parameter(
+            keys=["bbox_batch_size"], expected_type=int, default=8
+        )
+        self.text_batch_size = self.extract_parameter(
+            keys=["text_batch_size"], expected_type=int, default=8
+        )
+        self.bbox_learning_rate = self.extract_parameter(
+            keys=["bbox_learning_rate"], expected_type=float, default=0.001
+        )
+        self.text_learning_rate = self.extract_parameter(
+            keys=["text_learning_rate"], expected_type=float, default=0.001
+        )
+        self.bbox_save_epoch_step = self.extract_parameter(
+            keys=["bbox_save_epoch_step"], expected_type=int, default=10
+        )
+        self.text_save_epoch_step = self.extract_parameter(
+            keys=["text_save_epoch_step"], expected_type=int, default=10
+        )
