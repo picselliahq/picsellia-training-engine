1ef6eb550d156d338dc4b2b2f54e80437dd113f5
picsalex <132259399+picsalex@users.noreply.github.com>
2024-07-08 14:59:11 +0200

    fix: issues with imports and some edge cases

--- src/steps/data_validation/processing/processing_tiler_data_validator.py
+++ src/steps/data_validation/processing/processing_tiler_data_validator.py
@@ -9,6 +9,9 @@ from src.models.dataset.common.dataset_context import DatasetContext
 from src.models.parameters.processing.processing_tiler_parameters import (
     ProcessingTilerParameters,
 )
+from src.models.steps.data_validation.common.not_configured_dataset_context_validator import (
+    NotConfiguredDatasetContextValidator,
+)
 from src.models.steps.data_validation.common.object_detection_dataset_context_validator import (
     ObjectDetectionDatasetContextValidator,
 )
@@ -28,23 +31,35 @@ def tiler_data_validator(
         ProcessingTilerParameters
     ] = Pipeline.get_active_context()
 
-    if dataset_context.dataset_version.type == InferenceType.SEGMENTATION:
-        segmentation_dataset_validator = SegmentationDatasetContextValidator(
-            dataset_context=dataset_context
-        )
-        segmentation_dataset_validator.validate()
-    if (
-        dataset_context.dataset_version.type == InferenceType.OBJECT_DETECTION
-        or dataset_context.dataset_version.type == InferenceType.SEGMENTATION
-    ):
-        object_detection_dataset_validator = ObjectDetectionDatasetContextValidator(
-            dataset_context=dataset_context
-        )
-        object_detection_dataset_validator.validate()
-    else:
-        raise ValueError(
-            f"Dataset type {dataset_context.dataset_version.type} is not supported."
-        )
+    match dataset_context.dataset_version.type:
+        case InferenceType.NOT_CONFIGURED:
+            not_configured_dataset_validator = NotConfiguredDatasetContextValidator(
+                dataset_context=dataset_context
+            )
+            not_configured_dataset_validator.validate()
+
+        case InferenceType.SEGMENTATION:
+            object_detection_dataset_validator = ObjectDetectionDatasetContextValidator(
+                dataset_context=dataset_context
+            )
+            segmentation_dataset_validator = SegmentationDatasetContextValidator(
+                dataset_context=dataset_context
+            )
+
+            # SAHI requires bot the bounding boxes and the polygons to be valid
+            object_detection_dataset_validator.validate()
+            segmentation_dataset_validator.validate()
+
+        case InferenceType.OBJECT_DETECTION:
+            object_detection_dataset_validator = ObjectDetectionDatasetContextValidator(
+                dataset_context=dataset_context
+            )
+            object_detection_dataset_validator.validate()
+
+        case _:
+            raise ValueError(
+                f"Dataset type {dataset_context.dataset_version.type} is not supported."
+            )
 
     processing_validator = ProcessingTilerDataValidator(
         client=context.client,
