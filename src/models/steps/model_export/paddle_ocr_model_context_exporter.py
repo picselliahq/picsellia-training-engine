b9d2dd0d8071437be7e3084c18dabf9282a1cd91
SoniaGrh <sonia.garrouch@hotmail.fr>
2024-07-19 11:44:13 +0200

    fix: export the right model

--- src/models/steps/model_export/paddle_ocr_model_context_exporter.py
+++ src/models/steps/model_export/paddle_ocr_model_context_exporter.py
@@ -1,6 +1,7 @@
 import os
 import subprocess
 
+import yaml
 from picsellia import Experiment
 
 from src.models.model.model_context import ModelContext
@@ -19,22 +20,49 @@ class PaddleOCRModelContextExporter(ModelContextExporter):
         if not config_path:
             raise ValueError("No configuration file path found in model context")
 
-        command = [
-            "python3.10",
-            "src/pipelines/paddle_ocr/PaddleOCR/tools/export_model.py",
-            "-c",
-            config_path,
-        ]
+        with open(config_path, "r") as file:
+            config = yaml.load(file, Loader=yaml.FullLoader)
 
-        process = subprocess.Popen(
-            command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True
-        )
+        save_model_dir = config["Global"]["save_model_dir"]
 
-        process.wait()
-        if process.returncode != 0:
-            print("Export failed with errors")
-            if process.stderr:
-                errors = process.stderr.read()
-                print(errors)
+        found_best_accuracy = False
+        found_latest = False
+        if config["Global"]["save_model_dir"]:
+            for file in os.listdir(save_model_dir):
+                if file.startswith("best_accuracy"):
+                    found_best_accuracy = True
+                if file.startswith("latest"):
+                    found_latest = True
 
-        os.environ["PYTHONPATH"] = current_pythonpath
+        if not found_best_accuracy and not found_latest:
+            print(f"No model found in {save_model_dir}, skipping export...")
+        elif not found_best_accuracy:
+            config["Global"]["pretrained_model"] = f"{save_model_dir}/latest"
+        else:
+            config["Global"]["pretrained_model"] = f"{save_model_dir}/best_accuracy"
+
+        with open(config_path, "w") as file:
+            yaml.dump(config, file)
+
+        if found_latest or found_best_accuracy:
+            command = [
+                "python3.10",
+                "src/pipelines/paddle_ocr/PaddleOCR/tools/export_model.py",
+                "-c",
+                config_path,
+            ]
+
+            os.setuid(os.geteuid())
+
+            process = subprocess.Popen(
+                command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True
+            )
+
+            process.wait()
+            if process.returncode != 0:
+                print("Export failed with errors")
+                if process.stderr:
+                    errors = process.stderr.read()
+                    print(errors)
+
+            os.environ["PYTHONPATH"] = current_pythonpath
