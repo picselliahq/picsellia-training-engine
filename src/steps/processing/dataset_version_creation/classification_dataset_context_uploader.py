0a3908d4d68dc4a0e231aa99999d6503f5cc0b07
SoniaGrh <sonia.garrouch@hotmail.fr>
2024-07-25 18:14:01 +0200

    feat: define classif ds uploader

---
+++ src/steps/processing/dataset_version_creation/classification_dataset_context_uploader.py
@@ -0,0 +1,24 @@
+from src import Pipeline
+from src import step
+from src.models.contexts.processing.picsellia_processing_context import (
+    PicselliaProcessingContext,
+)
+from src.models.dataset.common.dataset_context import DatasetContext
+from src.models.steps.processing.dataset_version_creation.classification_dataset_context_uploader import (
+    ClassificationDatasetContextUploader,
+)
+
+
+@step
+def classification_dataset_context_uploader(dataset_context: DatasetContext):
+    context: PicselliaProcessingContext = Pipeline.get_active_context()
+    uploader = ClassificationDatasetContextUploader(
+        client=context.client,
+        dataset_context=dataset_context,
+        datalake=context.processing_parameters.datalake,
+        data_tags=[
+            context.processing_parameters.images_tag,
+            dataset_context.dataset_version.version,
+        ],
+    )
+    uploader.upload_dataset_context()
