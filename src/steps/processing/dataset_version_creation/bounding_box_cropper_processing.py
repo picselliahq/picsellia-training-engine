e69b5f421519ddb21135b7d1e665f1b5e1761235
SoniaGrh <sonia.garrouch@hotmail.fr>
2024-07-25 18:12:47 +0200

    fix: refacto processing

--- src/steps/processing/dataset_version_creation/bounding_box_cropper_processing.py
+++ src/steps/processing/dataset_version_creation/bounding_box_cropper_processing.py
@@ -1,11 +1,12 @@
-import os
-
 from src import Pipeline
 from src import step
 from src.models.contexts.processing.picsellia_processing_context import (
     PicselliaProcessingContext,
 )
 from src.models.dataset.common.dataset_context import DatasetContext
+from src.models.dataset.processing.processing_dataset_collection import (
+    ProcessingDatasetCollection,
+)
 from src.models.parameters.processing.processing_bounding_box_cropper_parameters import (
     ProcessingBoundingBoxCropperParameters,
 )
@@ -15,17 +16,16 @@ from src.models.steps.processing.dataset_version_creation.bounding_box_cropper_p
 
 
 @step
-def bounding_box_cropper_processing(dataset_context: DatasetContext):
+def bounding_box_cropper_processing(
+    dataset_collection: ProcessingDatasetCollection,
+) -> DatasetContext:
     context: PicselliaProcessingContext[
         ProcessingBoundingBoxCropperParameters
     ] = Pipeline.get_active_context()
 
     processor = BoundingBoxCropperProcessing(
-        client=context.client,
-        datalake=context.client.get_datalake(context.processing_parameters.datalake),
-        input_dataset_context=dataset_context,
+        dataset_collection=dataset_collection,
         label_name_to_extract=context.processing_parameters.label_name_to_extract,
-        output_dataset_version=context.output_dataset_version,
-        destination_path=os.path.join(os.getcwd(), str(context.job_id)),
     )
-    processor.process()
+    dataset_collection = processor.process()
+    return dataset_collection.output
