442b26c13d1b4f0eb7cf476be2944cf5bc338088
SoniaGrh <sonia.garrouch@hotmail.fr>
2024-07-25 10:35:20 +0200

    fix: import

--- src/steps/processing/pre_annotation/easyocr_processing.py
+++ src/steps/processing/pre_annotation/easyocr_processing.py
@@ -1,16 +1,14 @@
-import os
-
 from src import Pipeline, step
-from src.models.contexts.picsellia_context import PicselliaProcessingContext
+from src.models.contexts.processing.picsellia_processing_context import (
+    PicselliaProcessingContext,
+)
 from src.models.dataset.common.dataset_context import DatasetContext
-from src.models.parameters.processing.processing_bounding_box_cropper_parameters import (
-    ProcessingBoundingBoxCropperParameters,
+from src.models.parameters.processing.processing_easyorc_parameters import (
+    ProcessingEasyOcrParameters,
 )
-from src.models.parameters.processing.processing_easyorc_parameters import ProcessingEasyOcrParameters
-from src.models.steps.processing.dataset_version_creation.bounding_box_cropper_processing import (
-    BoundingBoxCropperProcessing,
+from src.models.steps.processing.pre_annotation.easyocr_processing import (
+    EasyOcrProcessing,
 )
-from src.models.steps.processing.pre_annotation.easyocr_processing import EasyOcrProcessing
 
 
 @step
