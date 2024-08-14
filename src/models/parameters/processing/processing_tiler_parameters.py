efafd261c23f1998a8e8bbd5f584de7cdf252d5a
SoniaGrh <sonia.garrouch@hotmail.fr>
2024-08-14 18:32:39 +0200

    fix: add range value

--- src/models/parameters/processing/processing_tiler_parameters.py
+++ src/models/parameters/processing/processing_tiler_parameters.py
@@ -10,15 +10,26 @@ class ProcessingTilerParameters(Parameters):
         )
         self.data_tag = self.extract_parameter(keys=["data_tag"], expected_type=str)
         self.tile_height = self.extract_parameter(
-            keys=["tile_height"], expected_type=int
+            keys=["tile_height"], expected_type=int, range_value=(0, int("inf"))
         )
-        self.tile_width = self.extract_parameter(keys=["tile_width"], expected_type=int)
+        self.tile_width = self.extract_parameter(
+            keys=["tile_width"], expected_type=int, range_value=(0, int("inf"))
+        )
         self.overlap_height_ratio = self.extract_parameter(
-            keys=["overlap_height_ratio"], expected_type=float, default=0.1
+            keys=["overlap_height_ratio"],
+            expected_type=float,
+            default=0.1,
+            range_value=(0, 0.99),
         )
         self.overlap_width_ratio = self.extract_parameter(
-            keys=["overlap_width_ratio"], expected_type=float, default=0.1
+            keys=["overlap_width_ratio"],
+            expected_type=float,
+            default=0.1,
+            range_value=(0, 0.99),
         )
         self.min_area_ratio = self.extract_parameter(
-            keys=["min_area_ratio"], expected_type=float, default=0.1
+            keys=["min_area_ratio"],
+            expected_type=float,
+            default=0.1,
+            range_value=(0, 0.99),
         )
