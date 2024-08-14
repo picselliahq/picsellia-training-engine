08c14c7f258c548561104ac3bfae785513ed4755
SoniaGrh <sonia.garrouch@hotmail.fr>
2024-08-14 18:03:05 +0200

    feat: update pipeline

--- src/pipelines/yolov8_classification/training_pipeline.py
+++ src/pipelines/yolov8_classification/training_pipeline.py
@@ -11,11 +11,19 @@ from src.models.parameters.common.hyper_parameters import UltralyticsHyperParame
 from src.steps.data_extraction.training.training_data_extractor import (
     training_data_extractor,
 )
-from src.steps.data_preparation.common.classification_data_preparator import (
-    classification_data_preparator,
+from src.steps.data_preparation.training.ultralytics_classification_data_preparator import (
+    ultralytics_classification_data_preparator,
 )
 from src.steps.data_validation.common.classification_data_validator import (
     classification_data_validator,
+)
+from src.steps.model_evaluation.ultralytics_model_evaluator import (
+    ultralytics_model_evaluator,
+)
+from src.steps.model_export.ultralytics_model_exporter import ultralytics_model_exporter
+from src.steps.model_training.ultralytics_trainer import ultralytics_trainer
+from src.steps.weights_extraction.training.ultralytics_weights_extractor import (
+    ultralytics_weights_extractor,
 )
 
 
@@ -36,12 +44,19 @@ def get_context() -> (
     remove_logs_on_completion=False,
 )
 def yolov8_classification_training_pipeline():
-    # Data pipeline
-    dataset_collection = training_data_extractor()
-    dataset_collection = classification_data_preparator(
+    dataset_collection = training_data_extractor(random_seed=2)
+    dataset_collection = ultralytics_classification_data_preparator(
         dataset_collection=dataset_collection
     )
     classification_data_validator(dataset_collection=dataset_collection)
+    model_context = ultralytics_weights_extractor()
+    model_context = ultralytics_trainer(
+        model_context=model_context, dataset_collection=dataset_collection
+    )
+    model_context = ultralytics_model_exporter(model_context)
+    ultralytics_model_evaluator(
+        model_context=model_context, dataset_context=dataset_collection["test"]
+    )
 
 
 if __name__ == "__main__":
