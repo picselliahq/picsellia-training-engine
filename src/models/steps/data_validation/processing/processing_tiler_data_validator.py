d924cbd991e78450c72373d6bbfc250c8070c78d
SoniaGrh <sonia.garrouch@hotmail.fr>
2024-08-19 13:28:39 +0200

    feat: add ratios validation

--- src/models/steps/data_validation/processing/processing_tiler_data_validator.py
+++ src/models/steps/data_validation/processing/processing_tiler_data_validator.py
@@ -7,11 +7,17 @@ class ProcessingTilerDataValidator:
         client: Client,
         tile_height: int,
         tile_width: int,
+        overlap_height_ratio: float,
+        overlap_width_ratio: float,
+        min_area_ratio: float,
         datalake: str,
     ):
         self.client = client
         self.tile_height = tile_height
         self.tile_width = tile_width
+        self.overlap_height_ratio = overlap_height_ratio
+        self.overlap_width_ratio = overlap_width_ratio
+        self.min_area_ratio = min_area_ratio
         self.datalake = datalake
 
     def _validate_tile_size(self) -> None:
@@ -23,6 +29,20 @@ class ProcessingTilerDataValidator:
         """
         if self.tile_height <= 0 or self.tile_width <= 0:
             raise ValueError("Slice size must be greater than 0")
+
+    def _validate_ratios(self) -> None:
+        """
+        Validate that the overlap and min area ratios are within the acceptable range.
+
+        Raises:
+            ValueError: If any of the ratios are out of the acceptable range.
+        """
+        if not (0 <= self.overlap_height_ratio < 1):
+            raise ValueError("overlap_height_ratio must be between 0 and 0.99")
+        if not (0 <= self.overlap_width_ratio < 1):
+            raise ValueError("overlap_width_ratio must be between 0 and 0.99")
+        if not (0 <= self.min_area_ratio < 1):
+            raise ValueError("min_area_ratio must be between 0 and 0.99")
 
     def _validate_datalake(self) -> None:
         """
@@ -38,5 +58,9 @@ class ProcessingTilerDataValidator:
             )
 
     def validate(self) -> None:
+        """
+        Validate the tiler parameters.
+        """
         self._validate_tile_size()
+        self._validate_ratios()
         self._validate_datalake()
