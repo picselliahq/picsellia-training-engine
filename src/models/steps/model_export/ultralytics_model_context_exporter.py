e4c5f8a93a55e3d715ae4ce86fcbefa86255ff39
SoniaGrh <sonia.garrouch@hotmail.fr>
2024-08-27 10:46:00 +0200

    fix: find the latest experiment dir

--- src/models/steps/model_export/ultralytics_model_context_exporter.py
+++ src/models/steps/model_export/ultralytics_model_context_exporter.py
@@ -3,7 +3,9 @@ import shutil
 
 from picsellia import Experiment
 
-from src.models.model.ultralytics_model_context import UltralyticsModelContext
+from src.models.model.ultralytics.ultralytics_model_context import (
+    UltralyticsModelContext,
+)
 from src.models.steps.model_export.model_context_exporter import ModelContextExporter
 
 
@@ -13,11 +15,22 @@ class UltralyticsModelContextExporter(ModelContextExporter):
 
     def export_model_context(self):
         self.model_context.loaded_model.export(format="onnx")
-        # move onnx to inference path
+        results_dir = os.listdir(self.model_context.results_path)
+        if len(results_dir) == 0:
+            raise ValueError("No results folder found")
+        elif len(results_dir) == 1:
+            model_folder = results_dir[0]
+        else:
+            model_folder = sorted(
+                [
+                    f
+                    for f in results_dir
+                    if f.startswith(self.model_context.model_name) and f[-1].isdigit()
+                ]
+            )[-1]
         exported_model_dir = os.path.join(
-            self.model_context.results_path, self.model_context.model_name, "weights"
+            self.model_context.results_path, model_folder, "weights"
         )
-        # find the onnx file
         onnx_file = [f for f in os.listdir(exported_model_dir) if f.endswith(".onnx")]
         if len(onnx_file) == 0:
             raise ValueError("No onnx file found")
