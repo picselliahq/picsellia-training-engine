1f07d63811903b5b5c0a05c5d10d40b9a0be5b65
SoniaGrh <sonia.garrouch@hotmail.fr>
2024-09-11 16:35:25 +0200

    fix: naming

--- src/steps/model_export/ultralytics_model_exporter.py
+++ src/steps/model_export/ultralytics_model_exporter.py
@@ -23,11 +23,14 @@ def ultralytics_model_exporter(model_context: ModelContext):
     model_context_exporter = UltralyticsModelContextExporter(
         model_context=model_context, experiment=context.experiment
     )
-    model_context_exporter.export_model_context(
-        exported_model_destination_path=model_context.inference_model_dir,
-        export_format=context.export_parameters.export_format,
-    )
-    model_context_exporter.save_model_to_experiment(
-        exported_model_dir=model_context.inference_model_dir,
-        saved_model_name="model-latest",
-    )
+    if model_context.exported_weights_dir:
+        model_context_exporter.export_model_context(
+            exported_weights_destination_path=model_context.exported_weights_dir,
+            export_format=context.export_parameters.export_format,
+        )
+        model_context_exporter.save_model_to_experiment(
+            exported_weights_dir=model_context.exported_weights_dir,
+            exported_weights_name="model-latest",
+        )
+    else:
+        print("No exported weights directory found in model context. Skipping export.")
