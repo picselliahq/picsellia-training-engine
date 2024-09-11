e7dc11137e4c14ad7de14bd207fad32368c64dc3
SoniaGrh <sonia.garrouch@hotmail.fr>
2024-09-11 16:30:36 +0200

    fix: consistency in naming

--- src/models/steps/model_export/model_context_exporter.py
+++ src/models/steps/model_export/model_context_exporter.py
@@ -1,6 +1,5 @@
 import os
 from abc import abstractmethod
-from typing import List
 
 from picsellia import Experiment
 from src.models.model.common.model_context import ModelContext
@@ -28,7 +27,9 @@ class ModelContextExporter:
         """
         pass
 
-    def save_model_to_experiment(self, exported_model_dir: str, saved_model_name: str):
+    def save_model_to_experiment(
+        self, exported_weights_dir: str, exported_weights_name: str
+    ):
         """
         Saves the exported model to the experiment.
 
@@ -39,33 +40,21 @@ class ModelContextExporter:
             ValueError: If no model files are found in the inference model path.
         """
 
-        exported_model_files = os.listdir(exported_model_dir)
-        if not exported_model_files:
+        exported_files = os.listdir(exported_weights_dir)
+        if not exported_files:
             raise ValueError("No model files found in the exported model directory")
 
-        self._store_model_files(saved_model_name, exported_model_files)
-
-    def _store_model_files(
-        self, saved_model_name: str, exported_model_files: List[str]
-    ) -> None:
-        """
-        Stores the model files in the experiment.
-
-        Args:
-            saved_model_name (str): The name under which the model will be saved.
-            exported_model_files (list): The list of files in the inference model path.
-        """
-        if len(exported_model_files) > 1:
+        if len(exported_files) > 1:
             self.experiment.store(
-                name=saved_model_name,
-                path=self.model_context.inference_model_dir,
+                name=exported_weights_name,
+                path=exported_weights_dir,
                 do_zip=True,
             )
         else:
             self.experiment.store(
-                name=saved_model_name,
+                name=exported_weights_name,
                 path=os.path.join(
-                    str(self.model_context.inference_model_dir),
-                    exported_model_files[0],
+                    exported_weights_dir,
+                    exported_files[0],
                 ),
             )
--- src/models/steps/model_export/ultralytics_model_context_exporter.py
+++ src/models/steps/model_export/ultralytics_model_context_exporter.py
@@ -16,93 +16,103 @@ class UltralyticsModelContextExporter(ModelContextExporter):
     """
 
     def __init__(self, model_context: ModelContext, experiment: Experiment):
+        """
+        Initializes the UltralyticsModelContextExporter.
+
+        Args:
+            model_context (ModelContext): The model context containing information about the model and its paths.
+            experiment (Experiment): The experiment associated with the model context.
+        """
         super().__init__(model_context=model_context, experiment=experiment)
 
     def export_model_context(
-        self, exported_model_destination_path: str, export_format: str
+        self, exported_weights_destination_path: str, export_format: str
     ) -> None:
         """
-        Exports the Ultralytics model context by converting it to the ONNX format and ensuring
-        that the resulting file is placed in the inference model path for subsequent saving.
+        Exports the Ultralytics model context by converting it to the specified format (typically ONNX)
+        and moves the resulting file to the specified destination path.
+
+        Args:
+            exported_weights_destination_path (str): The path where the exported model weights should be saved.
+            export_format (str): The format to export the model (e.g., ONNX).
 
         Raises:
             ValueError: If no results folder or ONNX file is found.
         """
         self._export_model(export_format=export_format)
 
-        model_folder = self._find_model_folder()
-        exported_model_dir = os.path.join(
-            self.model_context.results_dir, model_folder, "weights"
-        )
-        onnx_file = self._find_onnx_file(exported_model_dir)
+        onnx_file_path = self._find_exported_onnx_file()
 
         self._move_onnx_to_destination_path(
-            onnx_file_path=os.path.join(exported_model_dir, onnx_file),
-            exported_model_destination_path=exported_model_destination_path,
+            onnx_file_path=onnx_file_path,
+            exported_weights_destination_path=exported_weights_destination_path,
         )
 
     def _export_model(self, export_format: str) -> None:
         """
-        Exports the model to ONNX format directly into the inference model path.
+        Exports the loaded model in the specified format (e.g., ONNX) to the inference model path.
+
+        Args:
+            export_format (str): The format to export the model in.
         """
         self.model_context.loaded_model.export(format=export_format)
 
-    def _find_model_folder(self) -> str:
+    def _find_ultralytics_results_dir(self) -> str:
         """
-        Finds the correct model folder within the results directory.
+        Locates the appropriate results folder within the model's results directory.
 
         Returns:
-            str: The name of the model folder.
+            str: The full path to the results folder.
 
         Raises:
-            ValueError: If no results folder is found.
+            ValueError: If no results folder is found in the results directory.
         """
-        results_dir = os.listdir(self.model_context.results_dir)
-        if not results_dir:
+        results_dirs = os.listdir(self.model_context.results_dir)
+        if not results_dirs:
             raise ValueError("No results folder found")
-        elif len(results_dir) == 1:
-            return results_dir[0]
+        elif len(results_dirs) == 1:
+            return os.path.join(self.model_context.results_dir, results_dirs[0])
 
-        return sorted(
-            [
-                f
-                for f in results_dir
-                if f.startswith(self.model_context.model_name) and f[-1].isdigit()
-            ]
-        )[-1]
+        return os.path.join(
+            self.model_context.results_dir,
+            sorted(
+                [
+                    f
+                    for f in results_dirs
+                    if f.startswith(self.model_context.model_name) and f[-1].isdigit()
+                ]
+            )[-1],
+        )
 
-    def _find_onnx_file(self, exported_model_dir: str) -> str:
+    def _find_exported_onnx_file(self) -> str:
         """
-        Finds the ONNX file in the specified directory.
-
-        Args:
-            exported_model_dir (str): The directory to search for the ONNX file.
+        Searches for the ONNX file in the weights directory of the Ultralytics results folder.
 
         Returns:
-            str: The name of the ONNX file.
+            str: The full path to the ONNX file.
 
         Raises:
-            ValueError: If no ONNX file is found.
+            ValueError: If no ONNX file is found in the weights directory.
         """
-        onnx_files = [f for f in os.listdir(exported_model_dir) if f.endswith(".onnx")]
+        ultralytics_results_dir = self._find_ultralytics_results_dir()
+        ultralytics_weights_dir = os.path.join(ultralytics_results_dir, "weights")
+        onnx_files = [
+            f for f in os.listdir(ultralytics_weights_dir) if f.endswith(".onnx")
+        ]
         if not onnx_files:
             raise ValueError("No ONNX file found")
-        return onnx_files[0]
+        return os.path.join(ultralytics_weights_dir, onnx_files[0])
 
     def _move_onnx_to_destination_path(
-        self, onnx_file_path: str, exported_model_destination_path: str
+        self, onnx_file_path: str, exported_weights_destination_path: str
     ) -> None:
         """
-        Moves the ONNX file to the specified destination path.
+        Moves the ONNX file from its current location to the specified destination path.
 
         Args:
-            onnx_file_path:
-            exported_model_destination_path:
-
-        Returns:
+            onnx_file_path (str): The full path to the ONNX file.
+            exported_weights_destination_path (str): The destination path where the ONNX file should be moved.
 
         """
-        print(
-            f"Moving ONNX file to inference model path: {exported_model_destination_path}"
-        )
-        shutil.move(onnx_file_path, exported_model_destination_path)
+        print(f"Moving ONNX file to {exported_weights_destination_path}...")
+        shutil.move(onnx_file_path, exported_weights_destination_path)
