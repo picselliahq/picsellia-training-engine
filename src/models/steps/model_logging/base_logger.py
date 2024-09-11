46b657a019295555e81f75d441dd6dc95dd82db7
SoniaGrh <sonia.garrouch@hotmail.fr>
2024-09-11 16:11:16 +0200

    fix: remove unnecessary functions

--- src/models/steps/model_logging/base_logger.py
+++ src/models/steps/model_logging/base_logger.py
@@ -5,39 +5,6 @@ from picsellia import Experiment
 from picsellia.types.enums import LogType
 
 
-# class MetricStrategy:
-#     def __init__(self, phase: str, metric_name: str, value: float):
-#         self.phase = phase
-#         self.metric_name = metric_name
-#         self.value = value
-#
-#     def log(self, logger: "BaseLogger"):
-#         """
-#         Détermine automatiquement comment loguer la métrique en fonction de la phase et du type de métrique.
-#         """
-#         if self.phase == "train":
-#             if "loss" in self.metric_name:
-#                 logger.log_metric(self.phase, self.metric_name, self.value)
-#             elif "accuracy" in self.metric_name:
-#                 logger.log_metric(self.phase, self.metric_name, self.value)
-#                 # On peut aussi loguer l'accuracy finale en tant que value
-#                 logger.log_value(f"{self.phase}/final_accuracy", self.value)
-#             elif "epoch_time" in self.metric_name:
-#                 logger.log_metric(self.phase, self.metric_name, self.value)
-#                 # Supposons qu'on calcule l'average epoch time
-#                 logger.log_value(
-#                     f"{self.phase}/average_epoch_time", np.mean(self.value)
-#                 )
-#         elif self.phase == "val":
-#             if "accuracy" in self.metric_name or "loss" in self.metric_name:
-#                 logger.log_metric(self.phase, self.metric_name, self.value)
-#         elif self.phase == "test":
-#             # Sur la phase test, on logue les valeurs finales
-#             logger.log_value(f"{self.phase}/{self.metric_name}", self.value)
-#         else:
-#             raise ValueError(f"Phase {self.phase} non reconnue pour le logging.")
-
-
 class Metric:
     def __init__(self, standard_name: str, framework_name: Optional[str] = None):
         self.standard_name = standard_name
@@ -140,7 +107,9 @@ class BaseLogger:
         log_name = self.get_log_name(metric_name=name, phase=phase)
         self.experiment.log(log_name, value, log_type)
 
-    def log_value(self, name: str, value: float, phase: Optional[str] = None):
+    def log_value(
+        self, name: str, value: float, phase: Optional[str] = None, precision: int = 4
+    ):
         """
         Log a simple value.
 
@@ -150,7 +119,7 @@ class BaseLogger:
             phase (Optional[str]): The phase (e.g., 'train', 'val', 'test').
         """
         log_name = self.get_log_name(metric_name=name, phase=phase)
-        self.experiment.log(log_name, value, LogType.VALUE)
+        self.experiment.log(log_name, round(value, precision), LogType.VALUE)
 
     def log_image(self, name: str, image_path: str, phase: Optional[str] = None):
         """
@@ -207,7 +176,3 @@ class BaseLogger:
             metric_name, metric_name
         )
         return f"{phase}/{mapped_name}" if phase else mapped_name
-
-    # def log(self, phase: str, metric_name: str, value: float):
-    #     strategy = MetricStrategy(phase, metric_name, value)
-    #     strategy.log(self)
