3c292b61683926cc72c409a79ab31577f476d350
SoniaGrh <sonia.garrouch@hotmail.fr>
2024-09-11 16:30:59 +0200

    feat: create load model function

--- src/models/steps/model_loading/ultralytics/ultralytics_model_context_loader.py
+++ src/models/steps/model_loading/ultralytics/ultralytics_model_context_loader.py
@@ -1,4 +1,5 @@
 from ultralytics import YOLO
+import torch
 
 
 def ultralytics_load_model(weights_path_to_load: str, device: str) -> YOLO:
@@ -7,5 +8,7 @@ def ultralytics_load_model(weights_path_to_load: str, device: str) -> YOLO:
     Raises an error if the pretrained model path is not set.
     """
     loaded_model = YOLO(weights_path_to_load)
-    loaded_model.to(device)
+    torch_device = torch.device(device)
+    print(f"Loading model on device: {torch_device}")
+    loaded_model.to(device=device)
     return loaded_model
