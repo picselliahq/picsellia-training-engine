86b58ca1fc0c012b88985faab91cc9d5de03104c
SoniaGrh <sonia.garrouch@hotmail.fr>
2024-09-16 14:22:52 +0200

    fix: explicit model evaluation

--- src/steps/model_evaluation/paddle_ocr_model_evaluator.py
+++ src/steps/model_evaluation/paddle_ocr_model_evaluator.py
@@ -1,3 +1,5 @@
+# type: ignore
+
 from src import step, Pipeline
 from src.models.contexts.training.picsellia_training_context import (
     PicselliaTrainingContext,
@@ -6,27 +8,49 @@ from src.models.dataset.training.training_dataset_collection import TDatasetCont
 from src.models.model.paddle_ocr.paddle_ocr_model_collection import (
     PaddleOCRModelCollection,
 )
+from src.models.parameters.common.export_parameters import ExportParameters
+from src.models.parameters.training.paddle_ocr.paddle_ocr_augmentation_parameters import (
+    PaddleOCRAugmentationParameters,
+)
+from src.models.parameters.training.paddle_ocr.paddle_ocr_hyper_parameters import (
+    PaddleOCRHyperParameters,
+)
 from src.models.steps.model_evaluation.model_evaluator import ModelEvaluator
-from src.models.steps.model_inferencing.paddle_ocr_model_collection_inference import (
-    PaddleOCRModelCollectionInference,
+from src.models.steps.model_prediction.paddle_ocr_model_collection_predictor import (
+    PaddleOCRModelCollectionPredictor,
 )
 
 
 @step
-def paddle_ocr_model_evaluator(
+def paddle_ocr_model_collection_evaluator(
     model_collection: PaddleOCRModelCollection,
     dataset_context: TDatasetContext,
-):
-    context: PicselliaTrainingContext = Pipeline.get_active_context()
+) -> None:
+    context: PicselliaTrainingContext[
+        PaddleOCRHyperParameters, PaddleOCRAugmentationParameters, ExportParameters
+    ] = Pipeline.get_active_context()
 
-    model_inference = PaddleOCRModelCollectionInference(
-        model_collection=model_collection
+    model_collection_predictor = PaddleOCRModelCollectionPredictor(
+        model_collection=model_collection,
     )
-
-    model_evaluator = ModelEvaluator(
-        model_inference=model_inference,
+    image_paths = model_collection_predictor.pre_process_dataset_context(
+        dataset_context=dataset_context
+    )
+    image_batches = model_collection_predictor.prepare_batches(
+        image_paths=image_paths,
+        batch_size=min(
+            context.hyperparameters.bbox_batch_size,
+            context.hyperparameters.text_batch_size,
+        ),
+    )
+    batch_results = model_collection_predictor.run_inference_on_batches(
+        image_batches=image_batches
+    )
+    picsellia_ocr_predictions = model_collection_predictor.post_process_batches(
+        image_batches=image_batches,
+        batch_results=batch_results,
         dataset_context=dataset_context,
-        experiment=context.experiment,
     )
 
-    model_evaluator.evaluate()
+    model_evaluator = ModelEvaluator(experiment=context.experiment)
+    model_evaluator.evaluate(picsellia_predictions=picsellia_ocr_predictions)
