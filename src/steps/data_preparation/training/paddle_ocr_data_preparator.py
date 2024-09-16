985c9c4bcb825ee90b52b3f5a857c7acbef404ab
SoniaGrh <sonia.garrouch@hotmail.fr>
2024-09-16 14:05:48 +0200

    fix: adapt changes

--- src/steps/data_preparation/training/paddle_ocr_data_preparator.py
+++ src/steps/data_preparation/training/paddle_ocr_data_preparator.py
@@ -1,5 +1,7 @@
 from src import step
-from src.models.dataset.common.dataset_collection import DatasetCollection
+from src.models.dataset.training.training_dataset_collection import (
+    TrainingDatasetCollection,
+)
 from src.models.dataset.common.dataset_context import DatasetContext
 from src.models.dataset.common.paddle_ocr_dataset_context import PaddleOCRDatasetContext
 from src.models.steps.data_preparation.training.paddle_ocr_dataset_context_preparator import (
@@ -8,18 +10,14 @@ from src.models.steps.data_preparation.training.paddle_ocr_dataset_context_prepa
 
 
 @step
-def paddle_ocr_data_preparator(
-    dataset_collection: DatasetCollection[DatasetContext],
-) -> DatasetCollection[PaddleOCRDatasetContext]:
-    paddleocr_dataset_collection = DatasetCollection(
-        train_dataset_context=PaddleOCRDatasetContextPreparator(
-            dataset_collection.train
-        ).organize(),
-        val_dataset_context=PaddleOCRDatasetContextPreparator(
-            dataset_collection.val
-        ).organize(),
-        test_dataset_context=PaddleOCRDatasetContextPreparator(
-            dataset_collection.test
-        ).organize(),
+def paddle_ocr_dataset_collection_preparator(
+    dataset_collection: TrainingDatasetCollection[DatasetContext],
+) -> TrainingDatasetCollection[PaddleOCRDatasetContext]:
+    paddleocr_dataset_collection = TrainingDatasetCollection(
+        [
+            PaddleOCRDatasetContextPreparator(dataset_collection["train"]).organize(),
+            PaddleOCRDatasetContextPreparator(dataset_collection["val"]).organize(),
+            PaddleOCRDatasetContextPreparator(dataset_collection["test"]).organize(),
+        ]
     )
     return paddleocr_dataset_collection
