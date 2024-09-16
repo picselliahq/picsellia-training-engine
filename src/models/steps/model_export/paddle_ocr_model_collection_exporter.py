e66d74ba6cd4ea6c2aed0a9b1b115abee0d51a58
SoniaGrh <sonia.garrouch@hotmail.fr>
2024-09-16 10:53:19 +0200

    fix: separate export and save

--- src/models/steps/model_export/paddle_ocr_model_collection_exporter.py
+++ src/models/steps/model_export/paddle_ocr_model_collection_exporter.py
@@ -1,3 +1,9 @@
+import logging
+
+import yaml
+
+
+from src.models.model.common.model_context import ModelContext
 from src.models.model.paddle_ocr.paddle_ocr_model_collection import (
     PaddleOCRModelCollection,
 )
@@ -7,6 +13,8 @@ from src.models.steps.model_export.paddle_ocr_model_context_exporter import (
 
 from picsellia import Experiment
 
+logger = logging.getLogger(__name__)
+
 
 class PaddleOCRModelCollectionExporter:
     def __init__(
@@ -14,24 +22,49 @@ class PaddleOCRModelCollectionExporter:
     ):
         self.model_collection = model_collection
         self.experiment = experiment
+        self.bbox_config = self.get_config(self.model_collection.bbox_model)
+        self.text_config = self.get_config(self.model_collection.text_model)
+        self.bbox_model_context_exporter = PaddleOCRModelContextExporter(
+            model_context=self.model_collection.bbox_model, experiment=self.experiment
+        )
+        self.text_model_context_exporter = PaddleOCRModelContextExporter(
+            model_context=self.model_collection.text_model, experiment=self.experiment
+        )
 
-    def export_and_save_model_collection(self):
+    def get_config(self, model_context: ModelContext) -> dict:
+        if not model_context.config_path:
+            raise ValueError("No configuration file path found in model context")
+        with open(model_context.config_path, "r") as file:
+            config = yaml.load(file, Loader=yaml.FullLoader)
+        return config
+
+    def export_model_collection(self) -> PaddleOCRModelCollection:
         """
         Exports the trained models in the model collection.
         """
-        print("Exporting bounding box model...")
-        model_context_exporter = PaddleOCRModelContextExporter(
-            model_context=self.model_collection.bbox_model, experiment=self.experiment
-        )
-        self.model_collection.bbox_model = (
-            model_context_exporter.export_and_save_model_context()
+        logger.info("Exporting bounding box model...")
+        self.bbox_model_context_exporter.export_model_context(
+            exported_model_destination_path=self.bbox_config["Global"][
+                "save_model_dir"
+            ],
+            export_format="paddle",
         )
 
-        print("Exporting text recognition model...")
-        model_context_exporter = PaddleOCRModelContextExporter(
-            model_context=self.model_collection.text_model, experiment=self.experiment
-        )
-        self.model_collection.text_model = (
-            model_context_exporter.export_and_save_model_context()
+        logger.info("Exporting text recognition model...")
+        self.text_model_context_exporter.export_model_context(
+            exported_model_destination_path=self.text_config["Global"][
+                "save_model_dir"
+            ],
+            export_format="paddle",
         )
         return self.model_collection
+
+    def save_model_collection(self) -> None:
+        self.bbox_model_context_exporter.save_model_to_experiment(
+            exported_weights_dir=self.bbox_config["Global"]["save_model_dir"],
+            exported_weights_name="bbox-model-latest",
+        )
+        self.text_model_context_exporter.save_model_to_experiment(
+            exported_weights_dir=self.text_config["Global"]["save_model_dir"],
+            exported_weights_name="text-model-latest",
+        )
--- src/models/steps/model_export/paddle_ocr_model_context_exporter.py
+++ src/models/steps/model_export/paddle_ocr_model_context_exporter.py
@@ -1,3 +1,4 @@
+import logging
 import os
 import subprocess
 
@@ -7,45 +8,53 @@ from picsellia import Experiment
 from src.models.model.common.model_context import ModelContext
 from src.models.steps.model_export.model_context_exporter import ModelContextExporter
 
+logger = logging.getLogger(__name__)
+
 
 class PaddleOCRModelContextExporter(ModelContextExporter):
     def __init__(self, model_context: ModelContext, experiment: Experiment):
         super().__init__(model_context=model_context, experiment=experiment)
 
-    def export_model_context(self):
+    def export_model_context(
+        self, exported_model_destination_path: str, export_format: str
+    ):
         current_pythonpath = os.environ.get("PYTHONPATH", "")
         os.environ["PYTHONPATH"] = f".:{current_pythonpath}"
 
-        config_path = self.model_context.config_file_path
+        config_path = self.model_context.config_path
         if not config_path:
             raise ValueError("No configuration file path found in model context")
 
         with open(config_path, "r") as file:
             config = yaml.load(file, Loader=yaml.FullLoader)
 
-        save_model_dir = config["Global"]["save_model_dir"]
-
         found_best_accuracy = False
         found_latest = False
-        if config["Global"]["save_model_dir"]:
+        if exported_model_destination_path:
             model_files = [
                 f
-                for f in os.listdir(save_model_dir)
-                if os.path.isfile(os.path.join(save_model_dir, f))
+                for f in os.listdir(exported_model_destination_path)
+                if os.path.isfile(os.path.join(exported_model_destination_path, f))
             ]
-            for file in model_files:
-                if isinstance(file, str):
-                    if file.startswith("best_accuracy"):
+            for model_file in model_files:
+                if isinstance(model_file, str):
+                    if model_file.startswith("best_accuracy"):
                         found_best_accuracy = True
-                    if file.startswith("latest"):
+                    if model_file.startswith("latest"):
                         found_latest = True
 
         if not found_best_accuracy and not found_latest:
-            print(f"No model found in {save_model_dir}, skipping export...")
+            logger.info(
+                f"No model found in {exported_model_destination_path}, skipping export..."
+            )
         elif not found_best_accuracy:
-            config["Global"]["pretrained_model"] = f"{save_model_dir}/latest"
+            config["Global"][
+                "pretrained_model"
+            ] = f"{exported_model_destination_path}/latest"
         else:
-            config["Global"]["pretrained_model"] = f"{save_model_dir}/best_accuracy"
+            config["Global"][
+                "pretrained_model"
+            ] = f"{exported_model_destination_path}/best_accuracy"
 
         with open(config_path, "w") as file:
             yaml.dump(config, file)
