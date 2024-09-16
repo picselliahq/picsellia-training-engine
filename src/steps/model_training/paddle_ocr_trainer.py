d5339ae3e6108cb0c902c1e0fed7306bcf80a067
SoniaGrh <sonia.garrouch@hotmail.fr>
2024-09-16 14:11:03 +0200

    fix: rename

--- src/steps/model_training/paddle_ocr_trainer.py
+++ src/steps/model_training/paddle_ocr_trainer.py
@@ -3,6 +3,7 @@ from src import step, Pipeline
 from src.models.contexts.training.picsellia_training_context import (
     PicselliaTrainingContext,
 )
+from src.models.parameters.common.export_parameters import ExportParameters
 from src.models.parameters.training.paddle_ocr.paddle_ocr_augmentation_parameters import (
     PaddleOCRAugmentationParameters,
 )
@@ -15,9 +16,9 @@ from src.models.steps.model_training.paddle_ocr_model_collection_trainer import 
 
 
 @step
-def paddle_ocr_trainer(model_collection):
+def paddle_ocr_model_collection_trainer(model_collection):
     context: PicselliaTrainingContext[
-        PaddleOCRHyperParameters, PaddleOCRAugmentationParameters
+        PaddleOCRHyperParameters, PaddleOCRAugmentationParameters, ExportParameters
     ] = Pipeline.get_active_context()
     model_trainer = PaddleOCRModelCollectionTrainer(
         model_collection=model_collection, experiment=context.experiment
