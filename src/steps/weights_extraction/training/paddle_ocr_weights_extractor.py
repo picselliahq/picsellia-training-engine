f46b4dfee792f129814eb5b106e9cc0a2db425b4
SoniaGrh <sonia.garrouch@hotmail.fr>
2024-09-16 14:12:54 +0200

    fix: explicit model extraction

--- src/steps/weights_extraction/training/paddle_ocr_weights_extractor.py
+++ src/steps/weights_extraction/training/paddle_ocr_weights_extractor.py
@@ -1,21 +1,55 @@
+# type: ignore
+
+import os
+
 from src import step, Pipeline
 from src.models.contexts.training.picsellia_training_context import (
     PicselliaTrainingContext,
 )
+from src.models.model.common.model_context import ModelContext
 from src.models.model.paddle_ocr.paddle_ocr_model_collection import (
     PaddleOCRModelCollection,
 )
-from src.models.steps.weights_extraction.training.paddle_ocr_model_collection_extractor import (
-    PaddleOCRModelCollectionExtractor,
+from src.models.parameters.common.export_parameters import ExportParameters
+from src.models.parameters.training.paddle_ocr.paddle_ocr_augmentation_parameters import (
+    PaddleOCRAugmentationParameters,
+)
+from src.models.parameters.training.paddle_ocr.paddle_ocr_hyper_parameters import (
+    PaddleOCRHyperParameters,
 )
 
 
 @step
-def paddle_ocr_weights_extractor() -> PaddleOCRModelCollection:
-    context: PicselliaTrainingContext = Pipeline.get_active_context()
-    model_collection_extractor = PaddleOCRModelCollectionExtractor(
-        experiment=context.experiment
+def paddle_ocr_model_collection_extractor() -> PaddleOCRModelCollection:
+    context: PicselliaTrainingContext[
+        PaddleOCRHyperParameters, PaddleOCRAugmentationParameters, ExportParameters
+    ] = Pipeline.get_active_context()
+
+    model_version = context.experiment.get_base_model_version()
+    destination_path = os.path.join(os.getcwd(), context.experiment.name, "model")
+
+    bbox_model = ModelContext(
+        model_name="bbox-model",
+        model_version=model_version,
+        destination_path=destination_path,
+        pretrained_weights_name="bbox-pretrained-model",
+        trained_weights_name=None,
+        config_name="bbox-config",
+        exported_weights_name=None,
     )
-    model_collection = model_collection_extractor.get_model_collection()
+    text_model = ModelContext(
+        model_name="text-model",
+        model_version=model_version,
+        destination_path=destination_path,
+        pretrained_weights_name="text-pretrained-model",
+        trained_weights_name=None,
+        config_name="text-config",
+        exported_weights_name=None,
+    )
+
+    model_collection = PaddleOCRModelCollection(
+        bbox_model=bbox_model, text_model=text_model
+    )
     model_collection.download_weights()
+
     return model_collection
