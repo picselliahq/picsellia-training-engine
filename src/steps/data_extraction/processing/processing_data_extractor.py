13553f84ad472c58604747767ab6c3c1d0b3d78e
SoniaGrh <sonia.garrouch@hotmail.fr>
2024-09-17 16:54:54 +0200

    feat: add datalake extractor

--- src/steps/data_extraction/processing/processing_data_extractor.py
+++ src/steps/data_extraction/processing/processing_data_extractor.py
@@ -1,8 +1,11 @@
 import os
-from typing import Optional
+from typing import Optional, Union
 
 from src import Pipeline
 from src import step
+from src.models.contexts.processing.picsellia_datalake_processing_context import (
+    PicselliaDatalakeProcessingContext,
+)
 from src.models.contexts.processing.picsellia_processing_context import (
     PicselliaProcessingContext,
 )
@@ -105,3 +108,31 @@ def processing_dataset_collection_extractor(
         skip_asset_listing=skip_asset_listing,
     )
     return dataset_collection
+
+
+@step
+def processing_datalake_extractor() -> Union[DatalakeContext, DatalakeCollection]:
+    context: PicselliaDatalakeProcessingContext = Pipeline.get_active_context()
+    input_datalake_context = DatalakeContext(
+        datalake_name="input",
+        datalake=context.input_datalake,
+        destination_path=get_destination_path(context.job_id),
+        data_ids=context.data_ids,
+        use_id=context.use_id,
+    )
+    if context.output_datalake:
+        output_datalake_context = DatalakeContext(
+            datalake_name="output",
+            datalake=context.output_datalake,
+            destination_path=get_destination_path(context.job_id),
+            use_id=context.use_id,
+        )
+        datalake_collection = DatalakeCollection(
+            input_datalake_context=input_datalake_context,
+            output_datalake_context=output_datalake_context,
+        )
+        datalake_collection.download_all()
+        return datalake_collection
+    else:
+        input_datalake_context.download_data(image_dir=input_datalake_context.image_dir)
+        return input_datalake_context
