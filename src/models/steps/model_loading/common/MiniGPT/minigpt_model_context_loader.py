5c1cd4085ade0e0eba4df9b3265f83174f88755f
SoniaGrh <sonia.garrouch@hotmail.fr>
2024-09-25 16:10:18 +0200

    fix: formatting

--- src/models/steps/model_loading/common/MiniGPT/minigpt_model_context_loader.py
+++ src/models/steps/model_loading/common/MiniGPT/minigpt_model_context_loader.py
@@ -6,7 +6,6 @@ import yaml
 from minigpt4.common.config import Config
 from minigpt4.common.registry import registry
 from minigpt4.conversation.conversation import Chat
-from minigpt4.models.minigpt_v2 import MiniGPTv2
 
 import torch
 
@@ -42,21 +41,23 @@ def minigpt_load_model(
     model_config = cfg.model_cfg
     model_config.arch = config["model_architecture"]
     model_config.image_size = config["image_size"]
-    model_config.llama_model = "src/pipelines/datalake_autotagging/llama_model/Llama-2-7b-chat-hf"
+    model_config.llama_model = (
+        "src/pipelines/datalake_autotagging/llama_model/Llama-2-7b-chat-hf"
+    )
     model_config.num_query_token = config["num_query_token"]
     model_config.ckpt = weights_path_to_load
     model_config.low_resource = False
-    
-    if device.startswith('cuda') and torch.cuda.is_available():
-        print(f'Using GPU: {torch.cuda.get_device_name(0)}')
-        device = torch.device('cuda')
-    elif device.startswith('cuda') and not torch.cuda.is_available():
-        print('Using CPU')
-        device = torch.device('cpu')
+
+    if device.startswith("cuda") and torch.cuda.is_available():
+        print(f"Using GPU: {torch.cuda.get_device_name(0)}")
+        device = torch.device("cuda")
+    elif device.startswith("cuda") and not torch.cuda.is_available():
+        print("Using CPU")
+        device = torch.device("cpu")
     else:
-        print('Using CPU')
-        device = torch.device('cpu')
-        
+        print("Using CPU")
+        device = torch.device("cpu")
+
     model_cls = registry.get_model_class(config["model_architecture"])
 
     model = model_cls.from_config(model_config).to(device)
@@ -67,7 +68,7 @@ def minigpt_load_model(
     )
 
     model = model.eval()
-    model.half() 
+    model.half()
 
     chat = Chat(model=model, vis_processor=vis_processor, device=device)
 
