b67d4d1a1973d71a1da4a77a10cddd77dc0066b4
SoniaGrh <sonia.garrouch@hotmail.fr>
2024-09-26 14:04:10 +0200

    fix: remove unecessary conditions

--- src/steps/model_loading/common/CLIP/clip_model_context_loader.py
+++ src/steps/model_loading/common/CLIP/clip_model_context_loader.py
@@ -1,5 +1,3 @@
-import os
-
 from src import step, Pipeline
 from src.models.model.huggingface.hugging_face_model_context import (
     HuggingFaceModelContext,
@@ -14,20 +12,10 @@ def clip_model_context_loader(
     model_context: HuggingFaceModelContext,
 ) -> HuggingFaceModelContext:
     context = Pipeline.get_active_context()
-    if (
-        model_context.pretrained_weights_path
-        and os.path.exists(model_context.pretrained_weights_path)
-        and model_context.config_path
-        and os.path.exists(model_context.config_path)
-    ):
-        loaded_model, loaded_processor = clip_load_model(
-            model_name=model_context.hugging_face_model_name,
-            device=context.processing_parameters.device,
-        )
-        model_context.set_loaded_model(loaded_model)
-        model_context.set_loaded_processor(loaded_processor)
-    else:
-        raise FileNotFoundError(
-            f"Pretrained weights or config file not found at path: {model_context.pretrained_weights_path} or {model_context.config_path}"
-        )
+    loaded_model, loaded_processor = clip_load_model(
+        model_name=model_context.hugging_face_model_name,
+        device=context.processing_parameters.device,
+    )
+    model_context.set_loaded_model(loaded_model)
+    model_context.set_loaded_processor(loaded_processor)
     return model_context
