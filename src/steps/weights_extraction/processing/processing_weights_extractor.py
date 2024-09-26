c1cecba0ac640841debba5cd397529eb21c3d83d
SoniaGrh <sonia.garrouch@hotmail.fr>
2024-09-26 13:38:41 +0200

    fix: remove destination path

--- src/steps/weights_extraction/processing/processing_weights_extractor.py
+++ src/steps/weights_extraction/processing/processing_weights_extractor.py
@@ -21,15 +21,14 @@ def processing_model_context_extractor() -> ModelContext:
     model_context = ModelContext(
         model_name=model_version.name,
         model_version=model_version,
-        destination_path=os.path.join(
-            get_destination_path(job_id=context.job_id), "model"
-        ),
         pretrained_weights_name="checkpoints",
         trained_weights_name=None,
         config_name="config",
         exported_weights_name=None,
     )
     model_context.download_weights(
-        model_weights_destination_path=model_context.weights_dir
+        destination_path=os.path.join(
+            get_destination_path(job_id=context.job_id), "model"
+        )
     )
     return model_context
