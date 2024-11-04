9a4d2df73c442656bf24da975206965215fc05a0
SoniaGrh <sonia.garrouch@hotmail.fr>
2024-11-04 10:29:54 +0000

    feat: add final steps

--- src/pipelines/yolov7_segmentation/local_training_pipeline.py
+++ src/pipelines/yolov7_segmentation/local_training_pipeline.py
@@ -23,6 +23,7 @@ from src.steps.data_validation.common.yolo_segmentation_dataset_collection_valid
 from src.steps.model_evaluation.common.yolov7_model_evaluator import (
     yolov7_model_context_evaluator,
 )
+from src.steps.model_evaluation.common.yolov7_model_evaluator import yolov7_model_context_evaluator
 from src.steps.model_training.yolov7_trainer import yolov7_model_context_trainer
 from src.steps.weights_extraction.training.yolov7_weights_extractor import (
     yolov7_model_context_extractor,
@@ -74,7 +75,6 @@ def yolov7_segmentation_training_pipeline():
     model_context = yolov7_model_context_trainer(
         model_context=model_context, dataset_collection=dataset_collection
     )
-
     yolov7_model_context_evaluator(
         model_context=model_context, dataset_context=dataset_collection["test"]
     )
