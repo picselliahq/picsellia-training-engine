cfd8bf47db9aacf2b7ef7e0ac1b0fc743c4b5c4d
SoniaGrh <sonia.garrouch@hotmail.fr>
2024-11-04 10:25:11 +0000

    fix: use correct training command

--- src/models/steps/model_training/common/yolov7_model_context_trainer.py
+++ src/models/steps/model_training/common/yolov7_model_context_trainer.py
@@ -47,122 +47,65 @@ class Yolov7ModelContextTrainer:
         host: str,
         experiment_id: str,
     ):
-        # command = [
-        #     "python3.10",
-        #
-        #     "src/pipelines/yolov7_segmentation/yolov7/train.py",
-        #
-        #     "--weights",
-        #     self.model_context.pretrained_weights_path,
-        #
-        #     "--cfg",
-        #     self.model_context.config_path,
-        #
-        #     "--data",
-        #     dataset_collection.config_path,
-        #
-        #     "--hyp",
-        #     self.model_context.hyperparameters_path,
-        #
-        #     "--epochs",
-        #     str(hyperparameters.epochs),
-        #
-        #     "--batch-size",
-        #     str(hyperparameters.batch_size),
-        #
-        #     "--img-size",
-        #     str(hyperparameters.image_size),
-        #
-        #     "--device",
-        #     str(hyperparameters.device),
-        #
-        #     "--project",
-        #     self.model_context.results_dir,
-        #
-        #     "--name",
-        #     self.model_context.model_name,
-        # ]
-
+        train_file_path = os.path.abspath("src/pipelines/yolov7_segmentation/yolov7/seg/segment/train.py")
+        
         command = [
             "python3.10",
-            "src/pipelines/yolov7_segmentation/yolov7/train.py",
-            "--img-size",
-            "640",
-            "--cfg",
-            "test_2/model/weights/config/yolov7.yaml",
-            "--hyp",
-            "test_2/model/weights/hyp.scratch.custom.yaml",
-            "--batch-size",
-            "4",
-            "--epochs",
-            "1",
-            "--data",
-            "test_2/yolov7_dataset/dataset_config.yaml",
+        
+            train_file_path,
+        
             "--weights",
-            "test_2/model/weights/pretrained_weights/yolov7_training.pt",
+            self.model_context.pretrained_weights_path,
+        
+            "--cfg",
+            self.model_context.config_path,
+        
+            "--data",
+            dataset_collection.config_path,
+        
+            "--hyp",
+            self.model_context.hyperparameters_path,
+        
+            "--epochs",
+            str(hyperparameters.epochs),
+        
+            "--batch-size",
+            str(hyperparameters.batch_size),
+        
+            "--img-size",
+            str(hyperparameters.image_size),
+        
+            "--device",
+            str(hyperparameters.device),
+        
+            "--project",
+            os.path.join(self.model_context.results_dir, "training"),
+        
             "--name",
-            "Yolov7-0",
+            self.model_context.model_name,
+            
             "--api_token",
-            f"{api_token}",
+            api_token,
+            
             "--organization_id",
-            f"{organization_id}",
+            organization_id,
+            
             "--host",
-            f"{host}",
+            host,
+            
             "--experiment_id",
-            f"{experiment_id}",
+            experiment_id,
         ]
 
-        print(f"Command: {command}")
-
-        current_pythonpath = os.environ.get("PYTHONPATH", "")
-        os.environ["PYTHONPATH"] = f".:{current_pythonpath}"
-
         process = subprocess.Popen(
-            command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True
+            command,
+            stdout=None,
+            stderr=None,
+            text=True
         )
 
-        try:
-            self._process_training_output(process, self.model_context)
-        except Exception as e:
-            print("Error during model training:", e)
-        finally:
-            process.wait()
-            if process.returncode != 0:
-                handle_training_failure(process)
-
-            os.environ["PYTHONPATH"] = current_pythonpath
-
-        # process = subprocess.run(
-        #     command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True
-        # )
-        #
-        # print("Standard Output:\n", process.stdout)
-        # print("Error Output:\n", process.stderr)
-        # print("Exit Code:", process.returncode)
-        #
-        # if process.returncode != 0:
-        #     print("An error occurred during training")
-
-    def _process_training_output(
-        self, process: subprocess.Popen, model_context: ModelContext
-    ):
-        """
-        Processes the output from the training subprocess and extracts metrics.
-
-        This method reads the training logs line by line, extracts relevant metrics, and logs
-        them to the Picsellia experiment. Only new metrics from previously unlogged epochs are recorded.
-
-        Args:
-            process (subprocess.Popen): The subprocess object running the training command.
-            model_context (ModelContext): The model context containing information about the model being trained.
-        """
-        if process.stdout:
-            while True:
-                output = process.stdout.readline()
-                if output == "" and process.poll() is not None:
-                    break
-                if output:
-                    print(output.strip())
-                # Flush output to ensure continuous printing in real-time
-                sys.stdout.flush()
-                time.sleep(0.1)  # Small delay to avoid excessive CPU usage
+        return_code = process.wait()
+        if return_code != 0:
+            print("Training failed with errors.")
+        else:
+            print("Training completed successfully.")
