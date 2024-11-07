2e9bdec0b4b411c7bf374591e7cba8a5a18f0bf0
SoniaGrh <sonia.garrouch@hotmail.fr>
2024-11-07 11:39:57 +0100

    fix: typing

--- src/models/model/yolov7_model_context.py
+++ src/models/model/yolov7_model_context.py
@@ -6,13 +6,14 @@ from picsellia import ModelVersion, Label
 
 import yaml
 
+
 def find_latest_run_dir(dir):
     """
     Finds the latest run directory in the given directory.
     """
     run_dirs = os.listdir(dir)
     processed_run_dirs = {}
-    
+
     for run_dir in run_dirs:
         run_id = -1
         if "-" in run_dir:
@@ -20,10 +21,10 @@ def find_latest_run_dir(dir):
                 run_id = int(run_dir.split("-")[1])
             except ValueError:
                 pass
-        
+
         while run_id in processed_run_dirs:
             run_id -= 1
-            
+
         processed_run_dirs[run_id] = run_dir
 
     return processed_run_dirs[max(processed_run_dirs)]
