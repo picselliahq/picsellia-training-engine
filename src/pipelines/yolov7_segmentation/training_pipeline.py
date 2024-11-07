db1aadc9f0faefcf9215f09ebc7ecbe5e02de80d
SoniaGrh <sonia.garrouch@hotmail.fr>
2024-11-07 11:43:15 +0100

    fix: add evaluation step

--- src/pipelines/yolov7_segmentation/training_pipeline.py
+++ src/pipelines/yolov7_segmentation/training_pipeline.py
@@ -3,11 +3,11 @@ from src.models.contexts.training.picsellia_training_context import (
     PicselliaTrainingContext,
 )
 from src.models.parameters.common.export_parameters import ExportParameters
-from src.models.parameters.training.ultralytics.ultralytics_augmentation_parameters import (
-    UltralyticsAugmentationParameters,
+from src.models.parameters.training.yolov7.yolov7_augmentation_parameters import (
+    Yolov7AugmentationParameters,
 )
-from src.models.parameters.training.ultralytics.ultralytics_hyper_parameters import (
-    UltralyticsHyperParameters,
+from src.models.parameters.training.yolov7.yolov7_hyper_parameters import (
+    Yolov7HyperParameters,
 )
 from src.steps.data_extraction.training.training_data_extractor import (
     training_dataset_collection_extractor,
@@ -17,6 +17,9 @@ from src.steps.data_preparation.training.yolov7_data_preparator import (
 )
 from src.steps.data_validation.common.segmentation_dataset_collection_validator import (
     segmentation_dataset_collection_validator,
+)
+from src.steps.model_evaluation.common.yolov7_model_evaluator import (
+    yolov7_model_context_evaluator,
 )
 from src.steps.model_training.yolov7_trainer import yolov7_model_context_trainer
 from src.steps.weights_extraction.training.yolov7_weights_extractor import (
@@ -29,12 +32,12 @@ from src.steps.weights_preparation.training.yolov7_weights_preparator import (
 
 def get_context() -> (
     PicselliaTrainingContext[
-        UltralyticsHyperParameters, UltralyticsAugmentationParameters, ExportParameters
+        Yolov7HyperParameters, Yolov7AugmentationParameters, ExportParameters
     ]
 ):
     return PicselliaTrainingContext(
-        hyperparameters_cls=UltralyticsHyperParameters,
-        augmentation_parameters_cls=UltralyticsAugmentationParameters,
+        hyperparameters_cls=Yolov7HyperParameters,
+        augmentation_parameters_cls=Yolov7AugmentationParameters,
         export_parameters_cls=ExportParameters,
     )
 
@@ -55,14 +58,13 @@ def yolov7_segmentation_training_pipeline():
 
     model_context = yolov7_model_context_extractor()
     model_context = yolov7_model_context_preparator(model_context=model_context)
-    yolov7_model_context_trainer(
+    model_context = yolov7_model_context_trainer(
         model_context=model_context, dataset_collection=dataset_collection
     )
-    #
-    # ultralytics_model_context_exporter(model_context=model_context)
-    # ultralytics_model_context_evaluator(
-    #     model_context=model_context, dataset_context=dataset_collection["test"]
-    # )
+
+    yolov7_model_context_evaluator(
+        model_context=model_context, dataset_context=dataset_collection["test"]
+    )
 
 
 if __name__ == "__main__":
