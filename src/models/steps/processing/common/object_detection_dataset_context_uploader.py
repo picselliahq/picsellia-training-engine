65d6b2ab23f80e5041fb56c6c43ec15869716021
SoniaGrh <sonia.garrouch@hotmail.fr>
2024-11-14 14:34:16 +0100

    fix: use batch size

--- src/models/steps/processing/common/object_detection_dataset_context_uploader.py
+++ src/models/steps/processing/common/object_detection_dataset_context_uploader.py
@@ -32,6 +32,9 @@ class ObjectDetectionDatasetContextUploader(DataUploader):
         dataset_context: DatasetContext,
         datalake: str = "default",
         data_tags: Optional[List[str]] = None,
+        batch_size: int = 10000,
+        use_id: bool = True,
+        fail_on_asset_not_found: bool = True,
     ):
         """
         Initializes the ObjectDetectionDatasetContextUploader with a client, dataset context, and optional datalake.
@@ -41,38 +44,47 @@ class ObjectDetectionDatasetContextUploader(DataUploader):
             dataset_context (DatasetContext): The dataset context containing images and annotations.
             datalake (str): The name of the datalake where the images will be uploaded (default is 'default').
             data_tags (Optional[List[str]]): Optional tags to associate with the uploaded images.
+            batch_size (int): The number of images per batch for uploading (default is 10).
         """
         super().__init__(client, dataset_context.dataset_version, datalake)
         self.client = client
         self.dataset_context = dataset_context
         self.datalake = self.client.get_datalake(name=datalake)
         self.data_tags = data_tags
+        self.batch_size = batch_size
+        self.use_id = use_id
+        self.fail_on_asset_not_found = fail_on_asset_not_found
 
     def upload_dataset_context(self) -> None:
         """
         Uploads the dataset context to Picsellia, including images and annotations.
 
-        This method uploads the images from the dataset context to the datalake. If the dataset version
+        This method uploads the images from the dataset context to the datalake in batches. If the dataset version
         is configured for object detection (i.e., its type is not `NOT_CONFIGURED`), it will also upload
-        COCO annotations. Otherwise, it logs that no annotations will be uploaded due to the dataset type.
+        COCO annotations in batches. Otherwise, it logs that no annotations will be uploaded due to the dataset type.
         """
-        # Upload images to the datalake
+        # Upload images to the datalake in batches
         if self.dataset_context.images_dir:
-            self._add_images_to_dataset_version(
+            self._add_images_to_dataset_version_in_batches(
                 images_to_upload=[
                     os.path.join(self.dataset_context.images_dir, image_filename)
                     for image_filename in os.listdir(self.dataset_context.images_dir)
                 ],
                 data_tags=self.data_tags,
+                batch_size=self.batch_size,
             )
 
-        # Check the dataset type and conditionally upload annotations
+        # Check the dataset type and conditionally upload annotations in batches
         if (
             self.dataset_context.dataset_version.type != InferenceType.NOT_CONFIGURED
             and self.dataset_context.coco_file_path
         ):
-            self._add_coco_annotations_to_dataset_version(
-                annotation_path=self.dataset_context.coco_file_path
+            print("Uploading annotations")
+            self._add_coco_annotations_to_dataset_version_in_batches(
+                annotation_path=self.dataset_context.coco_file_path,
+                batch_size=self.batch_size,
+                use_id=self.use_id,
+                fail_on_asset_not_found=self.fail_on_asset_not_found,
             )
         else:
             logger.info(
