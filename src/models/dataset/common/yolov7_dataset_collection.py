374348992de8e8516357547de8d1354a1ca636b2
SoniaGrh <sonia.garrouch@hotmail.fr>
2024-11-14 14:29:51 +0100

    feat: skip download coco for yolov7

--- src/models/dataset/common/yolov7_dataset_collection.py
+++ src/models/dataset/common/yolov7_dataset_collection.py
@@ -11,6 +11,43 @@ class Yolov7DatasetCollection(DatasetCollection):
         super().__init__(datasets=datasets)
         self.config_path: Optional[str] = None
 
+    def download_all(
+        self,
+        destination_path: str,
+        use_id: Optional[bool] = None,
+        skip_asset_listing: bool = False,
+    ) -> None:
+        """
+        Downloads all assets and annotations for every dataset context in the collection.
+
+        For each dataset context, this method:
+        1. Downloads the assets (images) to the corresponding image directory.
+        2. Downloads and builds the COCO annotation file for each dataset.
+
+        Args:
+            destination_path (str): The directory where all datasets (images and annotations) will be saved.
+            use_id (Optional[bool]): Whether to use asset IDs in the file paths. If None, the internal logic of each dataset context will handle it.
+            skip_asset_listing (bool, optional): If True, skips listing the assets when downloading. Defaults to False.
+
+        Example:
+            If you want to download assets and annotations for both train and validation datasets,
+            this method will create two directories (e.g., `train/images`, `train/annotations`,
+            `val/images`, `val/annotations`) under the specified `destination_path`.
+        """
+        for dataset_context in self:
+            # Download dataset assets (images) into 'images' directory
+            print(f"Downloading assets for {dataset_context.dataset_name}")
+            dataset_context.download_assets(
+                destination_path=os.path.join(
+                    destination_path, dataset_context.dataset_name, "images"
+                ),
+                use_id=use_id,
+                skip_asset_listing=skip_asset_listing,
+            )
+
+        # Set the common dataset path for all splits
+        self.dataset_path = destination_path
+
     def write_config(self, config_path: str) -> None:
         """
         Writes the dataset collection configuration to a YAML file.
