20eef229194acba58688a663c3ea2dd9daf8681a
SoniaGrh <sonia.garrouch@hotmail.fr>
2024-11-14 14:37:40 +0100

    fix: move images by batch

--- src/steps/data_preparation/training/yolov7_data_preparator.py
+++ src/steps/data_preparation/training/yolov7_data_preparator.py
@@ -1,16 +1,17 @@
 import os
+import shutil
+
 from src import step, Pipeline
-from src.models.dataset.common.dataset_collection import DatasetCollection
 from picsellia.types.enums import AnnotationFileType
 
 from src.models.dataset.common.yolov7_dataset_collection import Yolov7DatasetCollection
 
-BATCH_SIZE = 500
+BATCH_SIZE = 10000
 
 
 @step
 def yolov7_dataset_collection_preparator(
-    dataset_collection: DatasetCollection,
+    dataset_collection: Yolov7DatasetCollection,
 ) -> Yolov7DatasetCollection:
     context = Pipeline.get_active_context()
 
@@ -39,23 +40,36 @@ def yolov7_dataset_collection_preparator(
         )
 
         for i in range(0, len(dataset_context.assets), BATCH_SIZE):
-            batch_assets = dataset_context.assets[i:i + BATCH_SIZE]
+            batch_assets = dataset_context.assets[i : i + BATCH_SIZE]
 
-            yolo_annotation_path = dataset_context.dataset_version.export_annotation_file(
-                annotation_file_type=AnnotationFileType.YOLO,
-                target_path=dataset_context.annotations_dir,
-                use_id=True,
-                assets=batch_assets,
+            yolo_annotation_path = (
+                dataset_context.dataset_version.export_annotation_file(
+                    annotation_file_type=AnnotationFileType.YOLO,
+                    target_path=os.path.join(
+                        yolov7_dataset_collection.dataset_path,
+                        dataset_context.dataset_name,
+                        "annotations",
+                    ),
+                    use_id=True,
+                    assets=batch_assets,
+                )
             )
-            
+
             os.system(
                 f"unzip -o {yolo_annotation_path} -d {os.path.join(annotations_destination_path, dataset_context.dataset_name)} > /dev/null 2>&1"
             )
             # os.remove(yolo_annotation_path)
 
-        os.system(
-            f"cp {dataset_context.images_dir}/* {os.path.join(images_destination_path, dataset_context.dataset_name)}"
+        image_files = os.listdir(dataset_context.images_dir)
+        destination_dir = os.path.join(
+            images_destination_path, dataset_context.dataset_name
         )
+
+        for i in range(0, len(image_files), BATCH_SIZE):
+            batch_files = image_files[i : i + BATCH_SIZE]
+            for file_name in batch_files:
+                src_file = os.path.join(dataset_context.images_dir, file_name)
+                shutil.copy(src_file, destination_dir)
 
         dataset_context.images_dir = os.path.join(
             images_destination_path, dataset_context.dataset_name
