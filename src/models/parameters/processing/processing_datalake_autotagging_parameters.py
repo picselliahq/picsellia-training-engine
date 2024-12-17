d3c10eb996504532cc2418781ec2bc6db5a6493c
SoniaGrh <sonia.garrouch@hotmail.fr>
2024-12-17 15:19:25 +0100

    fix: remove device + transform tag list

--- src/models/parameters/processing/processing_datalake_autotagging_parameters.py
+++ src/models/parameters/processing/processing_datalake_autotagging_parameters.py
@@ -1,3 +1,4 @@
+import re
 from typing import List
 
 from src.models.parameters.common.parameters import Parameters
@@ -7,11 +8,8 @@ class ProcessingDatalakeAutotaggingParameters(Parameters):
     def __init__(self, log_data):
         super().__init__(log_data)
 
-        self.tags_list = self.extract_parameter(
-            keys=["tags_list"], expected_type=List[str]
-        )
-        self.device = self.extract_parameter(
-            keys=["device"], expected_type=str, default="cuda:0"
+        self.tags_list: List[str] = re.findall(
+            r"'(.*?)'", self.extract_parameter(keys=["tags_list"], expected_type=str)
         )
         self.batch_size = self.extract_parameter(
             keys=["batch_size"], expected_type=int, default=8
