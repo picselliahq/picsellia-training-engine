60de6eb98d3c073cfe7eca950255486f459389c5
SoniaGrh <sonia.garrouch@hotmail.fr>
2024-12-17 15:20:43 +0100

    fix: use model version param for model name

--- src/steps/weights_extraction/common/hugging_face_weights_extractor.py
+++ src/steps/weights_extraction/common/hugging_face_weights_extractor.py
@@ -1,4 +1,5 @@
 import logging
+from typing import Optional
 
 from src import step, Pipeline
 from src.models.contexts.processing.picsellia_processing_context import (
@@ -13,10 +14,18 @@ logger = logging.getLogger(__name__)
 
 @step
 def hugging_face_model_context_extractor(
-    hugging_face_model_name: str,
+    hugging_face_model_name: Optional[str] = None,
 ) -> HuggingFaceModelContext:
     context: PicselliaProcessingContext = Pipeline.get_active_context()
     model_version = context.model_version
+    if not hugging_face_model_name:
+        model_parameters = model_version.sync()["parameters"]
+        hugging_face_model_name = model_parameters.get("hugging_face_model_name")
+        if not hugging_face_model_name:
+            raise ValueError(
+                "Hugging Face model name not provided. Please provide it as an argument or set the 'hugging_face_model_name' parameter in the model version."
+            )
+    print(f"Loading Hugging Face model {hugging_face_model_name}")
     model_context = HuggingFaceModelContext(
         hugging_face_model_name=hugging_face_model_name,
         model_name=model_version.name,
