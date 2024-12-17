4d8a0bee44787336b2792a2d36be20913acca785
SoniaGrh <sonia.garrouch@hotmail.fr>
2024-12-17 15:19:55 +0100

    fix: add device param

--- src/steps/model_loading/common/CLIP/clip_model_context_loader.py
+++ src/steps/model_loading/common/CLIP/clip_model_context_loader.py
@@ -1,4 +1,4 @@
-from src import step, Pipeline
+from src import step
 from src.models.model.huggingface.hugging_face_model_context import (
     HuggingFaceModelContext,
 )
@@ -10,11 +10,11 @@ from src.models.steps.model_loading.common.CLIP.clip_model_context_loader import
 @step
 def clip_model_context_loader(
     model_context: HuggingFaceModelContext,
+    device: str = "cuda:0",
 ) -> HuggingFaceModelContext:
-    context = Pipeline.get_active_context()
     loaded_model, loaded_processor = clip_load_model(
         model_name=model_context.hugging_face_model_name,
-        device=context.processing_parameters.device,
+        device=device,
     )
     model_context.set_loaded_model(loaded_model)
     model_context.set_loaded_processor(loaded_processor)
