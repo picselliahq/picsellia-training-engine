9cd3efaea939645c634b4000aef5da22dbc3f8c3
SoniaGrh <sonia.garrouch@hotmail.fr>
2025-01-09 16:25:50 +0100

    feat: add augmentation processing example

---
+++ examples/processing/augmentation/augmentations_pipeline.py
@@ -0,0 +1,31 @@
+from examples.processing.augmentation.utils.augmentations import process_dataset
+from src import pipeline
+from src.models.utils.picsellia_context import create_picsellia_processing_context
+from src.steps.data_extraction.processing.processing_data_extractor import (
+    get_processing_dataset_collection,
+)
+from src.steps.processing.common.dataset_context_uploader import upload_dataset_context
+
+
+@pipeline(
+    context=create_picsellia_processing_context(
+        processing_parameters={
+            "augmentation_probability": 0.5,
+            "datalake": "default",
+            "data_tag": "augmented_data",
+        }
+    ),
+    log_folder_path="logs/",
+    remove_logs_on_completion=False,
+)
+def augmentations_pipeline():
+    dataset_collection = get_processing_dataset_collection()
+    dataset_collection["output"] = process_dataset(
+        dataset_collection["input"], dataset_collection["output"]
+    )
+    upload_dataset_context(dataset_collection["output"], use_id=False)
+    return dataset_collection
+
+
+if __name__ == "__main__":
+    augmentations_pipeline()
